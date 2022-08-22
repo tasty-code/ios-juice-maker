f92551390f5c6c1df61fb7f4bc7d26fe37f869d9
wormsJJG <jjr66461837@gmail.com>
2022-08-22 11:56:26 +0900

    FIX: 초기값 선언 없앰

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,19 +6,18 @@
 
 import Foundation
 
-enum Fruit {
+enum Fruit: Int {
     case strawberry, banana, pineapple, kiwi, mango
 }
 
 // 과일 저장소 타입
 class FruitStore {
-    var strawberry: Int = 10
-    var banana: Int = 10
-    var pineapple: Int = 10
-    var kiwi: Int = 10
-    var mango: Int = 10
     
     
+    
+    func changeAmount(fruit: Fruit, amount: Int) {
+        
+    }
 }
 
 
