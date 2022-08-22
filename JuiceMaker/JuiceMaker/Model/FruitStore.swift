1ccc94ec44cb707c653b896068aaefbbf6ecc5aa
이은호 <ieunho@ieunhoui-MacBookAir.local>
2022-08-22 10:51:54 +0900

    feat: enum타입으로 과일들 생성, 정수형으로 과일 변수 생성, 과일 개수 수정 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,29 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
+    enum Fruits {
+        case starawberry, banana, pineapple, kiwi, mango
+    }
+    
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
+    
+    func changeAmount(fruit: Fruits, amount: Int) {
+        switch fruit {
+        case .starawberry:
+            strawberry = amount
+        case .banana:
+            banana = amount
+        case .pineapple:
+            pineapple = amount
+        case .kiwi:
+            kiwi = amount
+        case .mango:
+            mango = amount
+        }
+    }
+    
 }
