70b6276b850cf4a763a0eea99b5e581048ef51f1
wormsJJG <jjr66461837@gmail.com>
2022-08-22 12:26:24 +0900

    FEAT: 과일주스 레시피 제작

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,40 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
-struct JuiceMaker {
+struct JuiceIngredient {
+    var count: Int
+}
+
+enum Juice: String {
+    case strawberry = "딸기 주스"
+    case banana = "바나나 주스"
+    case kiwi = "키위 주스"
+    case pineapple = "파인애플 주스"
+    case strawberryBanana = "딸바 주스"
+    case mango = "망고 주스"
+    case mangoKiwi = "망키 주스"
     
+    var recipe: [Fruit: JuiceIngredient] {
+        switch self {
+        case .strawberry:
+            return [.strawberry: JuiceIngredient(count: 16)]
+        case .banana:
+            return [.banana: JuiceIngredient(count: 2)]
+        case .kiwi:
+            return [.kiwi: JuiceIngredient(count: 3)]
+        case .pineapple:
+            return [.pineapple: JuiceIngredient(count: 2)]
+        case .strawberryBanana:
+            return [.strawberry: JuiceIngredient(count: 10), .banana: JuiceIngredient(count: 1)]
+        case .mango:
+            return [.mango: JuiceIngredient(count: 3)]
+        case .mangoKiwi:
+            return [.mango: JuiceIngredient(count: 2), .kiwi: JuiceIngredient(count: 1)]
+        }
+    }
+}
+
+struct JuiceMaker {
+   let fruitStore = FruitStore()
+
 }
