bc528c9f8cc2be2c9fc6fba89d2090dcc0d7949f
wormsJJG <jjr66461837@gmail.com>
2022-08-22 11:49:22 +0900

    FEAT: 초기값 선언

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -12,7 +12,13 @@ enum Fruit {
 
 // 과일 저장소 타입
 class FruitStore {
-
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
+    
+    
 }
 
 
