529fe8f0cb6016718ad09451daacb1ea2a7b8af5
lcy <pgbenny0035@gmail.com>
2022-08-22 12:08:59 +0900

    FEAT: 과일 초기값 선언

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -12,11 +12,26 @@ enum Fruit: Int {
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
     
-    
-    
+     
     func changeAmount(fruit: Fruit, amount: Int) {
-        
+        switch fruit {
+        case .strawberry:
+            strawberry = amount
+        case .banana:
+            banana = amount
+        case .pineapple:
+            pineapple = amount
+        case .kiwi:
+            kiwi = amount
+        case .mango:
+            mango = amount
+        }
     }
 }
 
