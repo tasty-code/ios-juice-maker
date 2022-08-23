8c0611c2367bc493672fa5afcb1c7fa5eb83468e
moonsoungjin <seongjinmun74@gmail.com>
2022-08-23 14:37:19 +0900

    FruitStore 변수 선언

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,17 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
+    var strawbrrie : Int
+    var bannaa : Int
+    var pineapple : Int
+    var kiwi : Int
+    var mango : Int
+    
+    init(strawbrrie: Int = 10, bannaa: Int = 10,pineapple: Int = 10, kiwi: Int = 10, mango: Int = 10) {
+        self.strawbrrie = strawbrrie
+        self.bannaa = bannaa
+        self.pineapple = pineapple
+        self.kiwi = kiwi
+        self.mango = mango
+    }
 }
