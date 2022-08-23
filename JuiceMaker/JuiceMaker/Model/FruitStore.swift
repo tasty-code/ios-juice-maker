e63a1fa5b7ba9517072cf220607d2a77c54b7279
박도원 <ddowon@bagdowon-ui-MacBookAir.local>
2022-08-23 15:29:31 +0900

    feat: switch문으로 과일 제조 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,18 +8,19 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
-    var strawbrrie : Int
-    var bannaa : Int
+    var strawberry : Int
+    var banana : Int
     var pineapple : Int
     var kiwi : Int
     var mango : Int
+    var change: Int
     
-    init(strawbrrie: Int = 10, bannaa: Int = 10,pineapple: Int = 10, kiwi: Int = 10, mango: Int = 10) {
-        self.strawbrrie = strawbrrie
-        self.bannaa = bannaa
+    init(strawberry: Int = 10, banana: Int = 10, pineapple: Int = 10, kiwi: Int = 10, mango: Int = 10, change: Int = 0) {
+        self.strawberry = strawberry
+        self.banana = banana
         self.pineapple = pineapple
         self.kiwi = kiwi
         self.mango = mango
+        self.change = change
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,17 @@
 
 import Foundation
 
+
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let juice: String = readLine()!
+    let fruit: [Int] = []
+    let fruitQuantity = FruitStore(strawberry: -16, banana: -2, pineapple: -2, kiwi: -3, mango: -3, change: 0)
     
+    switch juice {
+        case: "딸기주스"
+            ㅇ
+            
+    }
 }
