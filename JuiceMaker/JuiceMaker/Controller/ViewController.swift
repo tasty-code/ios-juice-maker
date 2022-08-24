01724166d4b561aacd7e392d4213fac9c3d7e33b
wormsJJG <jjr66461837@gmail.com>
2022-08-24 13:56:17 +0900

    레이블 아웃렛 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,6 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryAmount: UILabel!
+    @IBOutlet weak var bananaAmount: UILabel!
+    @IBOutlet weak var kiwiAmount: UILabel!
+    @IBOutlet weak var pineAppleAmount: UILabel!
+    @IBOutlet weak var mangoAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
