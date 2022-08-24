a635105176d59222bc7ff799f73040ae85743561
wormsJJG <jjr66461837@gmail.com>
2022-08-24 14:17:56 +0900

    Alert 함수 생성

---
+++ JuiceMaker/JuiceMaker/Controller/Alerts.swift
@@ -0,0 +1,29 @@
+//
+//  Alerts.swift
+//  JuiceMaker
+//
+//  Created by 정재근 on 2022/08/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func sucessMakeJuiceAlert(_ juice: Juice) {
+        let alert = UIAlertController(title: "제조완료", message: "\(juice.name) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+    }
+    func failMakeJuiceAlert() {
+        let alert = UIAlertController(title: "제조실패", message: "재료가 모자라요 재고를 수정할까요?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "예", style: .cancel) { action in
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let amountView = storyboard.instantiateViewController(withIdentifier: "AmountV")
+            self.present(amountView, animated: false, completion: nil)
+        }
+        let noAction = UIAlertAction(title: "아니요", style: .default)
+        
+        alert.addAction(okAction)
+        alert.addAction(noAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
---
+++ JuiceMaker/JuiceMaker/Controller/AmountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AmountViewController.swift
+//  JuiceMaker
+//
+//  Created by 정재근 on 2022/08/24.
+//
+
+import UIKit
+
+class AmountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
