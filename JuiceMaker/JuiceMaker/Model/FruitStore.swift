f61974b4b9faa586f67c1d0fadbb0408d08c9d74
이은호 <ieunho@ieunhoui-MacBookAir.local>
2022-08-24 10:00:37 +0900

    feat: FruitStore 재고 관리 방식 변경 및 JuiceMaker의 주스 제작 함수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,28 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    enum Fruits {
-        case starawberry, banana, pineapple, kiwi, mango
+    enum Fruit {
+        case strawberry, banana, pineapple, kiwi, mango
     }
     
-    var strawberry: Int = 10
-    var banana: Int = 10
-    var pineapple: Int = 10
-    var kiwi: Int = 10
-    var mango: Int = 10
+    var fruits: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     
-    func changeAmount(fruit: Fruits, amount: Int) {
-        switch fruit {
-        case .starawberry:
-            strawberry = amount
-        case .banana:
-            banana = amount
-        case .pineapple:
-            pineapple = amount
-        case .kiwi:
-            kiwi = amount
-        case .mango:
-            mango = amount
-        }
+    func changeAmount(of fruit: Fruit, to amount: Int) {
+        fruits[fruit] = amount
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,9 +7,10 @@
 import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    var fruitStore = FruitStore()
     
     enum Juice {
-        case strawberryJuice = ["딸기": 1, "바나나": 2]
+        case strawberryJuice
         case bananaJuice
         case kiwiJuice
         case pineappleJuice
@@ -19,10 +20,35 @@ struct JuiceMaker {
         
     }
     
-    var fruitStore = FruitStore()
+    let ingredientsForJuice: [Juice: [FruitStore.Fruit: Int]] =
+    [.strawberryJuice: [.strawberry : 16],
+     .bananaJuice: [.banana: 2],
+     .kiwiJuice: [.kiwi: 3],
+     .pineappleJuice: [.pineapple: 2],
+     .strawberryBananaJuice: [.strawberry: 10, .banana: 1],
+     .mangoJuice: [.mango: 3],
+     .mangoKiwiJuice: [.mango: 2, .kiwi: 1]]
     
-    func makeJuice(of: Juice) {
+    func makeJuice(of juiceType: Juice) {
+        if isMakeAble(to: ingredientsForJuice[juiceType]) {
+            
+        }
+    }
+    
+    func isMakeAble(to optionalIngredients: [FruitStore.Fruit: Int]?) -> Bool {
+        guard let ingredients = optionalIngredients else {
+            return false
+        }
+        for demandFruitAmount in ingredients {
+            let currentIngredients: FruitStore.Fruit = demandFruitAmount.key
+            let storesFruitAmount = fruitStore.fruits[currentIngredients]
+            
+            if demandFruitAmount.value > storesFruitAmount ?? 0 {
+                return false
+            }
+        }
         
+        return true
     }
     
 }
