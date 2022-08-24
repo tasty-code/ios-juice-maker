6d22a7b14e704f31fad02b1d7fb1b7ce5a9fbc34
parkseyoon <issou06261@gmail.com>
2022-08-24 17:03:07 +0900

    feat: selectJuice 함수 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,42 @@
 
 import Foundation
 
+let Fruit = FruitStore()
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+    func selectJuice(fruit: String) {
+        switch fruit{
+        case "strawberry":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "banana":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "pineapple":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "kiwi":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "mango":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "strawberryBanana":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        case "mangoKiwi":
+            if checkStock(fruit: fruit) == true {
+                printFruitJuice(fruit: fruit)
+            }
+        default:
+            print("이상한 값 등장!")
+        }
+    }
 }
