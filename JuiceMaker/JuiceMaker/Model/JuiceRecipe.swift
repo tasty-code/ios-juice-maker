31b394c05f3d316d7f4705e0ce4cedcb951b4ccc
lcy <pgbenny0035@gmail.com>
2022-08-24 10:00:21 +0900

    FEAT: Recipe 타입 생성

--- JuiceMaker/JuiceMaker/Model/JuiceRecipe.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceRecipe.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 enum recipes {
-    static let strawberry = []
+    static let strawberry = [(Fruit.strawberry, 16)]
+    static let banana = [(Fruit.banana, 2)]
+    static let kiwi = [(Fruit.kiwi, 3)]
+    static let pineapple = [(Fruit.pineapple, 2)]
+    static let strawberryBanana = [(Fruit.strawberry, 10), (Fruit.banana, 1)]
+    static let mango = [(Fruit.mango, 3)]
+    static let mangoKiwi = [(Fruit.mango, 2), (Fruit.kiwi, 1)]
 }
 
