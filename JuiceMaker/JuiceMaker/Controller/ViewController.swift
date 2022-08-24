afaf70f82ccf17ffdc4f58c1ced8dae179de9477
lcy <pgbenny0035@gmail.com>
2022-08-24 14:02:58 +0900

    FEAT: updateAmount 함수 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -11,13 +11,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var strawberryAmount: UILabel!
     @IBOutlet weak var bananaAmount: UILabel!
     @IBOutlet weak var kiwiAmount: UILabel!
-    @IBOutlet weak var pineAppleAmount: UILabel!
+    @IBOutlet weak var pineappleAmount: UILabel!
     @IBOutlet weak var mangoAmount: UILabel!
+    
+    private func updateAmount() {
+        strawberryAmount.text = String(Fruit.strawberry.amount)
+        bananaAmount.text = String(Fruit.banana.amount)
+        kiwiAmount.text = String(Fruit.kiwi.amount)
+        pineappleAmount.text = String(Fruit.pineapple.amount)
+        mangoAmount.text = String(Fruit.mango.amount)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
 
 }
 
