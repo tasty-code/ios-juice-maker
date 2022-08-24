8281b49278b51021b77b1edbdb3c94bfb2d4e3e1
parkseyoon <issou06261@gmail.com>
2022-08-24 11:19:08 +0900

    feat: 재고 기본값 설정 및 재고 관리 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,17 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
     
+    func changeStock(strawberry: Int = 0, banana: Int = 0, pineapple: Int = 0, kiwi: Int = 0, mango: Int = 0) {
+        self.strawberry += strawberry
+        self.banana += banana
+        self.pineapple += pineapple
+        self.kiwi += kiwi
+        self.mango += mango
+    }
 }
