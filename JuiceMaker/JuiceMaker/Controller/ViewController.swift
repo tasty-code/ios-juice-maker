65f678610c0ddb4fead15b63694e6e07cbac864a
wormsJJG <jjr66461837@gmail.com>
2022-08-24 14:50:09 +0900

    orderButton 클래스 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var pineappleAmount: UILabel!
     @IBOutlet weak var mangoAmount: UILabel!
     
+    @IBOutlet weak var strawberryBananaOrderButton: orderButton!
+    @IBOutlet weak var strawberryOrderButton: orderButton!
+    @IBOutlet weak var BananaOrderButton: orderButton!
+    @IBOutlet weak var pineappleOrderButton: orderButton!
+    @IBOutlet weak var mangoKiwiOrderButton: orderButton!
+    @IBOutlet weak var kiwiOrderButton: orderButton!
+    @IBOutlet weak var mangoOrderButton: orderButton!
+    
     private func updateAmount() {
         strawberryAmount.text = String(Fruit.strawberry.amount)
         bananaAmount.text = String(Fruit.banana.amount)
@@ -22,9 +30,15 @@ class ViewController: UIViewController {
         mangoAmount.text = String(Fruit.mango.amount)
     }
     
+    private func settingOrderButton() {
+        
+    }
+    private func initialSetting() {
+        updateAmount()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateAmount()
+        initialSetting()
     }
 
     
---
+++ JuiceMaker/JuiceMaker/Extension/orderButton.swift
@@ -0,0 +1,13 @@
+//
+//  orderButton.swift
+//  JuiceMaker
+//
+//  Created by 정재근 on 2022/08/24.
+//
+
+import UIKit
+
+class orderButton: UIButton {
+    var juice: Juice = JuiceType.strawberry
+
+}
