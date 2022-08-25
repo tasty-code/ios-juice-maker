7465db6bbfcb6c735019e1cbece71f0b32d5c78d
Schro2109 <bloodparty297@gmail.com>
2022-08-25 15:34:59 +0900

    FruitStore의 재고관리 함수 미완성 상태로 추가 및 JuiceMaker의 쥬스 제작 함수 완성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -5,31 +5,32 @@
 //
 
 import Foundation
+import UIKit
 
 // 과일 저장소 타입
 class FruitStore {
-    class Storage {
-        private(set) var amount: Int = 10
-        
-        func plusAmount(to amount: Int) {
-            self.amount += amount
-        }
-        func minusAmount(to amount: Int) {
-            self.amount -= amount
-        }
-    }
     enum Fruit {
-//        static let strawberry = Storage()
-//        static let banana = Storage()
-//        static let pineapple = Storage()
-//        static let kiwi = Storage()
-//        static let mango = Storage()
-        
         case strawberry
         case banana
         case pineapple
         case kiwi
         case mango
+    }
+    
+    var fruits: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
+    //!!! pr 전 주석은 전부 지워주세요 FruitStore와 JuiceMaker 둘 다요 !!!
+    //
+    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾아 value를 반환하는 함수
+    func getAmount(of name: Fruit) -> Int {
+        //임시로 0을 반환하도록 했습니다. 함수 완성 후 결과값을 반환하도록 수정해주세요.
+        return 0
+    }
+    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾은 후 해당 과일의 갯수를 amount 만큼 증가시키는 함수
+    func increase(of name: Fruit, to amount: Int) {
+        
+    }
+    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾은 후 해당 과일의 갯수를 amount 만큼 감소시키는 함수
+    func decrease(of name: Fruit, to amount: Int) {
         
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,14 +8,6 @@ import Foundation
 // 쥬스 메이커 타입
 class JuiceMaker {
     enum Juice {
-//        static let strawberryJuice = ["strawberry": 16]
-//        static let bananaJuice = ["banana": 2]
-//        static let kiwiJuice = ["kiwi": 3]
-//        static let pineappleJuice = ["pineapple": 2]
-//        static let strawberryBananaJuice = ["strawberry": 10, "banana": 1]
-//        static let mangoJuice = ["mango": 3]
-//        static let mangoKiwiJuice = ["mango": 2, "kiwi": 1]
-        
         case strawberry
         case banana
         case kiwi
@@ -63,22 +55,34 @@ class JuiceMaker {
         }
     }
     
-    
-    func makeJuice(of optionalJuiceRecipe: [String: Int]?) {
-        guard let juiceRecipe = optionalJuiceRecipe else {
+    var fruitStore = FruitStore()
+    //쥬스 제작 시작 함수
+    func makeJuice(to menu: Juice) {
+        //재료가 충분하지 않을 시 함수가 종료됨
+        if isMakeAble(juice: menu) == false {
             return
         }
         
-        
-        
+        //재료를 소모하는 함수 실행
+        useIngredients(of: menu)
     }
-    
-    
-    func juiceMakeAble(recipes: Juice) {
-        if recipes.recipe
+    //지정한 메뉴를 만들 재료가 충분한지 반환하는 함수
+    func isMakeAble(juice: Juice) -> Bool {
+        for ingredients in juice.recipe {
+            let demandFruitType: FruitStore.Fruit = ingredients.key
+            
+            if fruitStore.getAmount(of: demandFruitType) < ingredients.value {
+                return false
+            }
+        }
+        
+        return true
     }
-   
-    
-   
-    
+    //지정한 메뉴의 재료를 소모하는 함수
+    func useIngredients(of menu: Juice) {
+        for ingredients in menu.recipe {
+            let demandFruitType: FruitStore.Fruit = ingredients.key
+            fruitStore.decrease(of: demandFruitType, to: ingredients.value)
+        }
+    }
 }
