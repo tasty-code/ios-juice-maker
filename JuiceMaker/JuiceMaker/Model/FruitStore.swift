f51364348b697d0877470799278352a7e4eeea93
이은호 <ieunho@ieunhoui-MacBookAir.local>
2022-08-26 16:02:54 +0900

    feat: 재고관리 함수인  getAmount함수 구현, 재고증가 increase함수 구현, 재고감소 decrease함수 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,9 +7,9 @@
 import Foundation
 import UIKit
 
-// 과일 저장소 타입
+
 class FruitStore {
-    enum Fruit {
+    enum Fruit: CaseIterable {
         case strawberry
         case banana
         case pineapple
@@ -18,19 +18,35 @@ class FruitStore {
     }
     
     var fruits: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
-    //!!! pr 전 주석은 전부 지워주세요 FruitStore와 JuiceMaker 둘 다요 !!!
-    //
-    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾아 value를 반환하는 함수
+    
     func getAmount(of name: Fruit) -> Int {
-        //임시로 0을 반환하도록 했습니다. 함수 완성 후 결과값을 반환하도록 수정해주세요.
+        for ingredient in fruits {
+            if name == ingredient.key {
+                return ingredient.value
+            }
+        }
         return 0
     }
-    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾은 후 해당 과일의 갯수를 amount 만큼 증가시키는 함수
+    
     func increase(of name: Fruit, to amount: Int) {
-        
+        for ingredinet in fruits {
+            var outDict: Int = ingredinet.value
+            
+            if name == ingredinet.key {
+                outDict += amount
+            }
+            fruits[name] = outDict
+        }
     }
-    //미완성 : 매개변수 name으로 받아온 항목을 fruits 딕셔너리에서 찾은 후 해당 과일의 갯수를 amount 만큼 감소시키는 함수
+   
     func decrease(of name: Fruit, to amount: Int) {
-        
+        for ingredinet in fruits {
+            var outDict: Int = ingredinet.value
+            
+            if name == ingredinet.key {
+                outDict -= amount
+            }
+            fruits[name] = outDict
+        }
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,7 +5,7 @@
 // 
 
 import Foundation
-// 쥬스 메이커 타입
+
 class JuiceMaker {
     enum Juice {
         case strawberry
@@ -56,17 +56,15 @@ class JuiceMaker {
     }
     
     var fruitStore = FruitStore()
-    //쥬스 제작 시작 함수
+    
     func makeJuice(to menu: Juice) {
-        //재료가 충분하지 않을 시 함수가 종료됨
         if isMakeAble(juice: menu) == false {
             return
         }
         
-        //재료를 소모하는 함수 실행
         useIngredients(of: menu)
     }
-    //지정한 메뉴를 만들 재료가 충분한지 반환하는 함수
+    
     func isMakeAble(juice: Juice) -> Bool {
         for ingredients in juice.recipe {
             let demandFruitType: FruitStore.Fruit = ingredients.key
@@ -78,7 +76,7 @@ class JuiceMaker {
         
         return true
     }
-    //지정한 메뉴의 재료를 소모하는 함수
+    
     func useIngredients(of menu: Juice) {
         for ingredients in menu.recipe {
             let demandFruitType: FruitStore.Fruit = ingredients.key
