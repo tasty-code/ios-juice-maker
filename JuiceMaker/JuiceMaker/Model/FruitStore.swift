3dfbd44c206eecc139849681d3b6479c3aa058dd
moonsoungjin <seongjinmun74@gmail.com>
2022-08-26 15:35:04 +0900

    test

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,20 +8,38 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    var strawberry : Int
-    var banana : Int
-    var pineapple : Int
-    var kiwi : Int
-    var mango : Int
-    var change: Int
-    
-    init() {
-        strawberry = 10
-        banana = 10
-        pineapple = 10
-        kiwi = 10
-        mango = 10
-        change = 0
+//    var strawberry : Int
+//    var banana : Int
+//    var pineapple : Int
+//    var kiwi : Int
+//    var mango : Int
+//    var change: Int
+//
+//    init() {
+//        strawberry = 10
+//        banana = 10
+//        pineapple = 10
+//        kiwi = 10
+//        mango = 10
+//        change = 0
+//    }
+  
+    enum Fruit {
+        case strawberry, banana, pineapple, kiwi, mango
+        
+        var name: String {
+            switch self {
+            case .strawberry:
+                return "딸기"
+            case .banana:
+                return "바나나"
+            case .pineapple:
+                return "파인애플"
+            case .kiwi:
+                return "키위"
+            case .mango:
+                return "망고"
+            }
+        }
     }
-    
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,54 +10,5 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    var fruitStore = FruitStore()
-    
-    func exceedNumberOfFruits(fruit: Int, num: Int) -> Void {
-        if fruit < num {
-            print("과일을 제조할 수 없습니다.")
-        }
-    }
-    
-    func makeJuice() {
-        guard let choiceJuice: String = readLine() else{
-            return
-        }
-        
-        var strawberrys = fruitStore.strawberry + fruitStore.change
-        var bananas = fruitStore.banana + fruitStore.change
-        var pineapples = fruitStore.pineapple + fruitStore.change
-        var kiwis = fruitStore.kiwi + fruitStore.change
-        var mangos = fruitStore.mango + fruitStore.change
-
-        switch choiceJuice {
-        case "strawberry":
-            strawberrys = strawberrys - 16
-            exceedNumberOfFruits(fruit: strawberrys, num: 16)
-        case "banana":
-            bananas = bananas - 2
-            exceedNumberOfFruits(fruit: bananas, num: 2)
-        case "kiwi":
-            kiwis = kiwis - 3
-            exceedNumberOfFruits(fruit: kiwis, num: 3)
-        case "strawbanana":
-            strawberrys = strawberrys - 10
-            bananas = bananas - 1
-            if strawberrys < 10 && bananas < 1 {
-                print("과일을 제조할 수 없습니다.")
-            }
-        case "pineapple":
-            pineapples = pineapples - 2
-            exceedNumberOfFruits(fruit: pineapples, num: 2)
-        case "mango":
-            mangos = mangos - 3
-            exceedNumberOfFruits(fruit: kiwis, num: 3)
-        case "mangokiwi":
-            mangos = mangos - 3
-            kiwis = kiwis - 3
-            if mangos < 2 && kiwis < 1 {
-                print("과일을 제조할 수 없습니다.")
-            }
-        default: break
-        }
-    }
+   
 }
