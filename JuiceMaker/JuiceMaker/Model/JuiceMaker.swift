3df926229d77fc7406cd22f5bcd1252cd2ba840b
에라 <jyh@babygo.kr>
2022-08-29 11:35:13 +0900

    feat: 주스 이름 enum추가 및 FruitStore class 사용

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,18 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
+enum JuiceName: String {
+    case 딸기쥬스
+    case 바나나쥬스
+    case 키위쥬스
+    case 파인애플_쥬스 = "파인애플 쥬스"
+    case 딸바쥬스
+    case 망고_쥬스 = "망고 쥬스"
+    case 망고키위_쥬스 = "망고키위 쥬스"
+}
+
 struct JuiceMaker {
-    
+    var fruitStore = FruitStore()
+
+    init () {}
 }
