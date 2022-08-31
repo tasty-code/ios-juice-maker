81b127c2359fd558808907880138b765567e8934
에라 <jyh@babygo.kr>
2022-08-31 12:13:05 +0900

    feat: 각 뷰 컨트롤러 생성 및 연결

---
+++ JuiceMaker/JuiceMaker/Controller/FruitManagementController.swift
@@ -0,0 +1,18 @@
+//
+//  FruitManagementController.swift
+//  JuiceMaker
+//
+//  Created by 정연호 on 2022/08/31.
+//
+
+import UIKit
+
+class FruitManagementController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ JuiceMaker/JuiceMaker/Controller/MakeJuiceController.swift
@@ -0,0 +1,18 @@
+//
+//  MakeJuiceController.swift
+//  JuiceMaker
+//
+//  Created by 정연호 on 2022/08/31.
+//
+
+import UIKit
+
+class MakeJuiceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
