b47bec23e49adf50a08124d37b11d96cf3dcca89
에라 <jyh@babygo.kr>
2022-08-31 13:24:19 +0900

    feat: 과일 갯수 라벨 업데이트

--- JuiceMaker/JuiceMaker/Controller/MakeJuiceController.swift
+++ JuiceMaker/JuiceMaker/Controller/MakeJuiceController.swift
@@ -14,10 +14,22 @@ class MakeJuiceController: UIViewController {
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        updateFluitLabel()
+    }
 
-
+    @IBAction func updateFluitLabel() {
+        strawberryLabel.text = String((try? juiceMaker.getQuantity(of: .strawberry)) ?? 0)
+        bananaLabel.text = String((try? juiceMaker.getQuantity(of: .banana)) ?? 0)
+        pineappleLabel.text = String((try? juiceMaker.getQuantity(of: .pineapple)) ?? 0)
+        kiwiLabel.text = String((try? juiceMaker.getQuantity(of: .kiwi)) ?? 0)
+        mangoLabel.text = String((try? juiceMaker.getQuantity(of: .mango)) ?? 0)
+    }
 }
