194c809de67ca37004b9764edc22a41df2e53d70
박도원 <ddowon@bagdowon-ui-MacBookAir.local>
2022-08-31 10:11:05 +0900

    feat: 과일의 n개를 변경하는 increase, decrease 함수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -5,16 +5,17 @@
 //
 
 import Foundation
+import UIKit
 
 // 과일 저장소 타입
 class FruitStore {
-    var strawberry : Int = 10
-    var banana : Int = 10
-    var pineapple : Int = 10
-    var kiwi : Int = 10
-    var mango : Int = 10
-  
-    enum Fruit {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
+    
+    enum Fruit: CaseIterable {
         case strawberry, banana, pineapple, kiwi, mango
         
         var name: String {
@@ -32,4 +33,38 @@ class FruitStore {
             }
         }
     }
+    let stocks = Fruit.allCases
+    
+    func increase(change: Int = 0) {
+        for stock in stocks {
+            if stock == .strawberry {
+                strawberry += change
+            } else if stock == .banana {
+                banana += change
+            } else if stock == .pineapple {
+                pineapple += change
+            } else if stock == .kiwi {
+                kiwi += change
+            } else if stock == .mango {
+                mango += change
+            }
+        }
+    }
+
+    func decrease(change: Int = 0) {
+        for stock in stocks {
+            if stock == .strawberry {
+                strawberry -= change
+            } else if stock == .banana {
+                banana -= change
+            } else if stock == .pineapple {
+                pineapple -= change
+            } else if stock == .kiwi {
+                kiwi -= change
+            } else if stock == .mango {
+                mango -= change
+            }
+        }
+    }
 }
+
