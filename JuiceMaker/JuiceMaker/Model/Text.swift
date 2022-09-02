c61a95d1e9196d7745695d803fd9c1104b09f5c3
에라 <jyh@babygo.kr>
2022-09-02 14:22:32 +0900

    style: Text관리 파일 추가

---
+++ JuiceMaker/JuiceMaker/Model/Text.swift
@@ -0,0 +1,20 @@
+//
+//  Text.swift
+//  JuiceMaker
+//
+//  Created by 정연호 on 2022/09/02.
+//
+
+import Foundation
+
+enum JuiceMakerText {
+    static let noneFruit = "없는 과일입니다."
+    static let lackOfFruit = "재료가 모자라요. 재고를 수정할까요?"
+    static let makeComplet = " 나왔습니다! 맛있게 드세요!"
+}
+
+enum JuiceMakerViewControllerText {
+    static let yes = "예"
+    static let no = "아니요"
+    static let confirm = "확인"
+}
