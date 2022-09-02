4c1760b4d3100dd5cf95966c96adfdc1aac8493e
박도원 <ddowon@bagdowon-ui-MacBookAir.local>
2022-09-02 14:08:55 +0900

    feat: fruitStore 타입 설계

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,62 +9,38 @@ import UIKit
 
 // 과일 저장소 타입
 class FruitStore {
-    var strawberry: Int = 10
-    var banana: Int = 10
-    var pineapple: Int = 10
-    var kiwi: Int = 10
-    var mango: Int = 10
+    enum Fruit: Int, CaseIterable {
+        case strawberry, banana, pineapple, kiwi, mango 
+    }
     
-    enum Fruit: CaseIterable {
-        case strawberry, banana, pineapple, kiwi, mango
-        
-        var name: String {
-            switch self {
-            case .strawberry:
-                return "딸기"
-            case .banana:
-                return "바나나"
-            case .pineapple:
-                return "파인애플"
-            case .kiwi:
-                return "키위"
-            case .mango:
-                return "망고"
+    var fruits = [Fruit: Int]()
+    
+    init(remainingStock: Int) {
+        for fruit in Fruit.allCases {
+            fruits[fruit] = remainingStock
+        }
+    }
+    
+    func increase(fruit: Fruit, stockChange: Int) {
+        for remainingStock in fruits {
+            var fruitsValue = remainingStock.value
+            if fruit == remainingStock.key {
+                fruitsValue += stockChange
+                fruits[fruit] = fruitsValue
             }
         }
     }
-    let stocks = Fruit.allCases
     
-    func increase(change: Int = 0) {
-        for stock in stocks {
-            if stock == .strawberry {
-                strawberry += change
-            } else if stock == .banana {
-                banana += change
-            } else if stock == .pineapple {
-                pineapple += change
-            } else if stock == .kiwi {
-                kiwi += change
-            } else if stock == .mango {
-                mango += change
-            }
-        }
-    }
-
-    func decrease(change: Int = 0) {
-        for stock in stocks {
-            if stock == .strawberry {
-                strawberry -= change
-            } else if stock == .banana {
-                banana -= change
-            } else if stock == .pineapple {
-                pineapple -= change
-            } else if stock == .kiwi {
-                kiwi -= change
-            } else if stock == .mango {
-                mango -= change
+    func decrease(fruit: Fruit, stockChange: Int) {
+        for remainingStock in fruits {
+            var fruitsValue = remainingStock.value
+            if fruit == remainingStock.key {
+                fruitsValue -= stockChange
+                fruits[fruit] = fruitsValue
             }
         }
     }
 }
+let fruitStore = FruitStore(remainingStock: 10)
+
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,11 +5,54 @@
 // 
 
 import Foundation
+import UIKit
 
 
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    let fruitStore = FruitStore()
-
+    
+    enum FruitJuice {
+        case strawberryJuice, bananaJuice, pineappleJuice, kiwiJuice, mangoJuice, strawbananaJuice, mankiJuice
+        
+        var name: String {
+            switch self {
+            case .strawberryJuice:
+                return "딸기주스"
+            case .bananaJuice:
+                return "바나나주스"
+            case .pineappleJuice:
+                return "파인애플주스"
+            case .kiwiJuice:
+                return "키위주스"
+            case .mangoJuice:
+                return "망고주스"
+            case .strawbananaJuice:
+                return "딸바주스"
+            case .mankiJuice:
+                return "망키주스"
+            }
+        }
+        
+        var recipe: [FruitStore.Fruit: Int] {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .strawbananaJuice:
+                return [.strawberry: 10, .banana: 1]
+            case .mankiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
+    
+    
 }
