5a605d10ffd674682cfd94c1d885722e2bbbe465
longlivedrgn <kimye9808@naver.com>
2023-01-02 17:05:05 +0900

    feat: 쥬스 만들기 시 재고 확인 로직 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -11,8 +11,21 @@ struct JuiceMaker {
     private let fruitStore = FruitStore()
     
     func makeJuice(of fruitJuice: FruitJuice) {
+        guard hasEnoughStock(for: fruitJuice) else {
+            return print("쥬스를 만들기 위한 재고가 부족합니다.")
+        }
+        
         fruitJuice.recipe.forEach { fruit, numberOfUse in
             fruitStore.changeStock(of: fruit, by: -numberOfUse)
+        }
+    }
+    
+    private func hasEnoughStock(for fruitJuice: FruitJuice) -> Bool {
+        fruitJuice.recipe.allSatisfy { fruit, numberOfUse in
+            guard let fruitStock = fruitStore.stock[fruit] else {
+                return false
+            }
+            return fruitStock >= numberOfUse
         }
     }
 }
