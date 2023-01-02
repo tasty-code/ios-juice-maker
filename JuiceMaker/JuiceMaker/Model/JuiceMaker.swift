252062199776c4800aeb7c56394b7e8f780f6e0d
qwerty3345 <qwerty3345@naver.com>
2023-01-02 16:53:24 +0900

    feat: 쥬스 만들기 기능 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,11 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private let fruitStore = FruitStore()
     
+    func makeJuice(of fruitJuice: FruitJuice) {
+        fruitJuice.recipe.forEach { fruit, numberOfUse in
+            fruitStore.changeStock(of: fruit, by: -numberOfUse)
+        }
+    }
 }
