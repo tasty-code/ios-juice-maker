1d966f074b940cb490cdaeef7fb2cfd8ddc434de
qwerty3345 <qwerty3345@naver.com>
2023-01-02 16:10:07 +0900

    feat: changeStock 함수를 통한 과일 재고 변경 기능 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,12 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     private(set) var stock: [Fruit: Int] = [:]
+    
+    func changeStock(of fruit: Fruit, by number: Int) {
+        guard let result = stock[fruit] else {
+            return
+        }
+        
+        stock[fruit] = result + number
+    }
 }
