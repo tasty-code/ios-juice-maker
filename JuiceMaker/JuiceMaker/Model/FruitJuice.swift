1540191c0bfc852d37d0db50be8a161c3fc99747
longlivedrgn <kimye9808@naver.com>
2023-01-02 16:30:01 +0900

    feat: 과일 쥬스 enum Model 구현 및 제조 recipe 지정

---
+++ JuiceMaker/JuiceMaker/Model/FruitJuice.swift
@@ -0,0 +1,37 @@
+//
+//  FruitJuice.swift
+//  JuiceMaker
+//
+//  Created by 김용재 on 2023/01/02.
+//
+
+import Foundation
+
+enum FruitJuice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mango
+    case mangoKiwi
+    
+    var recipe: [Fruit: Int] {
+        switch self {
+        case .strawberry:
+            return [.strawberry: 16]
+        case .banana:
+            return [.banana: 2]
+        case .kiwi:
+            return [.kiwi: 3]
+        case .pineapple:
+            return [.pineapple: 2]
+        case .strawberryBanana:
+            return [.strawberry: 10, .banana: 1]
+        case .mango:
+            return [.mango: 3]
+        case .mangoKiwi:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
+}
