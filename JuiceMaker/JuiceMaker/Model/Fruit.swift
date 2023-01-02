209da846970d039ee3684d0b98ab37d5770e9240
Sei Kim <vanism7557@gmail.com>
2023-01-02 16:45:06 +0900

    feat. Fruit enum 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by sei_dev on 1/2/23.
+//
+
+import Foundation
+
+enum Fruit {
+    case 딸기
+    case 바나나
+    case 파인애플
+    case 키위
+    case 망고
+}
