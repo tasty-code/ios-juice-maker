e83b947212c730df915684b02d025014b1475e18
devxsby <devxsby@gmail.com>
2023-01-03 10:42:03 +0900

    [feat] 쥬스 메이커, 과일 저장소 전체 틀 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,6 +7,32 @@
 import Foundation
 
 // 과일 저장소 타입
+
+/// singleton 적용하기
+
 class FruitStore {
+    
+    static var shared = FruitStore()
+    
+    // TODO:- 열거형들 재고 딕셔너리 만들기 [key: value] = [fruit: Int]
+    enum Fruits: CaseIterable {
+        case strawberries
+        case bananas
+        case kiwis
+        case pineapples
+        case mangos
+    }
+    
+    // 재고 확인용
+    func checkStock() {}
+    
+    // 과일 재고 n개 증가
+    func increase() {}
+    
+    // 과일 재고 n개 감소
+    func decrease() {}
+    
+    // TODO:- init에 기본값 넣기, 10개씩 초기화
+    private init() {}
     
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,4 +9,24 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
+    private let fruitStore = FruitStore.shared
+    
+    enum Juice {
+        case strawberry
+        case banana
+        case kiwi
+        case pineapple
+        case strawberryBanana
+        case mango
+        case mangoKiwi
+        
+        // TODO:- 레시피 적기(과일 필요한 개수)
+        
+    }
+    
+    // 쥬스 제조 -> 과일 저장소 재고 확인을 먼저하고, 재고 감소를 호출함
+    func startBlending () {
+        fruitStore.decrease()
+    }
+    
 }
