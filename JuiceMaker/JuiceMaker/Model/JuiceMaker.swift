801847860b0189b69743263a58e391d6a63104aa
ohdair <ohdiar@gmail.com>
2023-01-03 15:50:09 +0900

    feat: 쥬스 주문 함수 구현
    - order 함수, make 함수 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,35 +6,16 @@
 
 import Foundation
 
-enum Menu {
-    case strawberryJuice
-    case bananaJuice
-    case kiwiJuice
-    case pineappleJuice
-    case strawberryBananaJuice
-    case mangoJuice
-    case mangoKiwiJuice
+struct JuiceMaker {
+    let fruitStore = FruitStore.shared
 
-    var recipe: [Fruits: Quantity] {
-        switch self {
-        case .strawberryJuice:
-            return [.strawberry: 16]
-        case .bananaJuice:
-            return [.banana: 2]
-        case .kiwiJuice:
-            return [.kiwi: 3]
-        case .pineappleJuice:
-            return [.pineapple: 2]
-        case .strawberryBananaJuice:
-            return [.strawberry: 10, .banana: 1]
-        case .mangoJuice:
-            return [.mango: 3]
-        case .mangoKiwiJuice:
-            return [.mango: 2, .kiwi: 1]
+    func order(juice: Menu) {
+        make(juice: juice)
+    }
+
+    func make(juice order: Menu) {
+        order.recipe.forEach { (fruit, quantity) in
+            fruitStore.remove(fruit: fruit, quantity: quantity)
         }
     }
-}
-
-struct JuiceMaker {
-
 }
