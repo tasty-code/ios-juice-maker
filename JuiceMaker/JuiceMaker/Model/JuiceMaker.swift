c2855e0e1ce80714af43761be4be3e443fb83bae
devxsby <devxsby@gmail.com>
2023-01-03 13:19:18 +0900

    [feat] 쥬스 enum에 이름과 레시피 변수 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -20,7 +20,43 @@ struct JuiceMaker {
         case mango
         case mangoKiwi
         
-        // TODO:- 레시피 적기(과일 필요한 개수)
+        var name: String {
+            switch self {
+            case .strawberry:
+                return "딸기쥬스"
+            case .banana:
+                return "바나나쥬스"
+            case .kiwi:
+                return "키위쥬스"
+            case .pineapple:
+                return "파인애플쥬스"
+            case .strawberryBanana:
+                return "딸바쥬스"
+            case .mango:
+                return "망고쥬스"
+            case .mangoKiwi:
+                return "망키쥬스"
+            }
+        }
+        
+        var receipe: [FruitStore.Fruits: Int] {
+            switch self {
+            case .strawberry:
+                return [.strawberries: 16]
+            case .banana:
+                return [.bananas: 2]
+            case .kiwi:
+                return [.kiwis: 3]
+            case .pineapple:
+                return [.pineapples: 2]
+            case .strawberryBanana:
+                return [.strawberries: 10, .bananas: 1]
+            case .mango:
+                return [.mangos: 3]
+            case .mangoKiwi:
+                return [.mangos: 2, .kiwis: 1]
+            }
+        }
         
     }
     
