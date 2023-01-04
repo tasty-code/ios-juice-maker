01f4af3e5fe57d222fe37867be88cdba00c482f5
Jason <ggj00125@gmail.com>
2023-01-04 13:42:32 +0900

    Refactor: 재고의 여부를 확인하고 진행가능하면 주스를 만들고 남은 갯수를 업데이트하는 메서드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -17,32 +17,88 @@ struct JuiceMaker: Makeable {
     }
     
     func requestTo(strawberry juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.strawberry] else{
+        guard let currentStock = fruitStore.store[.strawberry] else {
             return
         }
+        
         if juice == FruitList.strawberry {
-            let calculationResult = calculator.subtract(number1: currentStock, number2: number)
-            guard calculationResult >= 0 else{
+            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
+            let determine = fruitStore.isPossibleMakeSingle(juice: .strawberry, stockNumber: currentStock)
+            
+            if determine == true {
+                print("딸기 재고 없음...")
                 return
             }
-            fruitStore.store.updateValue(calculator.subtract(number1: calculationResult, number2: number), forKey: .strawberry)
+            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.strawberry)
         }
     }
     
     func requestTo(banana juice: FruitList, number: Int) {
-        fruitStore.store.updateValue(number, forKey: juice)
+        guard let currentStock = fruitStore.store[.banana] else {
+            return
+        }
+        
+        if juice == FruitList.banana {
+            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
+            let determine = fruitStore.isPossibleMakeSingle(juice: .banana, stockNumber: currentStock)
+            
+            if determine == true {
+                print("바나나 재고 없음...")
+                return
+            }
+            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.banana)
+        }
     }
     
     func requestTo(kiwi juice: FruitList, number: Int) {
-        fruitStore.store.updateValue(number, forKey: juice)
+        guard let currentStock = fruitStore.store[.kiwi] else {
+            return
+        }
+        
+        if juice == FruitList.kiwi {
+            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
+            let determine = fruitStore.isPossibleMakeSingle(juice: .kiwi, stockNumber: currentStock)
+            
+            if determine == true {
+                print("키위 재고 없음...")
+                return
+            }
+            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.kiwi)
+        }
     }
     
     func requestTo(pineApple juice: FruitList, number: Int) {
-        fruitStore.store.updateValue(number, forKey: juice)
+        guard let currentStock = fruitStore.store[.pineApple] else {
+            return
+        }
+        
+        if juice == FruitList.pineApple {
+            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
+            let determine = fruitStore.isPossibleMakeSingle(juice: .pineApple, stockNumber: currentStock)
+            
+            if determine == true {
+                print("파인애플 재고 없음...")
+                return
+            }
+            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.pineApple)
+        }
     }
     
     func requestTo(mango juice: FruitList, number: Int) {
-        fruitStore.store.updateValue(number, forKey: juice)
+        guard let currentStock = fruitStore.store[.mango] else {
+            return
+        }
+        
+        if juice == FruitList.mango {
+            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
+            let determine = fruitStore.isPossibleMakeSingle(juice: .mango, stockNumber: currentStock)
+            
+            if determine == true {
+                print("망고 재고 없음...")
+                return
+            }
+            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.mango)
+        }
     }
     
     func requestTo(mixed juice: (FruitList, FruitList), numbers: (Int, Int)) -> Int {
