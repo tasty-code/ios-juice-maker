bd8d08ee4a21d8c250a15c48f54c28860247d21c
Jason <ggj00125@gmail.com>
2023-01-04 10:50:44 +0900

    Feat: Makeable 프로토콜을 구현
    - JuiceMaker에 채택할 Makeable 구현
    - UI에서 들어오는 주문버튼에 맞춰 구현해야할 메서드를 프로토콜에 작성

---
+++ JuiceMaker/JuiceMaker/Model/Protocol/Computable.swift
@@ -0,0 +1,13 @@
+//
+//  Computable.swift
+//  JuiceMaker
+//
+//  Created by Jason on 2023/01/04.
+//
+
+import Foundation
+
+protocol Computable {
+    func add(number1: Int, number2: Int) -> Int
+    func subtract(number1: Int, number2: Int) -> Int
+}
