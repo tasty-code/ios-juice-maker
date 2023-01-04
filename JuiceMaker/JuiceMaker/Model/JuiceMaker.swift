996050ed13ec2ee8fa560e76181bd806f35ae7b9
lust3r <s1skyblue1s@gmail.com>
2023-01-04 10:40:21 +0900

    Feat: Singleton FruitStore를 fruitStore에 할당 및 단일 주스를 만드는 메서드 requestToMakeJuice 정의
    fruitStore.shared를 통해 공유자원에 접근하여 재고관리의 원활함을 주려고 fruitStore를 상수선언 함
    requestToMakeJuice를 통해 과일 이름과 소모할 개수를 전달받아 기존 store 딕셔너리의 값을 변경할 수 있게 함

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,16 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore = FruitStore.shared
     
+    func requestToMakeJuice(fruitName: FruitList, numToConsume: Int){
+        guard var currentStock = fruitStore.store[fruitName] else{
+            return
+        }
+        guard currentStock - numToConsume >= 0 else {
+            return
+        }
+        currentStock -= numToConsume
+        fruitStore.store[fruitName] = currentStock
+    }
 }
