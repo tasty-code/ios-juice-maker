0c2b8bff288877ebad29b1192e35e5703f11a428
Jason <ggj00125@gmail.com>
2023-01-04 10:56:07 +0900

    Feat: JuiceMaker에 채택할 Protocol 구현
    - JuiceMaker에 채택할 Makeable 구현
    - UI에서 들어오는 주문버튼에 맞춰 구현해야할 메서드를 프로토콜에 작성

---
+++ JuiceMaker/JuiceMaker/Model/Protocol/Makeable.swift
@@ -0,0 +1,17 @@
+//
+//  Makeable.swift
+//  JuiceMaker
+//
+//  Created by Jason on 2023/01/04.
+//
+
+import Foundation
+
+protocol Makeable {
+    func requestTo(strawberry juice: FruitList, number: Int)
+    func requestTo(banana juice: FruitList, number: Int)
+    func requestTo(kiwi juice: FruitList, number: Int)
+    func requestTo(pineAplle juice: FruitList, number: Int)
+    func requestTo(mango juice: FruitList, number: Int)
+    func requestTo(mixed juice: (FruitList, FruitList), numbers: (Int, Int)) -> Int
+}
