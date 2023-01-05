d4bb97c82f1ad399ea69c7c065cb4244bffc6af4
zhunhe <xuzhunhe@gmail.com>
2023-01-05 09:57:28 +0900

    feat: JuiceMaker에 FruitStore 인스턴스 생성, 과일쥬스 레시피 작성
    - 레시피: 딸기, 바나나, 키위, 파인애플, 딸바, 망고, 망키

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,28 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+    let fruitStore = FruitStore()
+
+    enum Juice {
+        case strawberry, banana, kiwi, pineapple, strawberryBanana, mango, mangoKiwi
+
+        var recipe: [FruitStore.Fruit: UInt] {
+            switch self {
+            case .strawberry:
+                return [.strawberry: 16]
+            case .banana:
+                return [.banana: 2]
+            case .kiwi:
+                return [.kiwi: 3]
+            case .pineapple:
+                return [.pineapple: 2]
+            case .strawberryBanana:
+                return [.strawberry: 10, .banana: 1]
+            case .mango:
+                return [.mango: 3]
+            case .mangoKiwi:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
 }
