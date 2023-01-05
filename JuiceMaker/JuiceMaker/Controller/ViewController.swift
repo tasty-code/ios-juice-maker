36f1ff43b574bf3cde36d65d91f8b32ce0ae9dff
iYeahh <yboravender@gmail.com>
2023-01-05 16:27:13 +0900

    chore: Storyboard의 버튼과 레이블을 ViewController에 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func orderButton(_ sender: UIButton) {
+    }
 }
 
