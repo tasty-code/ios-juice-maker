3f015b3985cedc9c0789061d0c8dae92b83cf7d0
iYeahh <yboravender@gmail.com>
2023-01-06 11:10:02 +0900

    refactor: 재고를 나타내는 레이블을 IBOutlet 컬렉션으로 변경

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,19 +8,14 @@ import UIKit
 
 class ViewController: UIViewController {
     let juiceMaker = JuiceMaker()
-    @IBOutlet weak var strawberryStock: UILabel!
-    @IBOutlet weak var bananaStock: UILabel!
-    @IBOutlet weak var pineappleStock: UILabel!
-    @IBOutlet weak var kiwiStock: UILabel!
-    @IBOutlet weak var mangoStock: UILabel!
+
+    @IBOutlet var stockLabels: [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        strawberryStock.text = String(FruitStore.defaultStock)
-        bananaStock.text = String(FruitStore.defaultStock)
-        pineappleStock.text = String(FruitStore.defaultStock)
-        kiwiStock.text = String(FruitStore.defaultStock)
-        mangoStock.text = String(FruitStore.defaultStock)
+        for stock in stockLabels {
+            stock.text = String(FruitStore.defaultStock)
+        }
     }
     
     @IBAction func orderButton(_ sender: UIButton) {
