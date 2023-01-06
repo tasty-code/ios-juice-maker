368528db1fdb087051e9810b1ad90ecbfce9277b
iYeahh <yboravender@gmail.com>
2023-01-06 15:14:08 +0900

    feat: 얼럿 생성과 화면이동 구현
    - 재고 수정 버튼 터치시 재고 수정 화면으로 이동
    - 재고가 있는 경우 "*쥬스 나왔습니다! 맛있게 드세요!" 얼럿 표시
    - 재고가 없는 경우 "재로가 모자라요. 재고를 수정할까요?" 얼럿 표시
      - '예' 선택시 재고수정화면으로 이동
      - '아니오' 선택시 얼럿 닫기

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -35,9 +35,22 @@ class ViewController: UIViewController {
                         throw JuiceMakerError.notExistFruit
                     }
                     stockLabels[fruit.rawValue].text = String(fruitStock)
+                    let alert = UIAlertController(title: nil, message: "\(juiceName) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .cancel, handler : nil)
+                    alert.addAction(okAction)
+                    present(alert, animated: false, completion: nil)
                 }
             } catch {
                 print(error.localizedDescription)
+                let alert = UIAlertController(title: nil, message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "예", style: .default, handler : {_ in
+                    guard let addStockVC = self.storyboard?.instantiateViewController(withIdentifier: "addStockVC") else { return }
+                    self.navigationController?.pushViewController(addStockVC, animated: true)
+                })
+                alert.addAction(okAction)
+                let noAction = UIAlertAction(title: "아니오", style: .default, handler : nil)
+                alert.addAction(noAction)
+                present(alert, animated: false, completion: nil)
             }
         }
     }
