5f46a4c8bb0846919084806aa1cd0ebe90f1772a
ueunli <cdenen20@gmail.com>
2023-01-06 14:05:18 +0900

    [feat] 스토리보드 오토레이아웃 기초설정 및 뷰컨트롤러 연결

--- JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
@@ -1,12 +1,12 @@
 //
-//  JuiceMaker - ViewController.swift
+//  JuiceMaker - OrderViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+class OrderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMaker - StoreViewController.swift
+//
+//  Created by J.E on 2023/01/06.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
