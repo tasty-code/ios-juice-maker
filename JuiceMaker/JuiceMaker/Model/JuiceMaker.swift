a1fe60d2ed595dfa3d7042544c929d638919de77
lust3r <s1skyblue1s@gmail.com>
2023-01-06 13:13:58 +0900

    Feat: JuiceMaker에 startMake메서드를 통해 모든 주스 제조를 할 수 있도록 구현
    - single, mix로 나누어 제조할 수 있도록 메서드 분리

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,102 +9,48 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker: Makeable {
     let fruitStore = FruitStore.shared
-    
     let calculator: Computable
     
     init(calculator: Computable) {
         self.calculator = calculator
     }
     
-    func requestTo(strawberry juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.strawberry] else {
-            return
-        }
-        
-        if juice == FruitList.strawberry {
-            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
-            let determine = fruitStore.isPossibleMakeSingle(juice: .strawberry, stockNumber: currentStock)
-            
-            if determine == true {
-                print("딸기 재고 없음...")
-                return
-            }
-            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.strawberry)
-        }
-    }
-    
-    func requestTo(banana juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.banana] else {
-            return
-        }
-        
-        if juice == FruitList.banana {
-            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
-            let determine = fruitStore.isPossibleMakeSingle(juice: .banana, stockNumber: currentStock)
-            
-            if determine == true {
-                print("바나나 재고 없음...")
-                return
-            }
-            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.banana)
+    func startMake(single juice: FruitSingleJuice) {
+        switch juice {
+        case .strawberry:
+            requestTo(single: .strawberry)
+            break
+        case .banana:
+            requestTo(single: .banana)
+            break
+        case .kiwi:
+            requestTo(single: .kiwi)
+            break
+        case .pineApple:
+            requestTo(single: .pineApple)
+            break
+        case .mango:
+            requestTo(single: .mango)
+            break
         }
     }
     
-    func requestTo(kiwi juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.kiwi] else {
-            return
-        }
-        
-        if juice == FruitList.kiwi {
-            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
-            let determine = fruitStore.isPossibleMakeSingle(juice: .kiwi, stockNumber: currentStock)
-            
-            if determine == true {
-                print("키위 재고 없음...")
-                return
-            }
-            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.kiwi)
+    func startMake(mix juice: FruitMixJuice) {
+        switch juice {
+        case .strawberryAndBanana:
+            requestTo(mix: .strawberryAndBanana)
+            break
+        case .mangoAndKiwi:
+            requestTo(mix: .mangoAndKiwi)
+            break
         }
     }
     
-    func requestTo(pineApple juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.pineApple] else {
-            return
-        }
-        
-        if juice == FruitList.pineApple {
-            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
-            let determine = fruitStore.isPossibleMakeSingle(juice: .pineApple, stockNumber: currentStock)
-            
-            if determine == true {
-                print("파인애플 재고 없음...")
-                return
-            }
-            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.pineApple)
-        }
+    func requestTo(single juice: FruitSingleJuice) {
+        <#code#>
     }
     
-    func requestTo(mango juice: FruitList, number: Int) {
-        guard let currentStock = fruitStore.store[.mango] else {
-            return
-        }
-        
-        if juice == FruitList.mango {
-            let remainStrawberryCount = calculator.subtract(number1: currentStock, number2: number)
-            let determine = fruitStore.isPossibleMakeSingle(juice: .mango, stockNumber: currentStock)
-            
-            if determine == true {
-                print("망고 재고 없음...")
-                return
-            }
-            fruitStore.store.updateValue(remainStrawberryCount, forKey: FruitList.mango)
-        }
-    }
-    
-    func requestTo(mixed juice: (FruitList, FruitList), numbers: (Int, Int)) -> Int {
-        fruitStore.store.updateValue(numbers.0, forKey: juice.0)
-        fruitStore.store.updateValue(numbers.1, forKey: juice.1)
-        
-        return 0
+    func requestTo(mix juice: FruitMixJuice) {
+        <#code#>
     }
 }
