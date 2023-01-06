dd99dd2bc897563d5b5b11070b51d964796433b9
bomyuniverse <bomi@soongsil.ac.kr>
2023-01-06 15:12:51 +0900

    feat: 쥬스 만들기 화면의 초기 과일 재고 표시기능 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,9 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUI()
+    }
+    
+    func setUI() {
+        strawberryLabel.text = String(FruitStore.shared.fruits[.strawberry]?.stock ?? 0)
+        bananaLabel.text = String(FruitStore.shared.fruits[.banana]?.stock ?? 0)
+        pineappleLabel.text = String(FruitStore.shared.fruits[.pineapple]?.stock ?? 0)
+        kiwiLabel.text = String(FruitStore.shared.fruits[.kiwi]?.stock ?? 0)
+        mangoLabel.text = String(FruitStore.shared.fruits[.mango]?.stock ?? 0)
     }
     
     @IBAction func EditStockButton(_ sender: UIBarButtonItem) {
