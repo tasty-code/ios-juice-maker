72d3834257c91086e56d3c0dfd41784ded4b1da9
lust3r <s1skyblue1s@gmail.com>
2023-01-06 11:43:10 +0900

    Feat: Calculator에 compare 메서드를 추가하여 재고가 없을 때 StockError에 정의한 에러문구를 출력하도록 설정

--- JuiceMaker/JuiceMaker/Model/Calculator.swift
+++ JuiceMaker/JuiceMaker/Model/Calculator.swift
@@ -15,4 +15,22 @@ struct Calculator: Computable {
     func subtract(number1: Int, number2: Int) -> Int {
         return number1 - number2
     }
+    
+    func compare(type: FruitList, isRemainCount: Bool) -> Bool {
+        if isRemainCount == false {
+            switch type {
+            case .strawberry:
+                print(StockError.notEnoughStrawberryStock)
+            case .banana:
+                print(StockError.notEnoughBananaStock)
+            case .kiwi:
+                print(StockError.notEnoughKiwiStock)
+            case .pineApple:
+                print(StockError.notEnoughPineAppleStock)
+            case .mango:
+                print(StockError.notEnoughMangoStock)
+            }
+        }
+        return isRemainCount
+    }
 }
