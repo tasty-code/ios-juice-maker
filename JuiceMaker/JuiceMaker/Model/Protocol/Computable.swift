2afc9a336bf2c4f70dc3e30f10d41ccc2ca1df23
lust3r <s1skyblue1s@gmail.com>
2023-01-06 11:34:37 +0900

    Feat: Computable 프로토콜에 compare 메서드를 추가
    과일 유형과 남은 개수를 파악해서 주스를 제조할 수 있는 수량이 남아있는지 여부를 반환

--- JuiceMaker/JuiceMaker/Model/Protocol/Computable.swift
+++ JuiceMaker/JuiceMaker/Model/Protocol/Computable.swift
@@ -10,4 +10,5 @@ import Foundation
 protocol Computable {
     func add(number1: Int, number2: Int) -> Int
     func subtract(number1: Int, number2: Int) -> Int
+    func compare(type: FruitList, isRemainCount: Bool) -> Bool
 }
