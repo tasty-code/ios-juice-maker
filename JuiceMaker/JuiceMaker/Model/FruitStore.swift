b8926167b99e89fffa3e38f00a37948811e5b1ed
Toughie <airj907@gmail.com>
2023-01-09 14:29:28 +0900

    feat: 리뷰 내용 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -19,7 +19,7 @@ class FruitStore {
     func consumeStock(for fruit: Fruit, number: UInt) {
         guard let currentNumber = fruitStock[fruit] else { return }
         guard currentNumber >= number else {
-            print(InfoMessage.noStock.rawValue)
+            print(InfoMessage.noStock.rawValue, "현재 재고: \(currentNumber), 레시피 수량 : \(number), 부족한 수량: \(number - currentNumber)" )
             return
         }
         fruitStock.updateValue(currentNumber - number, forKey: fruit)
