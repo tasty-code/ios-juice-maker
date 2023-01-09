097a241264c93f2f4fdc5fc00fd944ac7694181c
qwerty3345 <qwerty3345@naver.com>
2023-01-09 14:07:57 +0900

    test: FruitStore의 changeStock - 현재 재고를 초과해 감소 시킬 때 테스트
    - 딸기_재고를_마이너스20만큼_변경했을때_에러가_발생한다

--- JuiceMaker/FruitStoreTests/FruitStoreTests.swift
+++ JuiceMaker/FruitStoreTests/FruitStoreTests.swift
@@ -10,7 +10,6 @@ import XCTest
 
 final class FruitStoreTests: XCTestCase {
     
-
     override func setUpWithError() throws {
         Fruit.allCases.forEach { fruit in
             FruitStore.shared.setStock(of: fruit, to: 10)
@@ -19,11 +18,16 @@ final class FruitStoreTests: XCTestCase {
     
     func test_망고_재고를_10만큼_변경했을때_재고가_20이된다() throws {
         try FruitStore.shared.changeStock(of: .mango, by: 10)
-        
 
         let result = FruitStore.shared.stock[.mango]!
-        let expectation = 30
+        let expectation = 20
         
         XCTAssertEqual(result, expectation)
+    }
+    
+    func test_딸기_재고를_마이너스20만큼_변경했을때_에러가_발생한다() {
+        XCTAssertThrowsError(try FruitStore.shared.changeStock(of: .strawberry, by: -20)) { error in
+            XCTAssertEqual(error as? StockError, StockError.notEnoughToChange)
+        }
     }
 }
---
+++ JuiceMaker/JuiceMaker/Model/Constants 2.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  JuiceMaker
+//
+//  Created by Mason Kim on 2023/01/07.
+//
+
+import Foundation
+
+enum Constatns {
+    enum Identifier {
+        static let stockViewController = "StockVC"
+    }
+    
+    enum UserMessage {
+        static let makejuiceCompletedTitle = "맛있게 드세요!"
+        
+        static let yes = "네"
+        static let no = "아니오"
+    }
+    
+    enum UIText {
+        
+    }
+    
+    enum Number {
+        
+    }
+}
