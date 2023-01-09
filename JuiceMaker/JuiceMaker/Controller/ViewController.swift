6ce5bdb752c167262710a4b76695956df109af9a
sloDevz <onec555@gmail.com>
2023-01-09 16:36:08 +0900

    refactor: main VC에 final 키워드 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
