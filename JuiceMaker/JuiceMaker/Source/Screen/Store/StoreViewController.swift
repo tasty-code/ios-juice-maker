bc7983117a26ef53bf5b5b293a0f48267b96a96a
ueunli <cdenen20@gmail.com>
2023-01-10 15:37:13 +0900

    [feat] storeViewController의 label, stepper연동기능 추가
    - 각 label과 stepper를 배열로 묶고 순서대로 tag를 부여
    - tag로 stepper가 동작할 때마다 label에 바뀐 값이 반영
    - 이전화면으로 돌아가는 버튼 2가지(재고변경 취소 또는 완료)

--- JuiceMaker/JuiceMaker/Source/Screen/Store/StoreViewController.swift
+++ JuiceMaker/JuiceMaker/Source/Screen/Store/StoreViewController.swift
@@ -7,8 +7,31 @@
 import UIKit
 
 final class StoreViewController: UIViewController {
+    private let fruitStore = FruitStore.shared
+    
+    @IBOutlet private var fruitStockLabels: [UILabel]!
+    @IBOutlet private var fruitStockSteppers: [UIStepper]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction private func stockStepperDidTap(_ sender: UIStepper) {
+        guard let label = fruitStockLabels[safe: sender.tag] else { return }
+        guard let stepper = fruitStockSteppers[safe: sender.tag] else { return }
+        label.text = "\(Int(stepper.value))"
+    }
+    
+    @IBAction private func confirmChangedStockButtonDidTap(_ sender: UIBarButtonItem) {
+        addStocks()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func addStocks() {
+        Fruits.allCases.enumerated().forEach {
+            let addedStocks = fruitStockSteppers.map { Int($0.value) }
+            fruitStore.increase(of: $0.1, amount: addedStocks[safe: $0.0] ?? 0)
+        }
     }
 }
