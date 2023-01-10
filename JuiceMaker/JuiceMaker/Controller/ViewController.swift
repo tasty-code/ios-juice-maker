26bc75e34b276c2dbc992ba25bd85793c09e8efb
Sei Kim <vanism7557@gmail.com>
2023-01-10 10:19:06 +0900

    feat. alert 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -32,15 +32,6 @@ class ViewController: UIViewController {
         updateStockValue()
     }
     
-    func showStoreView() {
-        guard let storeNaviVC = storyboard?.instantiateViewController(withIdentifier: "storeNavi") as? UINavigationController else { return }
-        
-        storeNaviVC.modalPresentationStyle = .fullScreen
-        storeNaviVC.modalTransitionStyle = .coverVertical
-        
-        present(storeNaviVC, animated: true)
-    }
-    
     @IBAction func ModifyStockButtonTapped(_ sender: UIBarButtonItem) {
         showStoreView()
     }
@@ -54,11 +45,25 @@ class ViewController: UIViewController {
         }
         do {
             _ = try juiceMaker.make(juice: juice)
+            showSuccessAlert(juice: juice)
+        } catch is JuiceMakerError {
+            showFailAlert()
         } catch {
             print(error)
         }
         updateStockValue()
     }
+    
+    
+    func showStoreView() {
+        guard let storeNaviVC = storyboard?.instantiateViewController(withIdentifier: "storeNavi") as? UINavigationController else { return }
+        
+        storeNaviVC.modalPresentationStyle = .fullScreen
+        storeNaviVC.modalTransitionStyle = .coverVertical
+        
+        present(storeNaviVC, animated: true)
+    }
+    
     
     func test() {
         do {
@@ -73,5 +78,36 @@ class ViewController: UIViewController {
         for (label, fruit) in fruitLabelFruitMap {
             label.text = String(fruitStore.items[fruit, default: 0])
         }
+    }
+}
+
+//MARK: - Alert 구현
+extension ViewController {
+    func showSuccessAlert(juice: Juice) {
+        let successAlert = UIAlertController(title: nil,
+                                             message: "\(juice.rawValue) 나왔습니다! 맛있게 드세요!",
+                                             preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "확인",
+                                          style: .default)
+        
+        successAlert.addAction(confirmAction)
+        present(successAlert, animated: true)
+    }
+    
+    func showFailAlert() {
+        let failAlert = UIAlertController(title: nil,
+                                          message: "재고가 모자라요. 재고를 수정할까요?",
+                                          preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "예",
+                                          style: .default,
+                                          handler: { _ in self.showStoreView() })
+        let cancelAction = UIAlertAction(title: "아니오",
+                                         style: .cancel)
+        
+        failAlert.addAction(confirmAction)
+        failAlert.addAction(cancelAction)
+        present(failAlert, animated: true)
     }
 }
