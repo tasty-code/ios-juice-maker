0c6ac8a3ce11c737ac3d681f1ea6b01400964183
Lust3r <s1skyblue1s@gmail.com>
2023-01-11 11:42:03 +0900

    Refactor: 사용하지 않는 메서드 제거 및 음료 제조 역할을 하는 blending 메서드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -73,108 +73,33 @@ struct JuiceMaker: Makeable {
         return isContinue
     }
     
-    func requestTo(single juice: SingleFruitJuice) {
-        juice.recipe.forEach { (key: Fruit, value: Int) in
-            guard let storeOfFruitCount = fruitStore.storeValue(fruit: key) else {
-                return
-            }
-            
-            var remainFruitCount = calculator.subtract(number1: storeOfFruitCount, number2: value)
-            if remainFruitCount < 0 {
-                remainFruitCount = storeOfFruitCount
-            }
-            
-            let determine = fruitStore.isPossibleMakeSingle(juice: key, stockNumber: storeOfFruitCount)
-            fruitStore.store.updateValue(remainFruitCount, forKey: key)
-            
-            guard calculator.compare(type: key, isRemainCount: determine) == true else {
-                return
-            }
+    private func blending(juice: SingleFruitJuice) {
+        let needToMakeJuiceCount = fruitStore.needCountOfMake(to: juice)
+        let isContinue = isMakeable(juice, send: needToMakeJuiceCount)
+        if !isContinue {
+            print("음료 제조 불가")
+            return
+        }
+        
+        juice.recipe.forEach { fruit in
+            fruitStore.update(fruit.key, stock: needToMakeJuiceCount)
         }
     }
     
-    func requestTo(mix juice: MixFruitJuice) {
-        var allOfMixJuiceCount = ([Int](), [Int]())
-        var firstFruitStoreDetermine = false
-        var secondFruitStoreDetermine = false
-        
-        juice.caseList.forEach { key in
-            if key == juice {
-                allOfMixJuiceCount = currentNumber(fruit: key)
-                let currentMixJuiceCount = allOfMixJuiceCount.0
-                let needMixJuiceCount = allOfMixJuiceCount.1
-                
-                var firstMixJuiceRemainCount = calculator.subtract(number1: currentMixJuiceCount[0], number2: needMixJuiceCount[0])
-                var secondMixJuiceRemainCount = calculator.subtract(number1: currentMixJuiceCount[1], number2: needMixJuiceCount[1])
-                if firstMixJuiceRemainCount < 0 || secondMixJuiceRemainCount < 0 {
-                    firstMixJuiceRemainCount = currentMixJuiceCount[0]
-                    secondMixJuiceRemainCount = currentMixJuiceCount[1]
-                }
-                
-                let determine = fruitStore.isPossibleMakeMix(juice: key, stockNumber: (currentMixJuiceCount[0], currentMixJuiceCount[1]))
-                
-                juice.recipe.forEach { (key: FruitList, value: Int) in
-                    switch key {
-                    case .strawberry:
-                        firstFruitStoreDetermine = calculator.compare(type: .strawberry, isRemainCount: determine)
-                        fruitStore.store.updateValue(firstMixJuiceRemainCount, forKey: .strawberry)
-                    case .banana:
-                        secondFruitStoreDetermine = calculator.compare(type: .banana, isRemainCount: determine)
-                        fruitStore.store.updateValue(secondMixJuiceRemainCount, forKey: .banana)
-                    case .kiwi:
-                        secondFruitStoreDetermine = calculator.compare(type: .kiwi, isRemainCount: determine)
-                        fruitStore.store.updateValue(secondMixJuiceRemainCount, forKey: .kiwi)
-                    case .pineApple:
-                        return
-                    case .mango:
-                        firstFruitStoreDetermine = calculator.compare(type: .mango, isRemainCount: determine)
-                        fruitStore.store.updateValue(firstMixJuiceRemainCount, forKey: .mango)
-                    }
-                }
-                guard firstFruitStoreDetermine == true || secondFruitStoreDetermine == true else {
-                    return
-                }
-            }
+    private func blending(juice: MixFruitJuice) {
+        var needCounts = fruitStore.needCountOfMake(to: juice)
+        let isContinue = isMakeable(juice, send: needCounts)
+        if !isContinue {
+            print("혼합음료 제조 불가")
+            return
         }
-    } 
-    
-    func currentNumber(fruit juice: MixFruitJuice) -> ([Int], [Int]) {
-        var storeItemsCount = [Int]()
-        var consumeItemsCount = [Int]()
         
-        for _ in 0..<juice.recipe.count {
-            juice.recipe.forEach { (key: Fruit, value: Int) in
-                var storeItem: Int?
-                var consumeItem: Int?
-                
-                switch key {
-                case .strawberry:
-                    storeItem = fruitStore.storeValue(fruit: .strawberry)
-                    consumeItem = juice.recipe[.strawberry]
-                case .banana:
-                    storeItem = fruitStore.storeValue(fruit: .banana)
-                    consumeItem = juice.recipe[.banana]
-                case .kiwi:
-                    storeItem = fruitStore.storeValue(fruit: .kiwi)
-                    consumeItem = juice.recipe[.kiwi]
-                case .pineApple:
-                    storeItem = fruitStore.storeValue(fruit: .pineApple)
-                    consumeItem = juice.recipe[.pineApple]
-                case .mango:
-                    storeItem = fruitStore.storeValue(fruit: .mango)
-                    consumeItem = juice.recipe[.mango]
-                }
-                guard let storeFruitNumber = storeItem else {
-                    return
-                }
-                guard let needFruitNumber = consumeItem else {
-                    return
-                }
-                storeItemsCount.append(storeFruitNumber)
-                consumeItemsCount.append(needFruitNumber)
+        juice.recipe.forEach { fruit in
+            guard let extractCount = needCounts.first else {
+                return
             }
-            break
+            fruitStore.update(fruit.key, stock: extractCount)
+            needCounts.removeFirst()
         }
-        return (storeItemsCount, consumeItemsCount)
     }
 }
