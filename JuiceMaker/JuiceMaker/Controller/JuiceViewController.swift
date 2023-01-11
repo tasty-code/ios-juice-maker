030bba71f0ea611032df0489cd0c0cb3742fc807
Lust3r <s1skyblue1s@gmail.com>
2023-01-11 16:12:13 +0900

    Feat: JuiceViewController에 View의 요소 IBOutlet collection과 IBAction 연결 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -8,6 +8,31 @@ import UIKit
 
 class JuiceViewController: UIViewController {
     
+    @IBOutlet var juiceEmojiBundle: [UILabel]!
+    @IBOutlet var juiceStoreCountBundle: [UILabel]!
+    @IBOutlet var mixJuiceOrderBundle: [UIButton]!
+    @IBOutlet var singleJuiceOrderBundle: [UIButton]!
+    
+    @IBAction func strawBerryBananaOrder(_ sender: UIButton) {
+        
+    }
+    @IBAction func mangoKiwiOrder(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func strawberryOrder(_ sender: UIButton) {
+    }
+    @IBAction func bananaOrder(_ sender: UIButton) {
+    }
+    @IBAction func pineAppleOrder(_ sender: UIButton) {
+    }
+    @IBAction func kiwiOrder(_ sender: UIButton) {
+    }
+    @IBAction func mangoOrder(_ sender: UIButton) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
