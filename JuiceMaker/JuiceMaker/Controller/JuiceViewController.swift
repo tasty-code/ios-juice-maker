3d2141d7b2f7ffe6835f4efc45d5922e9c94a9db
Lust3r <s1skyblue1s@gmail.com>
2023-01-11 17:08:26 +0900

    Feat: 과일 초기값을 보여주는 메서드 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -31,12 +31,39 @@ class JuiceViewController: UIViewController {
     @IBAction func mangoOrder(_ sender: UIButton) {
     }
     
-    
+    private let juiceMaker = JuiceMaker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        currentStockDisplay(label: juiceStoreCountBundle)
     }
     
+    func currentStockDisplay(label: [UILabel]) {
+        label.forEach { fruitLabel in
+            switch fruitLabel.text {
+            case "🍓":
+                label[0].text = currentStock(fruitName: .strawberry)
+            case "🍌":
+                label[1].text = currentStock(fruitName: .banana)
+            case "🍍":
+                label[2].text = currentStock(fruitName: .pineApple)
+            case "🥝":
+                label[3].text = currentStock(fruitName: .kiwi)
+            case "🥭":
+                label[4].text = currentStock(fruitName: .mango)
+            default:
+                return
+            }
+        }
+        
+    }
+    
+    func currentStock(fruitName: Fruit) -> String {
+        guard let fruitStock = juiceMaker.fruitStore.store[fruitName] else {
+            return ""
+        }
+        return String(fruitStock)
+    }
 }
 
