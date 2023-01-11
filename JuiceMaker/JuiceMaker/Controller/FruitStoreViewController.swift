0a8982b33c301241c0a36d11728a6e43d67db847
ohdair <ohdiar@gmail.com>
2023-01-11 16:22:56 +0900

    feat: fruitStoreViewController fruitLabels 설정
    - 재고 추가 Scene에서 UILabel, UIStepper을  IBOutlet에 연결
    - 재고 추가 Scene title 변경
    - 각 과일별 수량 조회 후, 라벨에 연결
    - FruitStoreViewController을 final class로 변경

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class FruitStoreViewController: UIViewController {
+final class FruitStoreViewController: UIViewController {
+
+    @IBOutlet var fruitLabels: [UILabel]!
+    @IBOutlet var fruitSteppers: [UIStepper]!
+
+    let fruitStore = FruitStore.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupLabel()
     }
-    
+
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
+    }
+
+    private func setupLabel() {
+        fruitLabels.forEach { label in
+            guard let fruit = Fruits(rawValue: label.tag) else {
+                return
+            }
+            let quantity = fruitStore.stock(fruit: fruit)
+            label.text = String(quantity)
+        }
+    }
 }
+
