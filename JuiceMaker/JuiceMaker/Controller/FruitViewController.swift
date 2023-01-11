c958d025e35e2f5cb3ab93dc92bb1d8dce6c3533
Jason <ggj00125@gmail.com>
2023-01-11 16:04:43 +0900

    Feat: Custom ViewController 구현
    - JuiceViewController =  초기 화면
    - FruitViewController = 다음 화면

---
+++ JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FruitViewController.swift
+//  JuiceMaker
+//
+//  Created by Jason on 2023/01/11.
+//
+
+import Foundation
+
+class FruitViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JuiceViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
