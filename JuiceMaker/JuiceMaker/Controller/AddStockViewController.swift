dbe10da0ea89d60dd78a4b9c8ed82390851ce079
iYeahh <yboravender@gmail.com>
2023-01-12 14:49:48 +0900

    feat: 재고 추가 화면 진입시 과일의 현재 재고 수량 표시 기능 추가

--- JuiceMaker/JuiceMaker/Controller/AddStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/AddStockViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 class AddStockViewController: UIViewController {
     var fruitStore: FruitStore!
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initLabels()
+    }
+
+    func initLabels() {
+        let strawberryStock = fruitStore.stock(byFruit: .strawberry)
+        strawberryStockLabel.text = String(strawberryStock)
+
+        let bananaStock = fruitStore.stock(byFruit: .banana)
+        bananaStockLabel.text = String(bananaStock)
+
+        let pineappleStock = fruitStore.stock(byFruit: .pineapple)
+        pineappleStockLabel.text = String(pineappleStock)
+
+        let kiwiStock = fruitStore.stock(byFruit: .kiwi)
+        kiwiStockLabel.text = String(kiwiStock)
+
+        let mangoStock = fruitStore.stock(byFruit: .mango)
+        mangoStockLabel.text = String(mangoStock)
     }
 
     @IBAction func closeButton(_ sender: UIBarButtonItem) {
