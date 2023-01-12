827fdb8549cbabd5bebfcc4f82147a694604815f
Jason <ggj00125@gmail.com>
2023-01-12 18:59:01 +0900

    Feat: 닫기 버튼 푸쉬시 이전 화면으로 돌아갈 수 있도록 구현

--- JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class FruitViewController: UIViewController {
     
+    @IBAction func touchUpDismissButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
