006bfc2b3c8452b7cf464424bc204d5431bac11c
sloDevz <onec555@gmail.com>
2023-01-12 15:51:09 +0900

    refactor: 재고라벨 클래스화, 스토리보드에 링크

--- JuiceMaker/JuiceMaker/View/FruitLabels.swift
+++ JuiceMaker/JuiceMaker/View/FruitLabels.swift
@@ -6,9 +6,24 @@
 //
 
 import Foundation
+import UIKit
 
+class StrawberryStockLabel: UILabel, FruitStockManagable {
+    var fruitName: Fruits = .strawberry
+}
 
+class BananaStockLabel: UILabel, FruitStockManagable {
+    var fruitName: Fruits = .banana
+}
 
+class PineappleStockLabel: UILabel, FruitStockManagable {
+    var fruitName: Fruits = .pineapple
+}
 
+class KiwiStockLabel: UILabel, FruitStockManagable {
+    var fruitName: Fruits = .kiwi
+}
 
-
+class MangoStockLabel: UILabel, FruitStockManagable {
+    var fruitName: Fruits = .mango
+}
