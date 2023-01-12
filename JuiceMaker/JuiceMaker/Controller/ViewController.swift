c6e03074062ba0e01ab233ead7701dfd6282cb1b
sloDevz <onec555@gmail.com>
2023-01-12 15:37:47 +0900

    refactor: step3 재고라벨 컬랙션화 및 프로토콜 정의

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,8 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var strawberryLabel: UILabel!
-    @IBOutlet weak var bananaLabel: UILabel!
-    @IBOutlet weak var pineappleLabel: UILabel!
-    @IBOutlet weak var kiwiLabel: UILabel!
-    @IBOutlet weak var mangoLabel: UILabel!
+    
+    @IBOutlet var fruitStockLabels: [UILabel]!
     
     let juiceMaker = JuiceMaker()
     
@@ -28,12 +25,8 @@ final class ViewController: UIViewController {
         setFruitLabel()
     }
     
-    @objc private func setFruitLabel() {
-        strawberryLabel.text = String(FruitStore.shared.fruits[.strawberry]?.stock ?? 0)
-        bananaLabel.text = String(FruitStore.shared.fruits[.banana]?.stock ?? 0)
-        pineappleLabel.text = String(FruitStore.shared.fruits[.pineapple]?.stock ?? 0)
-        kiwiLabel.text = String(FruitStore.shared.fruits[.kiwi]?.stock ?? 0)
-        mangoLabel.text = String(FruitStore.shared.fruits[.mango]?.stock ?? 0)
+    private func setFruitLabel() {
+        
     }
     
     @IBAction func orderButtonTapped(_ sender: UIButton) {
---
+++ JuiceMaker/JuiceMaker/Protocol/Protocols.swift
@@ -0,0 +1,12 @@
+//
+//  Protocols.swift
+//  JuiceMaker
+//
+//  Created by DONGWOOK SEO on 2023/01/12.
+//
+
+import Foundation
+
+protocol FruitStockManagable {
+    var fruitName: Fruits { get }
+}
