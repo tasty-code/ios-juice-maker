99c8c1726f1b1477268b48f82549667054aa8f06
Jason <ggj00125@gmail.com>
2023-01-12 17:24:52 +0900

    Feat: CustomStringConverible 프로토콜 채택하여 rawValue 구현
    - 각 케이스 별로 Button의 currentTitle을 값으로 사용

--- JuiceMaker/JuiceMaker/Model/FruitInformation/SingleFruitJuice.swift
+++ JuiceMaker/JuiceMaker/Model/FruitInformation/SingleFruitJuice.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-enum SingleFruitJuice: CaseIterable {
-    case strawberry
-    case banana
-    case kiwi
-    case pineApple
-    case mango
+enum SingleFruitJuice: String, CaseIterable, CustomStringConvertible {
+    case strawberry = "딸기쥬스 주문"
+    case banana = "바나나쥬스 주문"
+    case kiwi = "키위쥬스 주문"
+    case pineApple = "파인애플쥬스 주문"
+    case mango = "망고쥬스 주문"
     
     var recipe: [Fruit: Int]{
         switch self {
@@ -20,12 +20,16 @@ enum SingleFruitJuice: CaseIterable {
             return [.strawberry: 16]
         case .banana:
             return [.banana: 2]
-        case .kiwi:
-            return [.kiwi: 3]
         case .pineApple:
             return [.pineApple: 2]
+        case .kiwi:
+            return [.kiwi: 3]
         case .mango:
             return [.mango: 3]
         }
+    }
+    
+    var description: String {
+        return self.rawValue
     }
 }
