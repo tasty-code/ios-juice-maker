650d29bdf7556d7aedc5df2afb1163d3c49f3593
sei <vanism7557@gmail.com>
2023-01-13 17:17:55 +0900

    feat. FruitComponent, FruitLabel 생성

---
+++ JuiceMaker/JuiceMaker/View/Components/FruitComponent.swift
@@ -0,0 +1,11 @@
+//
+//  FruitComponent.swift
+//  JuiceMaker
+//
+//  Created by sei on 2023/01/13.
+//
+
+protocol FruitComponent {
+    var fruit: Fruit { get }
+    var stock: Int { get set }
+}
