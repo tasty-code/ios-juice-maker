ab8fd9a997f517b6e89cbfdbc10df6a8bccda58e
Jason <ggj00125@gmail.com>
2023-01-15 19:08:29 +0900

    Feat: JuiceViewController의 label 연결

--- JuiceMaker/JuiceMaker/Model/countLableAttribte.swift
+++ JuiceMaker/JuiceMaker/Model/countLableAttribte.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+//MARK: - JuiceViewController
+final class StrawberryJuiceLabel: UILabel, Gettable {
+    var fruit: Fruit = .strawberry
+}
+
+final class BananJuiceLabel: UILabel, Gettable {
+    var fruit: Fruit = .banana
+}
+
+final class PineAppleJuiceLabel: UILabel, Gettable {
+    var fruit: Fruit = .pineApple
+}
+
+final class KiwiJuiceLabel: UILabel, Gettable {
+    var fruit: Fruit = .kiwi
+}
+
+final class MangoJuiceLabel: UILabel, Gettable {
+    var fruit: Fruit = .mango
+}
+
+//MARK: - FruitViewController
 final class StrawberryLabel: UILabel, Gettable {
     var fruit: Fruit = .strawberry
 }
