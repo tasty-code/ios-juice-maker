0c8777e49dde68427263642c0f3e58b9b9a90203
Jason <ggj00125@gmail.com>
2023-01-15 15:44:41 +0900

    Feat: count Label마다 Attribute를 표현할 객체 구현 및 스토리보드 적용

---
+++ JuiceMaker/JuiceMaker/Model/countLableAttribte.swift
@@ -0,0 +1,28 @@
+//
+//  countLableAttribte.swift
+//  JuiceMaker
+//
+//  Created by Jason on 2023/01/15.
+//
+
+import UIKit
+
+final class StrawberryLabel: UILabel, Gettable {
+    var fruit: Fruit = .strawberry
+}
+
+final class BananaLabel: UILabel, Gettable {
+    var fruit: Fruit = .banana
+}
+
+final class PineAppleLabel: UILabel, Gettable {
+    var fruit: Fruit = .pineApple
+}
+
+final class KiwiLabel: UILabel, Gettable {
+    var fruit: Fruit = .kiwi
+}
+
+final class MangoLabel: UILabel, Gettable {
+    var fruit: Fruit = .mango
+}
