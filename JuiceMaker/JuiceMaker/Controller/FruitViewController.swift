43418110bf60fe9a6e63e644c72cf7857e413fbe
Jason <ggj00125@gmail.com>
2023-01-15 17:35:41 +0900

    Feat: sender로 통해 들어온 동일한 과일 타입을 갖는 stepper와 label 연결

--- JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
@@ -26,6 +26,20 @@ class FruitViewController: UIViewController, SendDataDelegate {
     @IBOutlet var stepperBundle: [UIStepper]!
     
     @IBAction func stepperAction(_ sender: UIStepper) {
+        // sender로 들어온 과일타입에 해당하는 stepper 추출
+        guard let stepper = sender as? Gettable else {
+            return
+        }
+        let senderFruit = stepper.fruit
+        let changeStock = Int(sender.value)
+        
+        // countLabel을 통해 위 sender와 동일한 과일 타입의 Label을 추출
+        guard let countLabel = countLabel(of: senderFruit) else {
+            return
+        }
+        
+        sender.autorepeat = false
+        countLabel.text = String(changeStock)
     }
     
     //MARK: - FruitViewController Property
@@ -36,5 +50,14 @@ class FruitViewController: UIViewController, SendDataDelegate {
         super.viewDidLoad()
         syncFruitStocks()
         fruitStore.delegate = self
+    }
+    
+    private func countLabel(of fruit: Fruit) -> UILabel? {
+        return fruitStoreCountBundle.first { countLable in
+            guard let fruitLabel = countLable as? Gettable else {
+                return false
+            }
+            return fruitLabel.fruit == fruit
+        }
     }
 }
