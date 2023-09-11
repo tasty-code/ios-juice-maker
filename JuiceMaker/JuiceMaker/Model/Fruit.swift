b145289960780af2a98605d38dc70cabf6df1a07
SimJaeHyeok <jaehyeoking@gmail.com>
2023-09-11 17:26:19 +0900

    feat: FruitStore 구현

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by JaeHyeok Sim on 2023/09/11.
+//
+
+import Foundation
+
+enum Fruit: CaseIterable {
+    case strawberry
+    case banana 
+    case kiwi
+    case pineapple
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,33 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitList = [Fruit: Int]()
     
+    init() {
+        Fruit.allCases.forEach {
+            self.fruitList.updateValue(10, forKey: $0)
+        }
+    }
+    
+    func add(_ ingredient: [Fruit: Int]) -> Void {
+        for (fruit, amount) in ingredient {
+            self.fruitList.updateValue(fruitList[fruit]! + amount, forKey: fruit)
+        }
+    }
+
+    
+    func deduct(_ ingredient: [Fruit: Int]) -> Void {
+        for (fruit, amount) in ingredient {
+            self.fruitList.updateValue(fruitList[fruit]! - amount, forKey: fruit)
+        }
+    }
+    
+    func getRemain(_ ingredient: [Fruit]) -> [Fruit: Int] {
+        var remains = [Fruit: Int]()
+        for fruit in ingredient {
+            remains.updateValue(fruitList[fruit]!, forKey: fruit)
+        }
+        return remains
+    }
 }
+
