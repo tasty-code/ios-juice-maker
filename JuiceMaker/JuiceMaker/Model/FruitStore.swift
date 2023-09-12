8c4884ac55ce300155b3626527607c4af791467c
YunSwain <destap@naver.com>
2023-09-12 09:26:37 +0900

    [Refactor] 재고 감산 로직 이슈 발견, 문제 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,17 +7,16 @@
 import Foundation
 
 // 과일 저장소 타입
-class FruitStore {
+final class FruitStore {
     // Fruits Properties
-    private var fruitsStorage: [Fruit] = [
-        Fruit(.strawberry, count: 10)
+    private var fruitsStorage: [FruitType:Int] = [
+        .strawberry : 10, .banana : 10, .kiwi : 10, .pineapple : 10, .mango : 10
     ]
     
     // 주문 받은 것대로 재고 빼기
     func receiveOrder(juiceType: JuiceType) {
         switch juiceType {
-        case .strawberryJuice:
-            updateInventory(.strawberry, count: -16)
+        case .strawberryJuice: break
         }
     }
 }
@@ -26,16 +25,7 @@ class FruitStore {
 extension FruitStore {
     @frozen enum JuiceType {
         case strawberryJuice
-        
-        struct Juice {
-            let firstIngredient: Fruit
-            let secondIngredient: Fruit
-            
-            init(_ firstIngredient: Fruit, _ secondIngredient: Fruit) {
-                self.firstIngredient = firstIngredient
-                self.secondIngredient = secondIngredient
-            }
-        }
+//        case bananaJuice, kiwiJuice, pineappleJuice, strawberryBananaMixJuice, mangoJuice, mangoKiwiMixJuice
     }
     
     @frozen enum FruitType {
@@ -44,34 +34,17 @@ extension FruitStore {
     
     struct Fruit {
         let fruitType: FruitType
-        let count: Int
-        
-        init(_ fruitType: FruitType, count: Int) {
-            self.fruitType = fruitType
-            self.count = count
-        }
-        
-        func updateInfo(newValue: Int) -> Fruit {
-            return Fruit(fruitType, count: newValue)
-        }
+        let quantity: Int
     }
 }
 
 // MARK: Private methods
 extension FruitStore {
-    // 재고 조정
-    private func updateInventory(_ firstIngredient: Fruit, _ secondIngredient: Fruit?) {
-        guard let firstFruit = fruitsStorage.filter( {$0.fruitType == firstIngredient.fruitType }).first else { return }
+    private func checkQuantity(_ firstIngredient: Fruit, _ secondIngredient: Fruit) throws {
         
-        if let secondFruit = fruitsStorage.filter( {$0.fruitType == secondIngredient?.fruitType }).first,
-           let count = secondIngredient?.count {
-            let remainStock: Int = secondFruit.count + count
-            guard remainStock > 0 else { return }
-            fruitsStorage.filter({ $0.fruitType == secondIngredient?.fruitType }).first = secondFruit.updateInfo(newValue: remainStock)
-        }
+    }
+    
+    private func updateInventory(_ fruitType: FruitType, count: Int) {
         
-        let remainStock: Int = firstFruit.count + firstIngredient.count
-        guard remainStock > 0 else { return }
-        fruitsStorage[fruitType] = remainStock
     }
 }
