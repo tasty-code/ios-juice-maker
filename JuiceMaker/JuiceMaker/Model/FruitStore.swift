f54ba9b37f53550f19f6277e5aae4b8f38595819
rarlala <wtw9611@gmail.com>
2023-09-12 15:18:13 +0900

    feat: Juice Maker switch 문 완성 및 재고 부족 에러문 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,32 +8,37 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    static let shared = FruitStore()
-    
-    enum Fruit: String {
-        case strawberry
-        case pineapple
-        case banana
-        case kiwi
-        case mango
+  static let shared = FruitStore()
+  
+  enum Fruit: String {
+    case strawberry
+    case pineapple
+    case banana
+    case kiwi
+    case mango
+  }
+  
+  var inventory = ["strawberry": 10, "pineapple": 10, "banana": 10, "kiwi": 10, "mango": 10]
+  
+  
+  func add(fruitName: Fruit, num: Int) {
+    guard let currentNum = inventory[fruitName.rawValue] else { return }
+    let number = currentNum + num
+    inventory.updateValue(number, forKey: fruitName.rawValue)
+  }
+  
+  func minus(fruitName: Fruit, num: Int) {
+    guard let currentNum = inventory[fruitName.rawValue] else { return }
+    if currentNum < num {
+      print("재고가 없습니다")
+      return
     }
-    
-    var inventory = ["strawberry": 10, "pineapple": 10, "banana": 10, "kiwi": 10, "mango": 10]
-    
-    func add(fruitName: Fruit, num: Int) {
-        guard let currentNum = inventory[fruitName.rawValue] else { return }
-        let number = currentNum + num
-        inventory.updateValue(number, forKey: fruitName.rawValue)
-    }
-    
-    func minus(fruitName: Fruit, num: Int) {
-        guard let currentNum = inventory[fruitName.rawValue] else { return }
-        let number = currentNum - num
-        inventory.updateValue(number, forKey: fruitName.rawValue)
-    }
-    
-    func getNum(fruitName: Fruit) -> Int? {
-        guard let currentNum = inventory[fruitName.rawValue] else { return nil }
-        return currentNum
-    }
+    let number = currentNum - num
+    inventory.updateValue(number, forKey: fruitName.rawValue)
+  }
+  
+  func getNum(fruitName: Fruit) -> Int? {
+    guard let currentNum = inventory[fruitName.rawValue] else { return nil }
+    return currentNum
+  }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,41 +8,36 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    let store = FruitStore.shared
-    
-    enum JuiceType: String {
-        case strawberry
-        case banana
-        case kiwi
-        case pineapple
-        case strawberryBanana
-        case mango
-        case mangoKiwi
+  let store = FruitStore.shared
+  
+  enum JuiceType: Int {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mango
+    case mangoKiwi
+  }
+  
+  func makeJuice(type: JuiceType) {
+    switch type {
+    case .strawberry:
+      store.minus(fruitName: .strawberry, num: 16)
+    case .banana:
+      store.minus(fruitName: .banana, num: 2)
+    case .kiwi:
+      store.minus(fruitName: .kiwi, num: 3)
+    case .pineapple:
+      store.minus(fruitName: .pineapple, num: 2)
+    case .strawberryBanana:
+      store.minus(fruitName: .strawberry, num: 10)
+      store.minus(fruitName: .banana, num: 1)
+    case .mango:
+      store.minus(fruitName: .mango, num: 3)
+    case .mangoKiwi:
+      store.minus(fruitName: .mango, num: 2)
+      store.minus(fruitName: .kiwi, num: 1)
     }
-    
-    func order() {
-        print("주문해주세요.")
-        guard let fruitOrder = readLine() else { return }
-    }
-    
-    func makeJuice(type: JuiceType) {
-        switch type {
-        case .strawberry:
-            break
-        case .banana:
-            break
-        case .kiwi:
-            break
-        case .pineapple:
-            break
-        case .strawberryBanana:
-            break
-        case .mango:
-            break
-        case .mangoKiwi:
-            break
-        default:
-            break
-        }
-    }
+  }
 }
