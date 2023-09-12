261b7a60388bf4babf00e48db9d55c2b63e20362
COMDORI <02145s1@gmail.com>
2023-09-12 12:59:02 +0900

    feat: 과일 종류 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by Wonji Ha on 2023/09/12.
+//
+
+import Foundation
+
+enum Fruit {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case mango
+}
