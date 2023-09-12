b273a13ce2a807d66cb29461297ee3d8c6e2883b
snowy-summer <chsb1221@naver.com>
2023-09-12 11:01:32 +0900

    [Feat] 해당 과일이 있는지 확인하는 함수를 생성, fruits 프로퍼티 추가,  Fruit struct에 equatable 프로토콜 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,10 +8,23 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    struct Fruit {
+    
+    var fruits: [Fruit]
+    
+    init(fruits: [Fruit]) {
+        self.fruits = fruits
+    }
+    
+    struct Fruit: Equatable {
         var name: String
         var stock: Int = 10
     }
+    
+    func containsFruits(inputFruit: Fruit) throws -> Fruit {
+        guard let fruit = fruits.filter({ $0 == inputFruit }).first else { throw Errorcase.canNotFound }
+        return fruit
+    }
+    
     
 }
 
