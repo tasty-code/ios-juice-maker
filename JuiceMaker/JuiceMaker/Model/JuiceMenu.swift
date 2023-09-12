e6f60e2b2f4c17787d78656fb10d2becc84b35c5
JinUng41 <ingjwjw@naver.com>
2023-09-12 13:08:40 +0900

    feat: add enum JuiceMenu

---
+++ JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
@@ -0,0 +1,31 @@
+//
+//  JuiceMenu.swift
+//  JuiceMaker
+//
+//  Created by 김진웅 on 2023/09/12.
+//
+
+import Foundation
+
+enum JuiceMenu {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+    
+    var recipe: [Fruit: UInt] {
+        switch self {
+        case .strawberryJuice: return [Fruit.strawberry: 16]
+        case .bananaJuice: return [Fruit.banana: 2]
+        case .kiwiJuice: return [Fruit.kiwi: 3]
+        case .pineappleJuice: return [Fruit.pineapple: 2]
+        case .strawberryBananaJuice: return [Fruit.strawberry: 10, Fruit.banana: 1]
+        case .mangoJuice: return [Fruit.mango: 3]
+        case .mangoKiwiJuice: return [Fruit.mango: 2, Fruit.kiwi: 1]
+        }
+    }
+}
+
