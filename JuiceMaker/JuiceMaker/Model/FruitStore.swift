4a4650cca9d10c014835323778a0a73ddbe9c430
JinUng41 <ingjwjw@naver.com>
2023-09-12 13:22:09 +0900

    feat: function consume and getRemainedCount in FruitStore class

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,25 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    private var fruitInventory: [Fruit: UInt] = [
+        Fruit.strawberry: 10,
+        Fruit.banana: 10,
+        Fruit.pineapple: 10,
+        Fruit.kiwi: 10,
+        Fruit.mango: 10
+    ]
     
+    func consume(of fruit: Fruit, for count: UInt) throws {
+        guard fruitInventory.contains(where: { $0.key == fruit }),
+              let remainedCount = fruitInventory[fruit] else { throw InventoryError.invalidMenuError }
+        
+        if remainedCount < count { throw InventoryError.noLongerConsumeError }
+        
+        fruitInventory[fruit] = (remainedCount - count)
+    }
+    
+    func getRemainedCount(of fruit: Fruit) throws -> UInt{
+        guard let remainedCount = fruitInventory[fruit] else { throw InventoryError.invalidMenuError }
+        return remainedCount
+    }
 }
