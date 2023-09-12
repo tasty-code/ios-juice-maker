ea03d9aa8a3e44ec5f8d02423440e5d6deac3736
kimjunsung <assd1108@naver.com>
2023-09-12 13:14:54 +0900

    refactor: FruitStore에 싱글턴 패턴 적용

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,13 +8,15 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    private var strawberry: Int
-    private var banana: Int
-    private var pineapple: Int
-    private var mango: Int
-    private var kiwi: Int
+    static let shared = FruitStore()
     
-    init(strawberry: Int, banana: Int, pineapple: Int, mango: Int, kiwi: Int) {
+    var strawberry: Int
+    var banana: Int
+    var pineapple: Int
+    var mango: Int
+    var kiwi: Int
+    
+    private init(strawberry: Int, banana: Int, pineapple: Int, mango: Int, kiwi: Int) {
         self.strawberry = strawberry
         self.banana = banana
         self.pineapple = pineapple
@@ -22,11 +24,11 @@ class FruitStore {
         self.kiwi = kiwi
     }
     
-    convenience init(count: Int) {
+    private convenience init(count: Int) {
         self.init(strawberry: count, banana: count, pineapple: count, mango: count, kiwi: count)
     }
     
-    convenience init() {
+    private convenience init() {
         self.init(count: 10)
     }
     
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,16 +8,15 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    private var fruitStore = FruitStore()
     func make(juice: JuiceType) throws {
-        try fruitStore.update(fruits: juice.ingredients, as: +)
+        try FruitStore.shared.update(fruits: juice.ingredients, as: +)
     }
 
     func add(fruit: Fruit) throws {
-        try fruitStore.update(fruit, as: +)
+        try FruitStore.shared.update(fruit, as: +)
     }
 
     func sub(fruit: Fruit) throws {
-        try fruitStore.update(fruit, as: -)
+        try FruitStore.shared.update(fruit, as: -)
     }
 }
