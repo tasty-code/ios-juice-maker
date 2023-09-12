bd4f04f96cae7a53dceef80e54e89c00c0d58e61
KimRoks <rudfhr3161@naver.com>
2023-09-12 13:25:50 +0900

    chore: Create Fruit Juice Model

---
+++ JuiceMaker/JuiceMaker/Model/FruitJuice.swift
@@ -0,0 +1,31 @@
+//
+//  FruitJuice.swift
+//  JuiceMaker
+//
+//  Created by 김경록 on 2023/09/12.
+//
+
+import Foundation
+
+@frozen
+enum FruitJuice: CustomStringConvertible {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+    case strawberryBanana
+    case mangoKiwi
+     
+    var description: String {
+        switch self {
+        case .strawberry: return "딸기"
+        case .banana: return "바나나"
+        case .pineapple: return "파인애플"
+        case .kiwi: return "키위"
+        case .mango: return "망고"
+        case .strawberryBanana: return "딸기바나나"
+        case .mangoKiwi: return "망고키위"
+        }
+    }
+}
