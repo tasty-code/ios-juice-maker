a1054916767c09da8f96fb54669f319e76ac8783
bamsak <qjatjrdl963@naver.com>
2023-09-12 14:58:34 +0900

    [Feat] checkStock 메소드, recipe 열거형으로 선언 및 주스 만들 때 필요한 과일 갯수 추출하는 메소드 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,11 +6,42 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
+
 struct JuiceMaker {
     
+    let fruitStore: FruitStore
     
+    func checkStock(inputFruit: FruitStore.Fruit, decreaseCount: Int) throws{
+        guard inputFruit.stock > decreaseCount else {throw Errorcase.outOfStock}
+    }
+    
+    
+    
+    
+    
+    func recipeCount(juice: String) throws -> Int{
+        guard let confirmJuice = Recipe(rawValue: juice) else { throw Errorcase.canNotFound }
+        return confirmJuice.dereaseCount
+        
+    }
+    
+    
+    enum Recipe: String {
+        case strawberryJuice = "딸기 주스"
+        case bananaJuice = "바나나 주스"
+        case pineappleJuice = "파인애플 주스"
+        case mangoJuice = "망고 주스"
+        case kiwiJuice = "키위 주스"
+        var dereaseCount: Int {
+            switch self {
+            case .strawberryJuice: return 16
+            case .bananaJuice, .pineappleJuice: return 2
+            case .mangoJuice, .kiwiJuice: return 3
+            }
+        }
+    }
 }
+
 
 
 
