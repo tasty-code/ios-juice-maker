8a73d16c9ffe7bcbf493ba5dc35c7980adceac63
COMDORI <02145s1@gmail.com>
2023-09-12 13:33:12 +0900

    feat: 쥬스 만들기 및 과일 수량 확인

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,30 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private let fruitStore = FruitStore()
     
+    func mixJuice(of menu: JuiceMenu) throws {
+        let fruitCheck = try enoughFruit(of: menu)
+        if fruitCheck == false {
+            throw InventoryError.noLongerConsumeError
+        }
+        
+        let recipe = menu.recipe
+        for (fruit, count) in recipe {
+            try fruitStore.consume(of: fruit, for: count)
+        }
+    }
+    
+    private func enoughFruit(of menu: JuiceMenu) throws -> Bool {
+        let recipe = menu.recipe
+        for (fruit, count) in recipe {
+            let remainedFruit = try fruitStore.getRemainedCount(of: fruit)
+            
+            if remainedFruit < count {
+                return false
+            }
+        }
+        
+        return true
+    }
 }
