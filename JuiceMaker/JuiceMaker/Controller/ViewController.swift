146f979334d0544cd8ea3c1bc42b80d44e9ff3a8
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-09-13 16:04:54 +0900

    chore: 출력 메세지 수정

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         let juiceMaker = JuiceMaker()
         
         juiceMaker.makeJuice(.strawberryBananaJuice)
+        juiceMaker.makeJuice(.strawberryBananaJuice)
     }
 }
 
