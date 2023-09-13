087726493adcf2e0e6c80e0a9ea0ef8d731a162c
YunSwain <destap@naver.com>
2023-09-13 20:13:13 +0900

    [Feat] Storyboard - ViewController 인터페이스빌더 링크 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,16 +7,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let juiceMaker: JuiceMaker = JuiceMaker()
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
     
     @IBOutlet weak var orderJuiceButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.manageFruitStock()
     }
 
-
     @IBAction func makeJuice(_ sender: UIButton) {
+        guard let id = sender.restorationIdentifier else { return }
+        do {
+            try juiceMaker.makeJuice(juiceName: id)
+            manageFruitStock()
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    private func manageFruitStock() {
+        let inventory = juiceMaker.checkFruitStoreInventory()
         
+        strawberryStockLabel.text = String(inventory["딸기"] ?? 0)
+        bananaStockLabel.text = String(inventory["바나나"] ?? 0)
+        kiwiStockLabel.text = String(inventory["키위"] ?? 0)
+        pineappleStockLabel.text = String(inventory["파인애플"] ?? 0)
+        mangoStockLabel.text = String(inventory["망고"] ?? 0)
     }
 }
 
