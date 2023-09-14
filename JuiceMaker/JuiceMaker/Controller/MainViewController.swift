a387bd8fd72d34c897151a0303c6342b58733fa8
JinUng41 <ingjwjw@naver.com>
2023-09-14 17:14:54 +0900

    chore: 불필요한 주석 삭제

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -12,7 +12,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
