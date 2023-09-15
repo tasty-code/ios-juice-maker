fcb068ab4a545a75034d1ab7d57afc3d43add0a2
JinUng41 <ingjwjw@naver.com>
2023-09-15 19:59:26 +0900

    feat: identifier를 받아 Fruit타입으로 리턴해주는 함수를 정의

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -13,4 +13,21 @@ enum Fruit {
     case kiwi
     case pineapple
     case mango
+    
+    static func identifyFruit(labelIdentifier: String) throws -> Fruit {
+        switch labelIdentifier {
+        case "딸기":
+            return .strawberry
+        case "바나나":
+            return .banana
+        case "파인애플":
+            return .pineapple
+        case "키위":
+            return .kiwi
+        case "망고":
+            return .mango
+        default:
+            throw InventoryError.invalidError
+        }
+    }
 }
