c6895ce27b96c1e975cef05c1874dfa1796fddba
JinUng41 <ingjwjw@naver.com>
2023-09-15 20:00:22 +0900

    refactor: 과일 수량 나타내는 함수를 수정

--- JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
@@ -19,30 +19,42 @@ class FruitInventoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initIdentifier()
+    }
+    
+    private func initIdentifier() {
+        strawberryLabel.accessibilityIdentifier = "딸기"
+        bananaLabel.accessibilityIdentifier = "바나나"
+        pineappleLabel.accessibilityIdentifier = "파인애플"
+        kiwiLabel.accessibilityIdentifier = "키위"
+        mangoLabel.accessibilityIdentifier = "망고"
+        
         remainFruit()
     }
     
     private func remainFruit() {
-        do {
-            guard let juiceMaker = juiceMaker else { return }
+        let labelArray: [UILabel] = [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel]
+        
+        labelArray.forEach { label in
+            guard let id = label.accessibilityIdentifier else { return }
             
-            let strawberry = try juiceMaker.remainingCount(fruit: .strawberry)
-            strawberryLabel.text = String(strawberry)
-            
-            let banana = try juiceMaker.remainingCount(fruit: .banana)
-            bananaLabel.text = String(banana)
-            
-            let kiwi = try juiceMaker.remainingCount(fruit: .kiwi)
-            kiwiLabel.text = String(kiwi)
-            
-            let pineapple = try juiceMaker.remainingCount(fruit: .pineapple)
-            pineappleLabel.text = String(pineapple)
-            
-            let mango = try juiceMaker.remainingCount(fruit: .mango)
-            mangoLabel.text = String(mango)
-        } catch {
-            print(error)
+            do {
+                guard let tempJuiceMaker = juiceMaker else { return }
+                let fruit = try Fruit.identifyFruit(labelIdentifier: id)
+                let count = try tempJuiceMaker.remainingCount(fruit: fruit)
+                label.text = "\(count)"
+            } catch {
+                defaultAlert(message: InventoryError.invalidError.description)
+            }
         }
     }
-    
+}
+
+extension FruitInventoryViewController {
+    private func defaultAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let yes = UIAlertAction(title: "예", style: .default)
+        alert.addAction(yes)
+        present(alert, animated: true, completion: nil)
+    }
 }
