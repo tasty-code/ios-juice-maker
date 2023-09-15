bd3c73f7520f0127e73d8ea03533f8b56e5e290f
JinUng41 <ingjwjw@naver.com>
2023-09-15 16:23:56 +0900

    feat: FruitInventoryVC에서 전달받은 과일을 레이블에 업데이트

--- JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
@@ -11,8 +11,38 @@ class FruitInventoryViewController: UIViewController {
     
     var juiceMaker: JuiceMaker?
     
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        remainFruit()
+    }
+    
+    private func remainFruit() {
+        do {
+            guard let juiceMaker = juiceMaker else { return }
+            
+            let strawberry = try juiceMaker.remainingCount(fruit: .strawberry)
+            strawberryLabel.text = String(strawberry)
+            
+            let banana = try juiceMaker.remainingCount(fruit: .banana)
+            bananaLabel.text = String(banana)
+            
+            let kiwi = try juiceMaker.remainingCount(fruit: .kiwi)
+            kiwiLabel.text = String(kiwi)
+            
+            let pineapple = try juiceMaker.remainingCount(fruit: .pineapple)
+            pineappleLabel.text = String(pineapple)
+            
+            let mango = try juiceMaker.remainingCount(fruit: .mango)
+            mangoLabel.text = String(mango)
+        } catch {
+            print(error)
+        }
     }
     
 }
