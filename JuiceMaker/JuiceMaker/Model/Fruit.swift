ef47cb4411918225a7759da71e240b667ef624bd
KimRoks <rudfhr3161@naver.com>
2023-09-17 15:25:43 +0900

    refactor: fruit enum definitions
    - 새로운 "fruit" enum 정의

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 김경록 on 2023/09/15.
+//
+
+import Foundation
+
+enum Fruit: Int {
+    case strawberry = 0
+    case banana = 1
+    case pineapple = 2
+    case kiwi = 3
+    case mango = 4
+}
