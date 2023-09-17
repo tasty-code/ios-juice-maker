23b560c9b0977e88ed7235eb21e090783a80366c
KimRoks <rudfhr3161@naver.com>
2023-09-17 15:30:04 +0900

    refactor: refactoring according to Enum definition
    - 새 enum 정의에 따른 juiceMunu의 case 네이밍 수정
    - 재사용성을 위한 juiceREcipe() function 정의

--- JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
@@ -7,24 +7,43 @@
 
 import Foundation
 
-enum JuiceMenu: Int, CustomStringConvertible {
-    case strawberry
-    case banana
-    case pineapple
-    case kiwi
-    case mango
-    case strawberryBanana
-    case mangoKiwi
+enum JuiceMenu {
+    case strawberryJuice
+    case bananaJuice
+    case pineappleJuice
+    case kiwiJuice
+    case mangoJuice
+    case strawberryBananaJuice
+    case mangoKiwiJuice
      
     var description: String {
         switch self {
-        case .strawberry: return "딸기"
-        case .banana: return "바나나"
-        case .pineapple: return "파인애플"
-        case .kiwi: return "키위"
-        case .mango: return "망고"
-        case .strawberryBanana: return "딸기바나나"
-        case .mangoKiwi: return "망고키위"
+        case .strawberryJuice: return "딸기"
+        case .bananaJuice: return "바나나"
+        case .pineappleJuice: return "파인애플"
+        case .kiwiJuice: return "키위"
+        case .mangoJuice: return "망고"
+        case .strawberryBananaJuice: return "딸기바나나"
+        case .mangoKiwiJuice: return "망고키위"
+        }
+    }
+    
+    func juiceRecipe() -> [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
         }
     }
 }
