40324f8114b415d47d014a11f00b6b170d2e1aef
rarlala <wtw9611@gmail.com>
2023-09-19 11:04:41 +0900

    feat: 팝업 예 버튼 클릭 시 화면 전환 핸들러 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -28,7 +28,12 @@ class ViewController: UIViewController {
     if type == "oneButton" {
       alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
     } else {
-      alert.addAction(UIAlertAction(title: "예", style: UIAlertAction.Style.default, handler: nil))
+      alert.addAction(UIAlertAction(title: "예", style: UIAlertAction.Style.default, handler: { _ in
+        guard let editStoreView = self.storyboard?.instantiateViewController(identifier: "editStoreView") else { return }
+        editStoreView.modalTransitionStyle = .coverVertical
+        editStoreView.modalPresentationStyle = .automatic
+        self.present(editStoreView, animated: true, completion: nil)
+      }))
       alert.addAction(UIAlertAction(title: "아니오", style: UIAlertAction.Style.cancel, handler: nil))
     }
     self.present(alert, animated: true, completion: nil)
