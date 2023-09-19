2bd00812cd325f5c2cc05f03ad971506f6ccf47e
yeji1008 <yeji@YEJIui-MacBookPro.local>
2023-09-19 16:14:07 +0900

    feat: 닫기터치 시 이전화면으로 복귀

---
+++ JuiceMaker/JuiceMaker/Controller/EditStoreViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditStoreViewController.swift
+//  JuiceMaker
+//
+//  Created by YEJI on 2023/09/19.
+//
+
+import UIKit
+
+class EditStoreViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+    self.dismiss(animated: true)
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
