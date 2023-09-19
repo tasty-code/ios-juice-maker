25233d654fc1250301d511e1926021e5fc4d9609
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-09-19 16:49:06 +0900

    feat: 레이아웃 수정 및, 라벨 업데이트

--- JuiceMaker/JuiceMaker/Controller/StockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -15,13 +15,26 @@ class StockViewController: UIViewController {
     var transferData: [Fruit : Int] = [:]
     var delegate : SendDataDelegate?
     
+    @IBOutlet var changableFruitsLabel: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
+        
+        initialConfig()
     }
     
     @IBAction func closeBtn(_ sender: UIButton) {
         delegate?.recieveData(response: transferData)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func initialConfig() {
+        for (fruit, _) in transferData {
+            guard let fruitLabelText = transferData[fruit] else {
+                return
+            }
+            changableFruitsLabel[fruit.rawValue].text = String(fruitLabelText)
+        }
     }
 }
