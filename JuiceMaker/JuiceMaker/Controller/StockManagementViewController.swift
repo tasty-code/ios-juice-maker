2dcb99e72c4724552847b1fb873e457fe2e41880
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-09-19 13:16:14 +0900

    feat: Connect stock management view controller
    - 재고 관리 viewController을 모달로 연결

---
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StockManagementViewController.swift
+//  JuiceMaker
+//
+//  Created by 김예준 on 2023/09/19.
+//
+
+import UIKit
+
+class StockManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
