950370df3ff3c31824cd8c65973ace35f1de7377
COMDORI <02145s1@gmail.com>
2023-09-20 17:12:37 +0900

    refactor: 공백 제거 및 코드 순서 변경

--- JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitInventoryViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 final class FruitInventoryViewController: UIViewController {
-    
     var juiceMaker: JuiceMaker?
     
     @IBOutlet weak var strawberryLabel: UILabel!
@@ -35,56 +34,11 @@ final class FruitInventoryViewController: UIViewController {
         pineappleLabel.accessibilityIdentifier = "파인애플"
         kiwiLabel.accessibilityIdentifier = "키위"
         mangoLabel.accessibilityIdentifier = "망고"
-        
         strawberryStepper.accessibilityIdentifier = "딸기"
         bananaStepper.accessibilityIdentifier = "바나나"
         pineappleStepper.accessibilityIdentifier = "파인애플"
         kiwiStepper.accessibilityIdentifier = "키위"
         mangoStepper.accessibilityIdentifier = "망고"
-    }
-    
-    private func updateFruitLabels() {
-        let fruitLabels = [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel]
-        
-        fruitLabels.forEach { label in
-            guard let tempLabel = label, let id = tempLabel.accessibilityIdentifier else {
-                return
-            }
-            do {
-                guard let fruit = try Fruit(id), let tempJuiceMaker = juiceMaker else {
-                    return
-                }
-                let count = try tempJuiceMaker.remainingCount(fruit: fruit)
-                tempLabel.text = "\(count)"
-            } catch {
-                defaultAlert(message: InventoryError.invalidError.description)
-            }
-        }
-    }
-    
-    @IBAction func closeButtonTapped(_ sender: UIBarButtonItem) {
-        self.dismiss(animated: true)
-    }
-    
-    
-    @IBAction func stepperTapped(_ sender: UIStepper) {
-        guard let id = sender.accessibilityIdentifier else { return }
-        do {
-            guard let fruit = try Fruit(id), let tempJuiceMaker = juiceMaker else { return }
-            tempJuiceMaker.changeCount(fruit: fruit, newCount: UInt(sender.value))
-            updateFruitLabels()
-        } catch {
-            defaultAlert(message: InventoryError.invalidError.description)
-        }
-    }
-}
-
-extension FruitInventoryViewController {
-    private func defaultAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "예", style: .default)
-        alert.addAction(yesAction)
-        present(alert, animated: true, completion: nil)
     }
     
     private func initStepperValue() {
@@ -107,5 +61,47 @@ extension FruitInventoryViewController {
                 defaultAlert(message: InventoryError.invalidError.description)
             }
         }
+    }
+    
+    private func updateFruitLabels() {
+        let fruitLabels = [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel]
+        fruitLabels.forEach { label in
+            guard let tempLabel = label, let id = tempLabel.accessibilityIdentifier else {
+                return
+            }
+            do {
+                guard let fruit = try Fruit(id), let tempJuiceMaker = juiceMaker else {
+                    return
+                }
+                let count = try tempJuiceMaker.remainingCount(fruit: fruit)
+                tempLabel.text = "\(count)"
+            } catch {
+                defaultAlert(message: InventoryError.invalidError.description)
+            }
+        }
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func stepperTapped(_ sender: UIStepper) {
+        guard let id = sender.accessibilityIdentifier else { return }
+        do {
+            guard let fruit = try Fruit(id), let tempJuiceMaker = juiceMaker else { return }
+            tempJuiceMaker.changeCount(fruit: fruit, newCount: UInt(sender.value))
+            updateFruitLabels()
+        } catch {
+            defaultAlert(message: InventoryError.invalidError.description)
+        }
+    }
+}
+
+extension FruitInventoryViewController {
+    private func defaultAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "예", style: .default)
+        alert.addAction(yesAction)
+        present(alert, animated: true, completion: nil)
     }
 }
--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -7,7 +7,6 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-    
     private let juiceMaker = JuiceMaker()
     
     @IBOutlet weak var strawberryLabel: UILabel!
@@ -39,7 +38,6 @@ final class MainViewController: UIViewController {
         pineappleLabel.accessibilityIdentifier = "파인애플"
         kiwiLabel.accessibilityIdentifier = "키위"
         mangoLabel.accessibilityIdentifier = "망고"
-        
         strawberryJuiceButton.accessibilityIdentifier = "딸기쥬스"
         bananaJuiceButton.accessibilityIdentifier = "바나나쥬스"
         pineappleJuiceButton.accessibilityIdentifier = "파인애플쥬스"
@@ -51,7 +49,6 @@ final class MainViewController: UIViewController {
         
     private func updateFruitLabels() {
         let fruitLabels = [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel]
-        
         fruitLabels.forEach { label in
             guard let tempLabel = label, let id = tempLabel.accessibilityIdentifier else {
                 return
@@ -68,28 +65,21 @@ final class MainViewController: UIViewController {
         }
     }
     
+    private func moveInventoryVC () {
+        guard let inventoryVC = self.storyboard?.instantiateViewController(identifier: "Inventory") as? FruitInventoryViewController else {
+            return
+        }
+        inventoryVC.juiceMaker = self.juiceMaker
+        let navigationVC = UINavigationController(rootViewController: inventoryVC)
+        navigationVC.modalTransitionStyle = .crossDissolve
+        navigationVC.modalPresentationStyle = .fullScreen
+        present(navigationVC, animated: true)
+    }
+    
     @IBAction func changeInventoryButtonTapped(_ sender: UIButton) {
         self.moveInventoryVC()
     }
-
-    @IBAction func orderJuice(_ sender: UIButton) {
-        guard let id = sender.accessibilityIdentifier else {
-            return
-        }
-        do {
-            let menu = try menuForJuiceButton(btnIdentifier: id)
-            try juiceMaker.makeJuice(order: menu)
-            updateFruitLabels()
-            defaultAlert(message: "\(menu.recipeName)쥬스 나왔습니다! 맛있게 드세요!")
-        } catch InventoryError.noLongerConsumeError {
-            juiceMakeFailAlert(message: InventoryError.noLongerConsumeError.description)
-        } catch {
-            defaultAlert(message: InventoryError.invalidError.description)
-        }
-    }
-}
-
-extension MainViewController {
+    
     private func menuForJuiceButton(btnIdentifier: String) throws -> JuiceMenu {
         switch btnIdentifier {
         case "딸기쥬스":
@@ -111,17 +101,24 @@ extension MainViewController {
         }
     }
     
-    private func moveInventoryVC () {
-        guard let inventoryVC = self.storyboard?.instantiateViewController(identifier: "Inventory") as? FruitInventoryViewController else {
+    @IBAction func orderJuice(_ sender: UIButton) {
+        guard let id = sender.accessibilityIdentifier else {
             return
         }
-        inventoryVC.juiceMaker = self.juiceMaker
-        let navigationVC = UINavigationController(rootViewController: inventoryVC)
-        navigationVC.modalTransitionStyle = .crossDissolve
-        navigationVC.modalPresentationStyle = .fullScreen
-        present(navigationVC, animated: true)
+        do {
+            let menu = try menuForJuiceButton(btnIdentifier: id)
+            try juiceMaker.makeJuice(order: menu)
+            updateFruitLabels()
+            defaultAlert(message: "\(menu.recipeName)쥬스 나왔습니다! 맛있게 드세요!")
+        } catch InventoryError.noLongerConsumeError {
+            juiceMakeFailAlert(message: InventoryError.noLongerConsumeError.description)
+        } catch {
+            defaultAlert(message: InventoryError.invalidError.description)
+        }
     }
-    
+}
+
+extension MainViewController {
     private func defaultAlert(message: String) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let yesAction = UIAlertAction(title: "예", style: .default)
@@ -140,4 +137,3 @@ extension MainViewController {
         present(alert, animated: true)
     }
 }
-
