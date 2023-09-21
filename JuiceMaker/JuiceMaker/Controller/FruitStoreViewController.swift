f980a8d4365a4f0ac262645460d628655475e712
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2023-09-21 10:33:07 +0900

    [Feat] 뒤로가기 버튼 추가, AutoLayout 적용

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -13,6 +13,7 @@ class FruitStoreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configStepperAndLabel()
+        self.navigationItem.setHidesBackButton(true, animated: true)
     }
     
    
@@ -38,9 +39,10 @@ class FruitStoreViewController: UIViewController {
     
     @IBOutlet weak var mangoStepper: UIStepper!
     
-    @IBAction func btn(_ sender: UIBarButtonItem) {
-        self.navigationController?.popViewController(animated: true)
+    @IBAction func goToJuiceMakerView(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
     }
+    
     @IBAction func fruitStockStepper(_ sender: UIStepper) {
         do {
             guard let stepperTag = StepperTag(rawValue: sender.tag) else { return }
