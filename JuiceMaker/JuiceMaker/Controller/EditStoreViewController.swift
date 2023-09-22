140991061bc589fc15a2a8a03e877cd9d10ad4d4
rarlala <wtw9611@gmail.com>
2023-09-22 10:01:48 +0900

    feat: stepper minimum value 설정 함수 추가

--- JuiceMaker/JuiceMaker/Controller/EditStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditStoreViewController.swift
@@ -21,9 +21,16 @@ class EditStoreViewController: UIViewController {
   @IBOutlet weak var kiwiNumberLabel: UILabel!
   @IBOutlet weak var mangoNumberLabel: UILabel!
   
+  @IBOutlet weak var strawberryStepper: UIStepper!
+  @IBOutlet weak var bananaStepper: UIStepper!
+  @IBOutlet weak var pineappleStepper: UIStepper!
+  @IBOutlet weak var kiwiStepper: UIStepper!
+  @IBOutlet weak var mangoStepper: UIStepper!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setNumberLabel()
+    setMinimumValue()
   }
   
   @IBAction func dismissButtonTapped(_ sender: UIButton) {
@@ -58,7 +65,6 @@ class EditStoreViewController: UIViewController {
     }
   }
   
-  
   func setNumberLabel() {
     strawberryNumberLabel.text = strawberry
     bananaNumberLabel.text = banana
@@ -66,4 +72,27 @@ class EditStoreViewController: UIViewController {
     kiwiNumberLabel.text = kiwi
     mangoNumberLabel.text = mango
   }
+  
+  func setMinimumValue() {
+    guard let strawberryString = strawberry else { return }
+    guard let strawberryDouble = Double(strawberryString) else { return }
+    strawberryStepper.minimumValue = -strawberryDouble
+    
+    guard let bananaString = banana else { return }
+    guard let bananaDouble = Double(bananaString) else { return }
+    bananaStepper.minimumValue = -bananaDouble
+    
+    guard let pineappleString = pineapple else { return }
+    guard let pineappleDouble = Double(pineappleString) else { return }
+    pineappleStepper.minimumValue = -pineappleDouble
+    
+    guard let kiwiString = kiwi else { return }
+    guard let kiwiDouble = Double(kiwiString) else { return }
+    kiwiStepper.minimumValue = -kiwiDouble
+    
+    guard let mangoString = mango else { return }
+    guard let mangoDouble = Double(mangoString) else { return }
+    mangoStepper.minimumValue = -mangoDouble
+  }
+  
 }
