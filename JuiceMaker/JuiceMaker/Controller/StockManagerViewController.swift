e3f4ba919fb1a6bd2026f74da6a15168e410b79a
KimRoks <rudfhr3161@naver.com>
2023-09-25 20:37:16 +0900

    feat: embody dismiss button
    - StockManagerVieController에서 닫기 버튼에 대한 화면이동 구현

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -9,8 +9,20 @@ import UIKit
 
 final class StockManagerViewController: UIViewController {
 
+    // MARK: - Properties
+
+    
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+    }
+    
+        // MARK: - Methods
+
+    @IBAction func pressCloseButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
 }
