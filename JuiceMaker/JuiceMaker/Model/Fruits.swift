64c36dc8cf9320594f0d91700651764f3cb2980d
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-04 17:44:34 +0900

    feat: 쥬스 레시피 별 로직 구현

---
+++ JuiceMaker/JuiceMaker/Model/Fruits.swift
@@ -0,0 +1,14 @@
+//
+//  Fruits.swift
+//  JuiceMaker
+//
+//  Created by 강창현 on 12/4/23.
+//
+
+enum Fruits {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -2,37 +2,79 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by Kyle& L
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    
+    private var fruitStore: FruitStore = FruitStore()
+    
     func makeStrawberryJuice() {
-        //
+        guard
+            fruitStore.strawberryStock >= 16
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.strawberryStock -= 16
     }
     
     func makeBananaJuice() {
-        //
+        guard
+            fruitStore.bananaStock >= 2
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.bananaStock -= 2
     }
     
     func makeKiwiJuice() {
-        //
+        guard
+            fruitStore.kiwiStock >= 3
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.kiwiStock -= 3
     }
     
     func makePineappleJuice() {
-        //
+        guard
+            fruitStore.pineappleStock >= 2
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.pineappleStock -= 2
     }
     
     func makeStrawberryBananaJuice() {
-        //
+        guard
+            fruitStore.strawberryStock >= 10,
+            fruitStore.bananaStock >= 1
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.strawberryStock -= 10
+        fruitStore.bananaStock -= 1
     }
     
     func makeMangojuice() {
-        //
+        guard
+            fruitStore.mangoStock >= 3
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.mangoStock -= 3
     }
     
-    func makeMagoKiwiJuice() {
-        //
+    func makeMangoKiwiJuice() {
+        guard
+            fruitStore.mangoStock >= 2,
+            fruitStore.kiwiStock >= 1
+        else {
+            return print("재료 소진")
+        }
+        fruitStore.mangoStock -= 2
+        fruitStore.kiwiStock -= 1
     }
 }
