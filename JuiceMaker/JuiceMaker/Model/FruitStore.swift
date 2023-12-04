192eb1cb4228624271d508069bb7d6ee2b523b92
LeeSe0ngYe0n <goblin__@naver.com>
2023-12-04 12:53:45 +0900

    feat: 과일 프로퍼티 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,9 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
 }
