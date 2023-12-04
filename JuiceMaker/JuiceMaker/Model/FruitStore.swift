eaa94f104a8405c6b8c5c02b8615cc1010f5022c
Effie <effie.thedev@gmail.com>
2023-12-04 16:32:26 +0900

    feat #1: JuiceFlavor 열거형 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -43,3 +43,32 @@ extension EachFruitStore: Hashable {
 enum JuiceMakerError: Error {
     case fruitShortage
 }
+
+enum JuiceFlavor {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+    case strawberryBanana
+    case mangoKiwi
+    
+    var recipe: [Fruits: Int] {
+        switch self {
+        case .strawberry: 
+            return [.strawberry: 16]
+        case .banana: 
+            return [.banana: 2]
+        case .pineapple: 
+            return [.pineapple: 2]
+        case .kiwi: 
+            return [.kiwi: 3]
+        case .mango: 
+            return [.mango: 3]
+        case .strawberryBanana: 
+            return [.strawberry: 10, .banana: 1]
+        case .mangoKiwi: 
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
+}
