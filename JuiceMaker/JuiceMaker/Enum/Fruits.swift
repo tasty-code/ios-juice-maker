0360451ac76784546901ca3cc7dba5f587105c85
MyNB1 <dishdib61@naver.com>
2023-12-04 11:48:13 +0900

    feat : fruitsStore 생성

---
+++ JuiceMaker/JuiceMaker/Enum/Fruits.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits.swift
+//  JuiceMaker
+//
+//  Created by 둘리 on 2023/12/04.
+//
+
+import Foundation
+
+enum Fruits {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+    
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,26 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitsInfo: [[Fruits: Int]] = [[.strawberry: 10], [.banana: 10], [.pineapple: 10], [.kiwi: 10], [.mango: 10]]
     
+    func addQuantity(fruits: Fruits) {
+        for i in 0..<fruitsInfo.count {
+            var dict = fruitsInfo[i]
+            if let value = dict[fruits] {
+                dict[fruits] = value + 1
+                fruitsInfo[i] = dict
+            }
+        }
+    }
+    
+    func deleteQuantity(fruits: Fruits) {
+        for i in 0..<fruitsInfo.count {
+            var dict = fruitsInfo[i]
+            if let value = dict[fruits] {
+                dict[fruits] = value - 1
+                fruitsInfo[i] = dict
+            }
+        }
+    }
 }
+
