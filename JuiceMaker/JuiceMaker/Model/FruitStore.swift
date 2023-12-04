e6fc0d8f0ea9ef2126ddc65c59f2a106887786d1
mireu79 <https://github.com/mireu79>
2023-12-04 17:16:15 +0900

    feat: FruitStore에서 초기재고 설정기능 구현 및 수량변경하는 기능 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,5 +6,19 @@
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitStock: [Fruit: UInt]
     
+    init(fruitStock: [Fruit : UInt] = [.banana: 10, .kiwi: 10, .mango: 10, .pineapple: 10, .strawberry: 10]) {
+        self.fruitStock = fruitStock
+    }
+    
+    func consumeFruit(juice: Juice) {
+        for (fruitName, numberOfFruits) in juice.recipe {
+            guard let stock = fruitStock[fruitName] else { return }
+            
+            fruitStock.updateValue(stock - numberOfFruits, forKey: fruitName)
+        }
+    }
 }
+
+
