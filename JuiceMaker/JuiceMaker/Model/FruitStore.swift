a7d891267eb3d6a8e57b997d2e5f4bce5f1ae020
Effie <effie.thedev@gmail.com>
2023-12-04 16:28:06 +0900

    feat #1: EachFruitStore 및 JuiceMakerError 추가
    - EachFruitStore 구조체 구현
    - EachFruitStore Hashable 구현
    - JuiceMakerError 선언 및 과일 부족 케이스 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -18,3 +18,28 @@ enum Fruits {
     case kiwi
     case mango
 }
+
+struct EachFruitStore {
+    private let fruitType: Fruits
+    
+    private var count: Int = 10
+    
+    init(fruitType: Fruits) {
+        self.fruitType = fruitType
+    }
+    
+    mutating func takeFruits(count: Int) throws {
+        guard self.count >= count else { throw JuiceMakerError.fruitShortage }
+        self.count -= count
+    }
+}
+
+extension EachFruitStore: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.fruitType)
+    }
+}
+
+enum JuiceMakerError: Error {
+    case fruitShortage
+}
