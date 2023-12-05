0411d1a5a2a88c4d2ea4449a09ef16c2c72290f9
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-05 15:51:26 +0900

    refactor: Fruits 열거형 name 변수 생성

--- JuiceMaker/JuiceMaker/Model/Fruits.swift
+++ JuiceMaker/JuiceMaker/Model/Fruits.swift
@@ -11,4 +11,19 @@ enum Fruits {
     case pineapple
     case kiwi
     case mango
+    
+    var name: String {
+        switch self {
+        case .strawberry:
+            "딸기"
+        case .banana:
+            "바나나"
+        case .pineapple:
+            "파인애플"
+        case .kiwi:
+            "키위"
+        case .mango:
+            "망고"
+        }
+    }
 }
