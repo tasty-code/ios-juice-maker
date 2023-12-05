9aec7b0f684c5c1103e125655813bff29d8db643
yoruck2 <yoruck2@gmail.com>
2023-12-05 11:30:01 +0900

    feat: fruitStore 입,출고 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -15,6 +15,24 @@ class FruitStore {
         Fruit.allCases.forEach { store[$0] = 10 }
     }
     
+    public func warehouse(fruit: Fruit, count: Int) {
+        guard let nowCount = store[fruit] else {
+            return
+        }
+        store[fruit] = nowCount + count
+    }
     
+    public func release(fruit: Fruit, count: Int) -> Bool {
+        guard let nowCount = store[fruit] else {
+            return false
+        }
+        
+        if nowCount < count {
+            return false
+        }
+        
+        store[fruit] = nowCount - count
+        return true
+    }
     
 }
