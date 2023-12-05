240dbdc697b23e36889aa46635b36a4b67ad0cc2
jus1234 <osjamg1996@gmail.com>
2023-12-05 10:57:44 +0900

    feat/JuicMenu name 변수 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 장우석 on 12/5/23.
+//
+
+import Foundation
+
+enum Fruit: String {
+    case strawberry = "딸기"
+    case banana = "바나나"
+    case kiwi = "키위"
+    case pineapple = "파인애플"
+    case mango = "망고"
+}
