97550273e55f7282d6dab39554af9f3c21f545dc
hemil0102 <hemil0102@gmail.com>
2023-12-05 10:45:21 +0900

    feat: FruitStore 타입 정의 및 모델링 구현 (#1)

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    static let shared = FruitStore()
     
+    var strawberry = 10
+    var banana = 10
+    var kiwi = 10
+    var pineapple = 10
+    var mango = 10
+    
+    private init() {}
 }
