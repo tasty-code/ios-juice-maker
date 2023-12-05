21a9d1d656f6cd659e00f5147eb68cc820398b10
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-12-05 15:56:01 +0900

    feat: 과일별 재고 저장 및 초기화

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,20 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var stock = [String: Int]()
+    init() {
+        stock = [
+            "strawberry": 10,
+            "banana": 10,
+            "pineapple": 10,
+            "kiwi": 10,
+            "mango": 10
+        ]
+    }
+    
+    func changeStock(){
+        
+    }
     
 }
+
