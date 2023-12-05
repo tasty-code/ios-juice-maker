3c6a61997f9afed80dbadf4b51e8602f7657393c
eunjuchoi <eunjuzzoo@naver.com>
2023-12-05 16:14:57 +0900

    feat: 쥬스 enum 타입 정의 (#1)

---
+++ JuiceMaker/JuiceMaker/Constant/Juice.swift
@@ -0,0 +1,19 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by EUNJU on 2023/12/05.
+//
+
+import Foundation
+
+@frozen
+enum Juice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mango
+    case mangoKiwi
+}
