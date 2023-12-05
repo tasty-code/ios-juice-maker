1bafa9cb9a797b5e829a038e639169790ea1aa5f
eunjuchoi <eunjuzzoo@naver.com>
2023-12-05 11:34:43 +0900

    feat: UIViewController + Extension 파일 추가 (#1)

---
+++ JuiceMaker/JuiceMaker/Extension/UIViewController + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController + Extension.swift
+//  JuiceMaker
+//
+//  Created by EUNJU on 2023/12/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+    var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
