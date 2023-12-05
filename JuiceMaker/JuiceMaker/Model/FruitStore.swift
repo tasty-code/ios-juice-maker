8bae38b314383e23d39c62168c90ae497aa3da96
yoruck2 <yoruck2@gmail.com>
2023-12-05 11:21:46 +0900

    feat: 저장소 변수, 생성자 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    private var store: Dictionary<Fruit, Int>
+    
+    init() {
+        self.store = Dictionary()
+        Fruit.allCases.forEach { store[$0] = 10 }
+    }
+    
+    
     
 }
