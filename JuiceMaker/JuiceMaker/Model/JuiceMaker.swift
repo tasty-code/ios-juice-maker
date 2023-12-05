bca01a943f01aa5cd0c38acade77f37fdd223444
leevwe@naver.com <leevwe@naver.com>
2023-12-05 11:28:02 +0900

    feat: 타입 정의 및 기능 구현 기초 작업

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,33 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+    var fruitStore: FruitStore
+
+    init(store: FruitStore) {
+        fruitStore = store
+    }
+
+    func makeJuice(type: String) -> Bool {
+        if type == "Strawberry" {
+            return fruitStore.removeFruit("strawberry", quantity: 16)
+        } else if type == "Banana" {
+            return fruitStore.removeFruit("banana", quantity: 2)
+        } else if type == "Kiwi" {
+            return fruitStore.removeFruit("kiwi", quantity: 3)
+        } else if type == "Pineapple" {
+            return fruitStore.removeFruit("pineapple", quantity: 2)
+        } else if type == "strawberryAndBanana" {
+            let strawberrySuccess = fruitStore.removeFruit("strawberry", quantity: 10)
+            let bananaSuccess = fruitStore.removeFruit("banana", quantity: 1)
+            return strawberrySuccess && bananaSuccess
+        } else if type == "Mango" {
+            return fruitStore.removeFruit("mango", quantity: 3)
+        } else if type == "Mango Kiwi" {
+            let mangoSuccess = fruitStore.removeFruit("mango", quantity: 2)
+            let kiwiSuccess = fruitStore.removeFruit("kiwi", quantity: 1)
+            return mangoSuccess && kiwiSuccess
+        } else {
+            return false
+        }
+    }
 }
