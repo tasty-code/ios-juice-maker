9a67731bd9ed072ca3f8d2e4c15ec4f099a24290
wlsdud-dev <hjymail1025@gmail.com>
2023-12-05 16:10:07 +0900

    feat: 쥬스제조 레시피 메소드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,30 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private var fruitStore = FruitStore()
     
+    mutating func makeJuice(juice: String) {
+        let recipe = juiceRecipe(juice: juice)
+    }
+    
+    private func juiceRecipe(juice: String) -> [String: Int] {
+        switch juice {
+        case "strawberry":
+            return ["strawberry": 16]
+        case "banana":
+            return ["banana": 2]
+        case "pineapple":
+            return ["pineapple": 2]
+        case "kiwi":
+            return ["kiwi": 3]
+        case "mango":
+            return ["mango": 3]
+        case "strawberrybanana":
+            return ["strawberry": 10,"banana": 1]
+        case "mangokiwi":
+            return ["mango": 2, "kiwi": 1]
+        default:
+            return [:]
+        }
+    }
 }
