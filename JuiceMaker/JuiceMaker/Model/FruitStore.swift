80b325d58b9981a4eb71d6b6321c1c1f5ea7ce27
ujhong7 <ujhong7>
2023-12-05 17:02:52 +0900

    [feat] 과일 초기 재고 및 수량 변화 설정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,30 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
+    var inventory: [Fruit:Int] = [:]
+
+      init() {
+          for fruit in Fruit.allCases {
+              let firstAmount = 10
+              inventory[fruit] = firstAmount
+          }
+      }
+
+      func increaseStock(fruit: Fruit, num: Int) {
+          let result = inventory[fruit]! + num
+          inventory[fruit] = result
+      }
+
+      func decreaseStock(fruit: Fruit, num: Int) {
+          let result = inventory[fruit]! - num
+          inventory[fruit] = result
+      }
+
+      func updateStock(fruit: Fruit, num: Int) {
+          inventory.updateValue(num, forKey: fruit)
+      }
+    
+    
+    
+    
 }
