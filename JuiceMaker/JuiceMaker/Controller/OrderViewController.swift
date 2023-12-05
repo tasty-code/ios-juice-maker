f61e18690eddb7c9e4ce5cb984b6f0912f4eb571
hemil0102 <hemil0102@gmail.com>
2023-12-05 15:49:24 +0900

    feat: 초기 화면 과일 Label과 데이터 연결(#1)

--- JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
@@ -7,9 +7,18 @@
 import UIKit
 
 class OrderViewController: UIViewController {
-
+    @IBOutlet weak var strawberryQuantityLabel: UILabel!
+    @IBOutlet weak var bananaQuantityLabel: UILabel!
+    @IBOutlet weak var pineappleQuantityLabel: UILabel!
+    @IBOutlet weak var kiwiQuantityLabel: UILabel!
+    @IBOutlet weak var mangoQuantityLabel: UILabel!
+    
+    let fruitStore = FruitStore.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configUI()
     }
 
     @IBAction func tapEditInventoryButton(_ sender: UIBarButtonItem) {
@@ -17,6 +26,14 @@ class OrderViewController: UIViewController {
                 as? UIViewController else { return }
         
         self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    func configUI() {
+        strawberryQuantityLabel.text = String(fruitStore.strawberry)
+        bananaQuantityLabel.text = String(fruitStore.banana)
+        pineappleQuantityLabel.text = String(fruitStore.pineapple)
+        kiwiQuantityLabel.text = String(fruitStore.kiwi)
+        mangoQuantityLabel.text = String(fruitStore.mango)
     }
 }
 
