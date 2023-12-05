b079035a834cbac9a07911a458c1a57831d2f8ba
eensungkim <eensungkim@gmail.com>
2023-12-05 16:44:40 +0900

    feat : JuiceMaker 타입 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,30 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    enum Menu {
+        case 딸기쥬스, 바나나쥬스, 키위쥬스, 파인애플쥬스, 딸바쥬스, 망고쥬스, 망고키위쥬스
+    }
     
+    var fruitStore = FruitStore()
+    
+    func makeJuice(menu: Menu) {
+        switch menu {
+        case .딸기쥬스:
+            fruitStore.consumeFruit(for: .strawberry, by: 16)
+        case .바나나쥬스:
+            fruitStore.consumeFruit(for: .banana, by: 2)
+        case .키위쥬스:
+            fruitStore.consumeFruit(for: .kiwi, by: 3)
+        case .파인애플쥬스:
+            fruitStore.consumeFruit(for: .pineapple, by: 2)
+        case .딸바쥬스:
+            fruitStore.consumeFruit(for: .strawberry, by: 10)
+            fruitStore.consumeFruit(for: .banana, by: 1)
+        case .망고쥬스:
+            fruitStore.consumeFruit(for: .mango, by: 3)
+        case .망고키위쥬스:
+            fruitStore.consumeFruit(for: .mango, by: 2)
+            fruitStore.consumeFruit(for: .kiwi, by: 1)
+        }
+    }
 }
