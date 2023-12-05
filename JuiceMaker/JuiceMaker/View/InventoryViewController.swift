6ff30984d87bb82e0e104f96efa42e5cf7fac2ae
eunjuchoi <eunjuzzoo@naver.com>
2023-12-05 11:37:40 +0900

    feat: InventoryViewController 파일 추가 (#1)

---
+++ JuiceMaker/JuiceMaker/View/InventoryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InventoryViewController.swift
+//  JuiceMaker
+//
+//  Created by EUNJU on 2023/12/05.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
