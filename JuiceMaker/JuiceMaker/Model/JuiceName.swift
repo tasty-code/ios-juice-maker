6489ec916e24cc4f7b824683d55c654045f6e1f3
AsherKim <tawans@naver.com>
2023-12-05 11:19:28 +0900

    feat(JuiceName): ENUM JuiceName 생성

---
+++ JuiceMaker/JuiceMaker/Model/JuiceName.swift
@@ -0,0 +1,18 @@
+//
+//  JuiceName.swift
+//  JuiceMaker
+//
+//  Created by Taewan Kim on 12/5/23.
+//
+
+import Foundation
+
+enum JuiceName {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case straberryBanana
+    case mango
+    case mangoKiwi
+}
