4d17962385b57ecdd999cc2dde8a07cc05963611
Hoi.Kwon <teaho177@gmail.com>
2023-12-05 11:35:57 +0900

    feat: 초기 재고값 dictionary추가 및 재고 확인 메서드 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,52 +8,20 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    var strawberryCount: Int
-    var bananaCount: Int
-    var pineappleCount: Int
-    var kiwiCount: Int
-    var mangoCount: Int
-
-    init() {
-        strawberryCount = 10
-        bananaCount = 10
-        pineappleCount = 10
-        kiwiCount = 10
-        mangoCount = 10
-    }
-
-    func addFruit(_ fruit: String, quantity: Int) {
-        if fruit == "strawberry" {
-            strawberryCount += quantity
-        } else if fruit == "banana" {
-            bananaCount += quantity
-        } else if fruit == "pineapple" {
-            pineappleCount += quantity
-        } else if fruit == "kiwi" {
-            kiwiCount += quantity
-        } else if fruit == "mango" {
-            mangoCount += quantity
+    private var fruitInventory: [String: Int] = [
+        "딸기": 10,
+        "바나나": 10,
+        "파인애플": 10,
+        "키위": 10,
+        "망고": 10
+    ]
+    func updateFruitQuantity(fruit: String, quantity: Int) {
+        if let currentQuantity = fruitInventory[fruit] {
+            fruitInventory[fruit] = max(0,currentQuantity + quantity)
         }
     }
-
-    func removeFruit(_ fruit: String, quantity: Int) -> Bool {
-        if fruit == "strawberry" && strawberryCount >= quantity {
-            strawberryCount -= quantity
-            return true
-        } else if fruit == "banana" && bananaCount >= quantity {
-            bananaCount -= quantity
-            return true
-        } else if fruit == "pineapple" && pineappleCount >= quantity {
-            pineappleCount -= quantity
-            return true
-        } else if fruit == "kiwi" && kiwiCount >= quantity {
-            kiwiCount -= quantity
-            return true
-        } else if fruit == "mango" && mangoCount >= quantity {
-            mangoCount -= quantity
-            return true
-        } else {
-            return false
-        }
-    }
+    
+    
+    
 }
+
