8bf58daab3f5b02b293f443e67620e4db859f5d7
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-05 15:53:32 +0900

    test: JuiceMaker 테스트 코드 작성 및 실행

--- JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
+++ JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
@@ -6,30 +6,47 @@
 //
 
 import XCTest
-
+@testable import JuiceMaker
 final class JuiceMakerTests: XCTestCase {
-
+    private var sut: JuiceMaker!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = JuiceMaker()
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_딸기쥬스를_만들수_있는_재고가_없다() {
+        let result = sut.hasEnoughStock(juice: .strawberryJuice)
+        
+        XCTAssertEqual(result, false, "딸기 재고 없음! 테스트 성공")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_바나나쥬스를_만들수_있는_재고가_있다() {
+        let result = sut.hasEnoughStock(juice: .bananaJuice)
+        
+        XCTAssertEqual(result, true, "바나나 재고 있음! 테스트 성공")
     }
-
+    
+    func test_딸기_기본재고_10개로_쥬스를_만들수_없다() {
+        sut.makeJuice(juice: .strawberryJuice)
+    }
+    
+    func test_망고키위쥬스를_만드는데_망고_키위_둘다_재고가_없다() {
+        sut.fruitStore.mangoStock = 0
+        sut.fruitStore.kiwiStock = 0
+        
+        sut.makeJuice(juice: .mangoKiwiJuice)
+    }
+    
+    func test_망고쥬스를_만들고_남은_망고는_7개다() {
+        sut.makeJuice(juice: .mangoJuice)
+        let result: Int = 7
+        let remainMango: Int = sut.fruitStore.mangoStock
+        
+        XCTAssertEqual(result, remainMango, "망고쥬스를 만들고 남은 망고는 7개 입니다! 테스트 성공")
+    }
 }
