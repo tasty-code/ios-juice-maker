a64968518ef781c4d42025a9187ddbbd753290f2
hemil0102 <hemil0102@gmail.com>
2023-12-05 16:44:06 +0900

    feat: 쥬스 제조 버튼 기능 구현 (#1)

--- JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
@@ -7,13 +7,15 @@
 import UIKit
 
 class OrderViewController: UIViewController {
+    
+    let fruitStore = FruitStore.shared
+    let juiceMaker = JuiceMaker()
+    
     @IBOutlet weak var strawberryQuantityLabel: UILabel!
     @IBOutlet weak var bananaQuantityLabel: UILabel!
     @IBOutlet weak var pineappleQuantityLabel: UILabel!
     @IBOutlet weak var kiwiQuantityLabel: UILabel!
     @IBOutlet weak var mangoQuantityLabel: UILabel!
-    
-    let fruitStore = FruitStore.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,60 @@ class OrderViewController: UIViewController {
                 as? UIViewController else { return }
         
         self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    
+    @IBAction func tapMakeJuiceButton(_ sender: UIButton) {
+        guard let selectedJuice = sender.currentTitle else {
+            return
+        }
+        
+        switch selectedJuice {
+        case "딸기쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .strawberry)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "바나나쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .banana)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "키위쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .kiwi)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "파인애플쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .pineapple)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "딸바쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .strawberryBanana)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "망고쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .mangoKiwi)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        case "망키쥬스 주문":
+            do {
+                try juiceMaker.makeJuice(juiceType: .strawberry)
+            } catch {
+                print("재고가 없습니다.")
+            }
+        default:
+            print("")
+        }
     }
     
     func configUI() {
