71b1fc91e99a2104036b731bf68841e38ca9cb68
eunjuchoi <eunjuzzoo@naver.com>
2023-12-05 11:33:27 +0900

    chore: 타입 추론 -> 타입 어노테이션 방식으로 수정 (#1)

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,13 +8,14 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    
     static let shared = FruitStore()
     
-    var strawberry = 10
-    var banana = 10
-    var kiwi = 10
-    var pineapple = 10
-    var mango = 10
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var kiwi: Int = 10
+    var pineapple: Int = 10
+    var mango: Int = 10
     
     private init() {}
 }
