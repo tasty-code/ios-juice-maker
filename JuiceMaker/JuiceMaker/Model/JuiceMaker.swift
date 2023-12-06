224bac402cf641a541602a78f3f74ac388027319
Nnn <jaehyung1299@naver.com>
2023-12-06 17:01:53 +0900

    Feat: 과일쥬스만들 때 소모되는 과일 수 매서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,4 +8,11 @@ import Foundation
 
 struct JuiceMaker {
     
+    func useFruits(fruit: FruitType.RawValue, quantity: Int) throws {
+        let currentQuantity = fruitStore.getQuantity(of: FruitType(rawValue: fruit) ?? .banana)
+        guard currentQuantity >= quantity else {
+            throw ErrorType.insufficientFruits
+        }
+        fruitStore.updateInventory(fruit: FruitType(rawValue: fruit) ?? .banana, quantity: -quantity)
+    }
 }
