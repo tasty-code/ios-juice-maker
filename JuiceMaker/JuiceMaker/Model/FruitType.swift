7d5f7c3743bea8fb7082cdb25426f6a7c393e056
rlwhs12 <150293158+rlwhs12@users.noreply.github.com>
2023-12-06 15:39:52 +0900

    feat: FruitType파일 생성 및 fruit enum생성

---
+++ JuiceMaker/JuiceMaker/Model/FruitType.swift
@@ -0,0 +1,32 @@
+//
+//  FruitType.swift
+//  JuiceMaker
+//
+//  Created by MAC2020 on 2023/12/06.
+//
+
+import Foundation
+
+enum FruitType {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case mango
+    
+    var FruitName:String {
+        switch self {
+        case .strawberry:
+            return "딸기"
+        case .banana:
+            return "바나나"
+        case .kiwi:
+            return "키위"
+        case .pineapple:
+            return "파인애플"
+        case .mango:
+            return "망고"
+        }
+    }
+}
+
