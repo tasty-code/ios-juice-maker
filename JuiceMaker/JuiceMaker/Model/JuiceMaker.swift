bd76420c2d035fd0b634c5ed16e4c1997ed192c3
Nnn <jaehyung1299@naver.com>
2023-12-06 17:03:01 +0900

    Feat: 쥬스만드는 메서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,6 +7,30 @@
 import Foundation
 
 struct JuiceMaker {
+    let fruitStore = FruitStore()
+    
+    func makeJuice(_ juice: JuiceType.RawValue) throws {
+        switch juice {
+        case "딸기쥬스":
+            try useFruits(fruit: "딸기", quantity: 16)
+        case "바나나쥬스":
+            try useFruits(fruit: "바나나", quantity: 2)
+        case "키위쥬스":
+            try useFruits(fruit: "키위", quantity: 3)
+        case "파인애플쥬스":
+            try useFruits(fruit: "파인애플", quantity: 2)
+        case "딸기바나나쥬스":
+            try useFruits(fruit: "딸기", quantity: 10)
+            try useFruits(fruit: "바나나", quantity: 1)
+        case "망고쥬스":
+            try useFruits(fruit: "망고", quantity: 3)
+        case "망고키위쥬스":
+            try useFruits(fruit: "망고", quantity: 2)
+            try useFruits(fruit: "키위", quantity: 1)
+        default:
+            ErrorType.insufficientFruits
+        }
+    }
     
     func useFruits(fruit: FruitType.RawValue, quantity: Int) throws {
         let currentQuantity = fruitStore.getQuantity(of: FruitType(rawValue: fruit) ?? .banana)
