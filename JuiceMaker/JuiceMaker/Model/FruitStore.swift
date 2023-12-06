2dad46e13f57692dc44c55560b4122656f6186ec
AsherKim <tawans@naver.com>
2023-12-06 10:36:59 +0900

    fix(FruitStore): 닐 코어레싱으로 oldQuantity 넣는 부분 guard let으로 변경

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -11,11 +11,15 @@ final class FruitStore {
     private var quantityOfAllFruits: [FruitName: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     
     func consumeFruit(fruitName: FruitName, fruitQuantity quantity: Int) {
-        let oldQuantity = quantityOfAllFruits[fruitName] ?? 0
+        guard let oldQuantity = quantityOfAllFruits[fruitName] else {
+            return
+        }
+        
         guard oldQuantity >= quantity else {
             print("과일 재고 부족")
             return
         }
+        
         let newQuantity = oldQuantity - quantity
         quantityOfAllFruits[fruitName] = newQuantity
     }
