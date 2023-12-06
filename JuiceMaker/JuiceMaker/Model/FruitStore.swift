694803ab822c14eb791d01b5cb899da52a33b10e
kimdj1102 <djkim1102@naver.com>
2023-12-06 16:47:11 +0900

    [feat] 재료없음 메세지 출력 enum추가 및 재료관련 함수들 추가 checkStock() checkIngredients() useIngredients() consumeStocks()

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,6 +8,11 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum InventoryError : Error {
+            static let outOfStockMessage = "재료 없음. 재고를 수정할까요?"
+
+            case inventoryError(description: String)
+        }
     
     var inventory: [Fruit:Int] = [:]
 
@@ -31,4 +36,34 @@ class FruitStore {
       func updateStock(fruit: Fruit, num: Int) {
           inventory.updateValue(num, forKey: fruit)
       }
+    
+    // 재고 체킹 함수 checkStock
+    // amountOfFruitNow: 현재 과일의 수   amountRequired: 필요한 과일의 수
+    func checkStock(amountOfFruitNow: Int, amountRequired: Int) throws {
+        guard amountOfFruitNow > amountRequired else {
+            throw InventoryError.inventoryError(description: InventoryError.outOfStockMessage)
+        }
+    }
+    
+    // 재료 체킹 함수 checkIngredients
+    func checkIngredients() {
+        
+    }
+    
+    // 재료 사용 함수 useIngredients
+    func useIngredients() {
+        
+    }
+    
+    // 주스 produce 성공시 재고 소비, 에러뜨면 catch문으로
+    func consumeStocks(_ recipes: [(requiredFruit: Fruit, requestedAmount: Int)]) throws {
+        // 필요한 재료의 수량 체크해서 과일 몇개인지
+        let amountOfFruit = try checkIngredients()
+    
+        useIngredients()
+    }
 }
+
+// 만들기    -> / 재고 소비       ->      재료 체크     (재고 체크)     -> 재료 사용        -> 재고 감소
+// JuiceMaker /    FruitStore
+// produce -> / consumeStocks -> checkIngredients (checkStock) -> useIngredients -> decreaseStock
