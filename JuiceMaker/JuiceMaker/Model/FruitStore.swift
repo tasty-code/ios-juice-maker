b37cad3028cc114e0bc55afdcb80cd287dc641b2
Nnn <jaehyung1299@naver.com>
2023-12-06 16:56:09 +0900

    Feat: 과일 갯수 업데이트 메서드 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,5 +7,14 @@
 import Foundation
 
 class FruitStore {
+    var inventory: [FruitType: Int]
+    
+    init() {
+        inventory = [.banana: 10, .kiwi: 10, .mango: 10, .pineapple: 10, .strawberry: 10]
+    }
+    func updateInventory(fruit: FruitType, quantity: Int) {
+        guard let currentQuantity = inventory[fruit] else { return }
+        inventory[fruit] = currentQuantity + quantity
+    }
     
 }
