2a190f4f84c929e43734049267313a49118e01e3
Nnn <jaehyung1299@naver.com>
2023-12-06 17:03:22 +0900

    chore: 필요없는 주석 제거

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -12,12 +12,12 @@ class FruitStore {
     init() {
         inventory = [.banana: 10, .kiwi: 10, .mango: 10, .pineapple: 10, .strawberry: 10]
     }
-    // 특정 과일의 재고을 업데이트 하는 메서드
+
     func updateInventory(fruit: FruitType, quantity: Int) {
         guard let currentQuantity = inventory[fruit] else { return }
         inventory[fruit] = currentQuantity + quantity
     }
-    // 특정 과일의 현재 재고 수량을 반환하는 메서드
+
     func getQuantity(of fruit: FruitType) -> Int {
         return inventory[fruit, default: 0]
     }
