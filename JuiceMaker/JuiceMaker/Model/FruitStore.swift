12c3883de7067bdbb351e3961abac806069c4def
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2023-12-06 15:53:37 +0900

    feat: 과일 초기값 설정, 과일을 추가할 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,6 +7,17 @@
 import Foundation
 
 // 과일 저장소 타입
-class FruitStore {
+final class FruitStore {
     
+    var fruitInventory: [FruitType: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .kiwi: 10,
+        .pineapple: 10,
+        .mango: 10
+    ]
+    
+    func addFruitInvetory(for fruit: FruitType, inventory: Int) {
+        fruitInventory[fruit, default: 0] += inventory
+    }
 }
