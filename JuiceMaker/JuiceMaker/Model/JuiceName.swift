324f1a276f7672ef81fb1565b41ce33c65b72042
ijs1103 <ajaijs1103@naver.com>
2023-12-06 15:53:49 +0900

    chore(JuiceName): 쥬스별 과일 종류와 재고를 가지는 computed properties 추가

--- JuiceMaker/JuiceMaker/Model/JuiceName.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceName.swift
@@ -15,4 +15,23 @@ enum JuiceName {
     case straberryBanana
     case mango
     case mangoKiwi
+    
+    var recipe: [FruitName: Int] {
+        switch self {
+        case .strawberry:
+            return [.strawberry: 16]
+        case .banana:
+            return [.banana: 12]
+        case .kiwi:
+            return [.kiwi: 3]
+        case .pineapple:
+            return [.pineapple: 2]
+        case .straberryBanana:
+            return [.strawberry: 10, .banana: 1]
+        case .mango:
+            return [.mango: 3]
+        case .mangoKiwi:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
 }
