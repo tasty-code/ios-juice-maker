87172a0090ac2cdb58f97d39fe420b00226d4520
ujhong7 <ujhong7>
2023-12-07 15:41:32 +0900

    [chore] 주석 제거

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-// 과일 저장소 타입
 class FruitStore {
     enum InventoryError : Error {
             static let outOfStockMessage = "재료 없음. 재고를 수정할까요?"
@@ -14,45 +13,36 @@ class FruitStore {
         
             case inventoryError(description: String)
         }
-    
-    // ⭐️⭐️⭐️⭐️⭐️
-    // reduce -> '시퀀스의 각 요소'에 대해 제공된 클로저를 사용하여 값을 결합 reduce(초기값) 여기에서는 빈 딕셔너리임 ([Fruit: Int]())
+
     private(set) var inventory = Fruit.allCases.reduce([Fruit: Int]()) { bag, fruit in
-        var bag = bag // 클로저 내에서 값을 직접 변경할 수 없기 때문에 새로운 변수에 값을 할당
+        var bag = bag
         let initialNumberOfFruits = 10
-        bag[fruit] = initialNumberOfFruits // 현재 순회 중인 과일에 대한 재고를 딕셔너리에 추가합니다.
-        return bag // 없데이트된 딕셔너리를 반환 -> reduce 함수는 이 반환값을 다음 순회에 사용하여 값을 누적합니다. -> inventory 프로퍼티에 할당되어 해당 과일의 초기 재고를 나타내게 됩니다.
+        bag[fruit] = initialNumberOfFruits
+        return bag
     }
 
-    // 과일의 수량 n개를 변경하는 함수
     func changeAmount(_ fruit: Fruit, _ number: Int) {
         inventory[fruit] = number
     }
     
-    // 재고 증가 함수
     func increaseStock() {
         
     }
     
-    // 재고 감소 함수
     func decreaseStock() {
         
     }
     
-    // 재료 사용 함수 useIngredients
     func useIngredients() {
         decreaseStock()
     }
     
-    // 재고 체킹 함수 checkStock
-    // amountOfFruitNow: 현재 과일의 수   amountRequired: 필요한 과일의 수
     func checkStock(amountOfFruitNow: Int, amountRequired: Int) throws {
         guard amountOfFruitNow > amountRequired else {
             throw InventoryError.inventoryError(description: InventoryError.outOfStockMessage)
         }
     }
     
-    //
     func giveBackNumberIfExist(_ fruit: Fruit) throws -> Int {
         guard let numberOfFruitExist = inventory[fruit] else {
             throw InventoryError.inventoryError(description: InventoryError.fruitThatDoesNotExistMessage)
@@ -60,7 +50,6 @@ class FruitStore {
         return numberOfFruitExist
     }
     
-    // 재료 체킹 함수 checkIngredients
     func checkIngredients(_ recipes: [(requiredFruit: Fruit, requestedAmount: Int)]) throws -> [Int] {
         var numberOfFruitExist = [Int]()
         
@@ -73,16 +62,10 @@ class FruitStore {
         return numberOfFruitExist
     }
     
-    // 주스 produce 성공시 재고 소비, 에러뜨면 catch문으로
     func consumeStocks(_ recipes: [(requiredFruit: Fruit, requestedAmount: Int)]) throws {
-        // 필요한 재료의 수량 체크해서 과일 몇개인지
         let amountOfFruit = try checkIngredients(recipes)
         
         useIngredients()
     }
     
 }
-
-// 만들기    -> / 재고 소비       ->      재료 체크     (재고 체크)     -> 재료 사용        -> 재고 감소
-// JuiceMaker /    FruitStore
-// produce -> / consumeStocks -> checkIngredients (checkStock) -> useIngredients -> decreaseStock
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,11 +6,8 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
 struct JuiceMaker {
     enum JuiceMenu: String, CustomStringConvertible {
-        // CustomStringConvertible -> 사용자 지정된 문자열 표현을 제공할 수 있도록 하는 기능을 제공합니다.
-        // description이라는 속성을 구현해야 합니다. 이 속성은 '해당 타입의 인스턴스'를 '문자열'로 나타내는 데 사용됩니다.
         case strawberry = "딸기 쥬스"
         case banana = "바나나 쥬스"
         case pineapple = "파인애플 쥬스"
@@ -19,12 +16,10 @@ struct JuiceMaker {
         case strawberryBanana = "딸바 쥬스"
         case mangoKiwi = "망키 쥬스"
         
-        // JuiceMenu 열거형의 '각 케이스'를 '문자열'로 표현하기 위한 사용자 정의된 표현을 제공하는 부분입니다.
         var description: String {
             self.rawValue
         }
         
-        // 쥬스 레시피 받는 함수
         fileprivate func receiveRecipes() -> [(requiredFruit: Fruit, requestedAmount: Int)] {
             var recipe: [(Fruit, Int)]
             
@@ -48,19 +43,16 @@ struct JuiceMaker {
         }
     }
     
-    // 주스메이킹 결과
     enum JuiceMakingResult {
         case success(message: String)
         case failure(description: String)
     }
     
-    // JuiceMaker는 FruitStore를 소유
     let fruitStore: FruitStore
     
-    // 주스 제조
     func produce(_ menuName: JuiceMenu) -> JuiceMakingResult {
         let recipes = menuName.receiveRecipes()
-        var outcomeCreated: JuiceMakingResult // 메이킹 결과 <성공/실패>
+        var outcomeCreated: JuiceMakingResult 
         
         do {
             try fruitStore.consumeStocks(recipes)
