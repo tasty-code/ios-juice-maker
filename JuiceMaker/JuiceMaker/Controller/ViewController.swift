b9a4d8b21cd7d07ae016f653f8c3f4e6ba78ea56
suojae3 <suojae3@naver.com>
2023-12-07 12:50:17 +0900

    feat: 쥬스머신 뷰컨과 스토리보드 버튼 액션 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -1,18 +1,44 @@
-//
-//  JuiceMaker - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
 
 import UIKit
 
-class ViewController: UIViewController {
 
+//MARK: - JuiceMachineViewController 초기화
+class JuiceMachineViewController: UIViewController {
+    
+    let juiceMachineView = JuiceMachineView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //Button Action Method
+    @IBAction func ddalbaJuiceOrderButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mangkiJuiceButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func strawberryJuiceOrderButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func bananaJuiceOrderButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pineappleJuiceOrderButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func kiwiJuiceOrderButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mangoJuiceButtonTapped(_ sender: UIButton) {
+        
+    }
 }
-
