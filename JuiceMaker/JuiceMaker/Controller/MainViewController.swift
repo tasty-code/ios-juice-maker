6ec2443233b181ed067e15c465c41f5e5e13efb6
crisine <crisine@knou.ac.kr>
2023-12-07 14:09:46 +0900

    feat: 쥬스 제조 시 과일 재고가 부족한 경우 Alert 창을 표시하도록 구현

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -22,9 +22,8 @@ class MainViewController: UIViewController {
     }
 
 
-    @IBAction func ManageStockButton(_ sender: UIButton) {
-        guard let viewController = self.storyboard?.instantiateViewController(identifier: "viewController") as? ManageStockViewController else { return }
-        self.navigationController?.pushViewController(viewController, animated: true)
+    @IBAction func manageStockButton(_ sender: UIButton) {
+        moveToManageStockView()
     }
     
     @IBAction func checkJuiceOrder(_ sender: UIButton) {
@@ -34,14 +33,18 @@ class MainViewController: UIViewController {
         orderJuice(juice: juice)
     }
     
+    func moveToManageStockView() {
+        guard let viewController = self.storyboard?.instantiateViewController(identifier: "viewController") as? ManageStockViewController else { return }
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
     func orderJuice(juice: Juice) {
         do {
-            print("재료 준비")
             try juiceMaker.orderJuice(juice: juice)
-            print("\(juice.name) 주문 성공!!!")
+            showSuccessAlert(juiceName: juice.name)
             checkFruitsStock()
         } catch {
-            print(error)
+            showOutOfStockAlert()
         }
     }
     
@@ -51,5 +54,28 @@ class MainViewController: UIViewController {
         pineappleLabel.text = String(juiceMaker.fruitStore.fruitStock[.pineapple] ?? 0)
         kiwiLabel.text = String(juiceMaker.fruitStore.fruitStock[.kiwi] ?? 0)
         mangoLabel.text = String(juiceMaker.fruitStore.fruitStock[.mango] ?? 0)
+    }
+    
+    func showOutOfStockAlert() {
+        let alert = UIAlertController(title: nil, message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "예", style: .default) { _ in
+            self.moveToManageStockView()
+        }
+        let cancelAction = UIAlertAction(title: "아니오", style: .default)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showSuccessAlert(juiceName: String) {
+        let alert = UIAlertController(title: nil, message: "\(juiceName) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
