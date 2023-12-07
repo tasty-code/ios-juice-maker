b31bad2f247c8cf49daf9349d8225f47da1217fa
Effie <effie.thedev@gmail.com>
2023-12-07 13:35:43 +0900

    chore: STEP1 리뷰 사항 반영 1차
    - 외부에서 안 쓰는 속성 접근 제한
    - import Foundation 제거
    - FruitStore init 구현 내 상수 이름 변경
    - FruitStore getFruits 메소드 이름 변경
    - 템플릿 주석 제거

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,22 +4,20 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
-// 과일 저장소 타입
 final class FruitStore {
     private var fruitStocks: [Fruit: FruitStock]
     
     init() {
-        let newFruits: [Fruit: FruitStock] = Fruit.allCases.reduce(into: [:]) { (result, fruit) in
+        let newStocks: [Fruit: FruitStock] = Fruit.allCases.reduce(into: [:]) { (result, fruit) in
             let stock = FruitStock(fruitType: fruit)
             result[fruit] = stock
         }
-        self.fruitStocks = newFruits
+        self.fruitStocks = newStocks
     }
     
-    func getFruits(_ fruit: Fruit, count: Int) throws {
-        guard let targetFruitStore = fruitStocks[fruit] else { return }
-        try targetFruitStore.consumeFruits(count: count)
+    
+    func consume(_ fruitType: Fruit, numberOfFruits: Int) throws {
+        guard let targetFruitStore = fruitStocks[fruitType] else { return }
+        try targetFruitStore.consumeFruits(count: numberOfFruits)
     }
 }
