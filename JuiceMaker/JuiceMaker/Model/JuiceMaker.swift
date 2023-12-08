3e1eea0739bb0d07bebddc8678b1a5ac46f6eab6
ijs1103 <ajaijs1103@naver.com>
2023-12-08 19:37:27 +0900

    chore(JuiceMaker): makeJuice 메서드 로직 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,23 +10,33 @@ import Foundation
 struct JuiceMaker {
     let fruitStore: FruitStore = FruitStore()
     
-    func makeJuice(juiceMenu: JuiceMenu) -> Result<Void, JuiceError> {
-        guard let fruitName = juiceMenu.recipe.keys.first, let fruitQuantity = juiceMenu.recipe.values.first else {
-            return .failure(JuiceError.quantityOfAllFruitsAccessFailed)
-        }
-        do {
-            try fruitStore.consumeFruit(fruitName: fruitName, fruitQuantity: fruitQuantity)
-            // 쥬스 제조 성공
-            NotificationCenter.default.post(name: NSNotification.Name("JuiceMade"), object: nil)
-            print("Successfully made \(juiceMenu.recipe) juice")
-            return .success(())
-        } catch JuiceError.outOfStock {
-            return .failure(JuiceError.outOfStock)
-        } catch {
-            // 다른 예외적인 에러 처리
-            return .failure(JuiceError.unknown)
+    func makeJuice(juiceMenu: JuiceMenu) {
+        for (fruitName, fruitQuantity) in juiceMenu.recipe {
+            consumeFruit(fruitName: fruitName, fruitQuantity: fruitQuantity)
         }
     }
     
+    private func consumeFruit(fruitName: FruitName, fruitQuantity: Int) {
+        do {
+            try fruitStore.consumeFruit(fruitName: fruitName, fruitQuantity: fruitQuantity)
+        } catch JuiceError.outOfStock {
+            print(JuiceError.outOfStock.localizedDescription)
+        } catch {
+            print(JuiceError.unknown.localizedDescription)
+        }
+    }
+    
+//    do {
+//        try fruitStore.consumeFruit(fruitName: fruitName, fruitQuantity: fruitQuantity)
+//        // 쥬스 제조 성공
+//        NotificationCenter.default.post(name: NSNotification.Name("JuiceMade"), object: nil)
+//        print("Successfully made \(juiceMenu.recipe) juice")
+//        return .success(())
+//    } catch JuiceError.outOfStock {
+//        return .failure(JuiceError.outOfStock)
+//    } catch {
+//        // 다른 예외적인 에러 처리
+//        return .failure(JuiceError.unknown)
+//    }
     
 }
