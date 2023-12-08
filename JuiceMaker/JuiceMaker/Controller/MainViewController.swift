2fab157e7f6a4728ce913aa7ffc8fe6596279ce7
AsherKim <tawans@naver.com>
2023-12-08 16:19:51 +0900

    feat(*): 과일 기본 재고수량 50개씩을 변경, MaintViewController 생성

---
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  JuiceMaker
+//
+//  Created by Taewan Kim on 12/8/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var strawberryQuantity: UILabel!
+    @IBOutlet weak var bananaQuantity: UILabel!
+    @IBOutlet weak var pineappleQuantity: UILabel!
+    @IBOutlet weak var kiwiQuantity: UILabel!
+    @IBOutlet weak var mangoQuantity: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+
+
+
+}
