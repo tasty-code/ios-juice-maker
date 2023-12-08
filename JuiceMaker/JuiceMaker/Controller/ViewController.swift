ec7a570eec1d444b7fd45c4543950ac5261ff12d
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2023-12-08 15:01:29 +0900

    refactor : 테스트 코드 제거

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,21 +8,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var fruitStore = FruitStore()
-    lazy var juiceMaker = JuiceMaker(store: self.fruitStore)
-   
     override func viewDidLoad() {
         super.viewDidLoad()
-        juiceMaker.showMenu()
-        juiceMaker.order(Recipe.mangoKiwi)
-        juiceMaker.order(Recipe.mangoKiwi)
-        juiceMaker.order(Recipe.strawberry)
-        fruitStore.supplyFruits(fruitName: Fruit.strawberry.rawValue, quantity: 20)
-        juiceMaker.order(Recipe.strawberry)
-        juiceMaker.order(Recipe.mango)
-        juiceMaker.order(Recipe.mango)
-        juiceMaker.order(Recipe.mango)
-        fruitStore.supplyFruits(fruitName: Fruit.mango.rawValue, quantity: 15)
     }
 }
 
