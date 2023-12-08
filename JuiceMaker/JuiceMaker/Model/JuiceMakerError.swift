84ee5f8bdae5cfe87733bac43b05542927842c0c
LeeSe0ngYe0n <goblin__@naver.com>
2023-12-08 20:17:00 +0900

    refactor: LocalizedError 채택

--- JuiceMaker/JuiceMaker/Model/JuiceMakerError.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerError.swift
@@ -5,6 +5,18 @@
 //  Created by 강창현 on 12/6/23.
 //
 
+import Foundation
+
 enum JuiceMakerError: Error {
     case outOfStock(fruit: String)
+}
+
+extension JuiceMakerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .outOfStock(let fruit):
+            return "\(fruit)가 모자라요. 재고를 수정할까요?"
+        }
+    }
+    
 }
