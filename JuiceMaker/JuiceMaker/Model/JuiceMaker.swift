72cbc9488852b8311470a9bcc38046d817f480a7
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-08 16:38:26 +0900

    refactor: 함수 은닉화 및 에러핸들링 코드 리팩토링

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -1,11 +1,10 @@
 //
 //  JuiceMaker - JuiceMaker.swift
-//  Created by Kyle& L
-//  Copyright © yagom academy. All rights reserved.
+//  Created by Kyle & L
 //
 
 struct JuiceMaker {
-    var fruitStore: FruitStore = FruitStore(
+    private let fruitStore: FruitStore = FruitStore(
         strawberryStock: 10,
         bananaStock: 10,
         pineappleStock: 10,
@@ -13,29 +12,24 @@ struct JuiceMaker {
         mangoStock: 10
     )
     
-    func makeJuice(juice: Juice) {
+    func makeJuice(juice: Juice) throws {
         do {
             try checkUnderstockedFruits(juice: juice)
             for (fruit, count) in juice.recipe {
                 fruitStore.useJuiceIngredient(fruit, count: count)
             }
         } catch {
-            switch error {
-            case JuiceMakerError.outOfStock(let fruit):
-                print("\(fruit) 재고 없음")
-            default:
-                print("알 수 없는 에러 발생")
-            }
+            print(error.localizedDescription)
         }
     }
     
-    func checkUnderstockedFruits(juice: Juice) throws {
+    private func checkUnderstockedFruits(juice: Juice) throws {
         var understockedFruits: [String] = []
-        var canMakeJuice = true
+        var canMakeJuice: Bool = true
         
         for (fruit, count) in juice.recipe {
             guard
-                hasEnoughStock(for: fruit, count: count)
+                fruitStore.hasEnoughStock(fruit: fruit, count: count)
             else {
                 understockedFruits.append(fruit.name)
                 canMakeJuice = false
@@ -43,24 +37,11 @@ struct JuiceMaker {
             }
         }
         
-        guard canMakeJuice else {
+        guard 
+            canMakeJuice
+        else {
             let understockedFruit = understockedFruits.joined(separator: ",")
             throw JuiceMakerError.outOfStock(fruit: understockedFruit)
-        }
-    }
-    
-    private func hasEnoughStock(for fruit: Fruits, count: Int) -> Bool {
-        switch fruit {
-        case .strawberry:
-            return fruitStore.strawberryStock >= count
-        case .banana:
-            return fruitStore.bananaStock >= count
-        case .pineapple:
-            return fruitStore.pineappleStock >= count
-        case .kiwi:
-            return fruitStore.kiwiStock >= count
-        case .mango:
-            return fruitStore.mangoStock >= count
         }
     }
 }
