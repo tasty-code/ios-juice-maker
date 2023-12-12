5d524b7e2264adb32b255b5144f14fb08fc00a98
kimdj1102 <djkim1102@naver.com>
2023-12-12 14:35:35 +0900

    [feat] 레이블, 버튼 JuiceMakingViewController에 연결

--- JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
@@ -8,6 +8,21 @@ import UIKit
 
 class JuiceMakingViewController: UIViewController {
 
+    @IBOutlet var numberOfStrawberry: UILabel!
+    @IBOutlet var numberOfBanana: UILabel!
+    @IBOutlet var numberOfPineApple: UILabel!
+    @IBOutlet var numberOfKiwi: UILabel!
+    @IBOutlet var numberOfMango: UILabel!
+    
+    @IBOutlet var orderStrawberryButton: UIButton!
+    @IBOutlet var orderBananaButton: UIButton!
+    @IBOutlet var orderPineAppleButton: UIButton!
+    @IBOutlet var orderKiwiButton: UIButton!
+    @IBOutlet var orderMangoButton: UIButton!
+    @IBOutlet var orderStrawberryBananaButton: UIButton!
+    @IBOutlet var orderMangoKiwiButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
