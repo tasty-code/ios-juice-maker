b7e41e42ef8081dbb5f581089e39832fafa0b9c4
hemil0102 <hemil0102@gmail.com>
2023-12-12 11:22:31 +0900

    feat: configureUI()를 viewWillAppear() 내부에 구현 (#2)

--- JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
@@ -19,6 +19,10 @@ final class OrderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         configureUI()
     }
