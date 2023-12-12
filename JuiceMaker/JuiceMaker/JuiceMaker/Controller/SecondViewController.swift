c1a27844a9c16eb66b2c8918b1d65a2fbd381ce5
Nnn <jaehyung1299@naver.com>
2023-12-12 16:22:09 +0900

    Fix: 깃 오류로 인한 파일 복구

---
+++ JuiceMaker/JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  JuiceMaker
+//
+//  Created by 박찬호 on 12/7/23.
+//
+
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var data: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
+
