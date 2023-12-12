36842acf15c838d302e51819459767619acfe6a8
Andy An <andyanappdev@gmail.com>
2023-12-12 14:34:22 +0900

    feat: IBOutlet 연결 및 displayStock 메서드 구현
    Co-authored-by: Effie <effie.thedev@gmail.com>

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -10,7 +10,17 @@ final class JuiceMakerViewController: UIViewController {
     
     private var stockDisplay: StockDisplay?
     
+    @IBOutlet private weak var strawberryStockLabel: UILabel!
     
+    @IBOutlet private weak var bananaStockLabel: UILabel!
+    
+    @IBOutlet private weak var pineappleStockLabel: UILabel!
+    
+    @IBOutlet private weak var kiwiStockLabel: UILabel!
+    
+    @IBOutlet private weak var mangoStockLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let fruitStore = FruitStore(initialCount: 10)
@@ -35,7 +45,13 @@ final class JuiceMakerViewController: UIViewController {
 
 extension JuiceMakerViewController: StockDisplayResultDisplayable {
     func displayStock(viewModel: StockDisplayModel.ViewModel) {
+        guard let eachFruitCount = viewModel.eachFruitCount else { return }
         
+        self.strawberryStockLabel.text = "\(eachFruitCount.strawberryCount)"
+        self.bananaStockLabel.text = "\(eachFruitCount.bananaCount)"
+        self.pineappleStockLabel.text = "\(eachFruitCount.pineappleCount)"
+        self.kiwiStockLabel.text = "\(eachFruitCount.kiwiCount)"
+        self.mangoStockLabel.text = "\(eachFruitCount.mangoCount)"
     }
 }
 
