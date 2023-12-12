206f2ff1cb7eec58260fcb9bca96e6d44254467c
ujhong7 <ujhong7>
2023-12-12 14:13:47 +0900

    [feat] StockManagementViewController 생성

--- JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JuiceMakingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockManagementViewController.swift
+//  JuiceMaker
+//
+//  Created by yujaehong on 12/12/23.
+//
+
+import UIKit
+
+class StockManagementViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
