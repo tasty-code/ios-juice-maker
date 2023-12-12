72caf7523a2c3d62a0815625fdf5e04e8a91fa72
yoruck2 <yoruck2@gmail.com>
2023-12-12 10:52:10 +0900

    feat: 초기화면 과일재고 세팅

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,10 +8,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
+    var labels: Dictionary<UILabel, Fruit> = [:]
+    var fruitSore: FruitStore = FruitStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labels = [strawberryLabel : .strawberry, bananaLabel : .banana, pineappleLabel : .pineapple, kiwiLabel : .kiwi, mangoLabel : .mango]
+        configureUI()
     }
 
 }
 
+extension ViewController {
+    func configureUI() {
+        labels.forEach { $0.text = fruitSore.fruitCount(fruit: $1).description}
+    }
+}
