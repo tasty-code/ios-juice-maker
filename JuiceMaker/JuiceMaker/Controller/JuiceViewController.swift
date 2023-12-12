cf087435b42ccb8fc990256c33c76790cda0e730
llamang <yohanlee1230@daum.net>
2023-12-12 16:10:15 +0900

    chore: alert 메시지 및 일부 메서드 이름 변경

--- JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -32,10 +32,10 @@ class JuiceViewController: UIViewController {
     }
 
     @IBAction private func adjustStock(_ sender: Any) {
-        pushNavigationView()
+        showNavigationView()
     }
     
-    private func pushNavigationView() {
+    private func showNavigationView() {
         let supplyVC = storyboard?.instantiateViewController(withIdentifier: "SupplyViewController") as! SupplyViewController
         navigationController?.pushViewController(supplyVC, animated: true)
     }
@@ -81,14 +81,14 @@ class JuiceViewController: UIViewController {
     
     private func alertJuiceServed(menu: JuiceMaker.Menu) {
         let alert = UIAlertController(title: "", message: "\(menu) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("쥬스 수령", comment: "Default action"), style: .default))
         self.present(alert, animated: true, completion: nil)
     }
     
     private func alertInsufficientStock() {
         let alert = UIAlertController(title: "", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("예", comment: "Default action"), style: .default, handler: { _ in
-            self.pushNavigationView()
+            self.showNavigationView()
         }))
         alert.addAction(UIAlertAction(title: NSLocalizedString("아니오", comment: "Default action"), style: .default))
         self.present(alert, animated: true, completion: nil)
