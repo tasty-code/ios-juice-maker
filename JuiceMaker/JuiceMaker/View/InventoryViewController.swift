c78b3bde46183c599031b7993fadfe45100976cf
hemil0102 <hemil0102@gmail.com>
2023-12-12 11:24:24 +0900

    feat: 재고관리화면 Stepper 기능 구현 및 Label  연동 (#2)

--- JuiceMaker/JuiceMaker/View/InventoryViewController.swift
+++ JuiceMaker/JuiceMaker/View/InventoryViewController.swift
@@ -17,17 +17,51 @@ final class InventoryViewController: UIViewController {
     @IBOutlet weak var kiwiQuantityLabel: UILabel!
     @IBOutlet weak var mangoQuantityLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
+    private lazy var fruitQuantityLabels: [Fruit: UILabel] = [
+        .strawberry: strawberryQuantityLabel,
+        .banana: bananaQuantityLabel,
+        .pineapple: pineappleQuantityLabel,
+        .kiwi: kiwiQuantityLabel,
+        .mango: mangoQuantityLabel
+    ]
+    
+    private lazy var fruitSteppers: [UIStepper: Fruit] = [
+        strawberryStepper: .strawberry,
+        bananaStepper: .banana,
+        pineappleStepper: .pineapple,
+        kiwiStepper: .kiwi,
+        mangoStepper: .mango
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureInitialStepperValue()
+    }
+    
+    @IBAction func modifyFruitStepper(_ sender: UIStepper) {
+        if let fruit = fruitSteppers[sender] {
+            fruitStore.fruitContainer[fruit] = Int(sender.value)
+            fruitQuantityLabels[fruit]?.text = String(format: "%.0f", sender.value)
+        }
     }
     
     private func configureUI() {
-        strawberryQuantityLabel.text = String(fruitStore.fruitContainer[.strawberry, default: 0])
-        bananaQuantityLabel.text = String(fruitStore.fruitContainer[.banana, default: 0])
-        pineappleQuantityLabel.text = String(fruitStore.fruitContainer[.pineapple, default: 0])
-        kiwiQuantityLabel.text = String(fruitStore.fruitContainer[.kiwi, default: 0])
-        mangoQuantityLabel.text = String(fruitStore.fruitContainer[.mango, default: 0])
+        for (fruit, label) in fruitQuantityLabels {
+            label.text = String(fruitStore.fruitContainer[fruit, default: 0])
+        }
+    }
+    
+    private func configureInitialStepperValue() {
+        for (stepper, fruit) in fruitSteppers {
+            stepper.value = Double(fruitStore.fruitContainer[fruit, default: 0])
+        }
     }
 }
