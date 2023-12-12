582f67ad79a62f8baf858625205a8794978261db
yoruck2 <yoruck2@gmail.com>
2023-12-12 11:17:50 +0900

    feat: 재고관리화면 과일재고 세팅

--- JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
@@ -9,5 +9,24 @@ import UIKit
 
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
     
+    var labels: Dictionary<UILabel, Fruit> = [:]
+    var fruitSore: FruitStore = FruitStore()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labels = [strawberryLabel : .strawberry, bananaLabel : .banana, pineappleLabel : .pineapple, kiwiLabel : .kiwi, mangoLabel : .mango]
+        configureUI()
+    }
+}
+
+extension SecondViewController {
+    func configureUI() {
+        labels.forEach { $0.text = fruitSore.fruitCount(fruit: $1).description}
+    }
 }
