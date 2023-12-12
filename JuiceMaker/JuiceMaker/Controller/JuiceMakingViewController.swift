41988e13a1b7472771bda9ab6fc315adc46f4973
ujhong7 <ujhong7>
2023-12-12 14:58:10 +0900

    [feat] JuiceMakingViewController 레이블 세팅

--- JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakingViewController.swift
@@ -27,9 +27,30 @@ class JuiceMakingViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-    
     
 }
+
+extension JuiceMakingViewController {
+    func setUp(number: Int, on label:UILabel) {
+        label.text = String(number)
+    }
+    
+    func showNumberOnLabel(fruits: [Fruit: Int]) {
+        for fruit in fruits {
+            switch fruit.key {
+            case .strawberry:
+                setUp(number: fruit.value, on: numberOfStrawberry)
+            case .banana:
+                setUp(number: fruit.value, on: numberOfBanana)
+            case .pineapple:
+                setUp(number: fruit.value, on: numberOfPineApple)
+            case .kiwi:
+                setUp(number: fruit.value, on: numberOfKiwi)
+            case .mango:
+                setUp(number: fruit.value, on: numberOfMango)
+            }
+        }
+    }
+}
+
 
--- JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class StockManagementViewController: UIViewController {
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
