8838c76d0d5b63802d13dbbf50412d8619fc7156
Effie <effie.thedev@gmail.com>
2023-12-12 16:22:09 +0900

    feat: StockManagerViewController 추가
    Co-authored-by: Andy <andyanappdev@gmail.com>

---
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -0,0 +1,63 @@
+//
+//  StockManagerViewController.swift
+//  JuiceMaker
+//
+//  Created by Effie on 12/12/23.
+//
+
+import UIKit
+
+final class StockManagerViewController: UIViewController {
+    @IBOutlet private weak var strawberryStockLabel: UILabel!
+    
+    @IBOutlet private weak var bananaStockLabel: UILabel!
+    
+    @IBOutlet private weak var pineappleStockLabel: UILabel!
+    
+    @IBOutlet private weak var kiwiStockLabel: UILabel!
+    
+    @IBOutlet private weak var mangoStockLabel: UILabel!
+    
+    private let stockDisplay: StockDisplay?
+    
+    required init?(coder: NSCoder) {
+        self.stockDisplay = nil
+        super.init(coder: coder)
+    }
+    
+    init?(coder: NSCoder, fruitStore: FruitStore) {
+        self.stockDisplay = StockDisplay(fruitStore: fruitStore)
+        super.init(coder: coder)
+        setUp()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stockDisplay?.displayStock()
+    }
+    
+    @IBAction func completeManaging(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    private func setUp() {
+        let resultConverter = StockDisplayResultConverter()
+        resultConverter.display = self
+        stockDisplay?.resultConverter = resultConverter
+    }
+}
+
+extension StockManagerViewController: StoryboardIdentifiable { }
+
+extension StockManagerViewController: StockDisplayResultDisplayable {
+    func displayStock(viewModel: StockDisplayModel.ViewModel) {
+        guard let eachFruitCount = viewModel.eachFruitCount else { return }
+        
+        self.strawberryStockLabel.text = "\(eachFruitCount.strawberryCount)"
+        self.bananaStockLabel.text = "\(eachFruitCount.bananaCount)"
+        self.pineappleStockLabel.text = "\(eachFruitCount.pineappleCount)"
+        self.kiwiStockLabel.text = "\(eachFruitCount.kiwiCount)"
+        self.mangoStockLabel.text = "\(eachFruitCount.mangoCount)"
+    }
+}
+
