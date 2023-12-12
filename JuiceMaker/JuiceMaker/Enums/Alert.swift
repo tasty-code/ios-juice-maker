0de0f517c3bf0d23dc00474e39ff6200001b2cb5
jus1234 <osjamg1996@gmail.com>
2023-12-12 14:17:03 +0900

    chore: 오타 수정

--- JuiceMaker/JuiceMaker/Enums/Alert.swift
+++ JuiceMaker/JuiceMaker/Enums/Alert.swift
@@ -16,17 +16,17 @@ extension Alert {
     var modal: UIAlertController {
         switch self {
         case .juiceSucess(let juice):
-            let alretController = UIAlertController(title: "알림", message: "\(juice.name) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+            let alertController = UIAlertController(title: "알림", message: "\(juice.name) 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
             let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
-            alretController.addAction(yes)
-            return alretController
+            alertController.addAction(yes)
+            return alertController
         case .juiceFail(let handler):
-            let alretController = UIAlertController(title: "알림", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+            let alertController = UIAlertController(title: "알림", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
             let yes = UIAlertAction(title: "예", style: .default, handler: { _ in handler() })
             let no = UIAlertAction(title: "아니오", style: .destructive, handler: nil)
-            alretController.addAction(yes)
-            alretController.addAction(no)
-            return alretController
+            alertController.addAction(yes)
+            alertController.addAction(no)
+            return alertController
         }
     }
 }
