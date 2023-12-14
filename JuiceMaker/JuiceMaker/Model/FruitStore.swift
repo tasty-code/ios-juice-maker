dc6c3869e8ba52677de538ad0b338d16c75ba064
July <90888402+July911@users.noreply.github.com>
2023-12-14 10:48:35 +0900

    Revert "쥬스 자판기 [STEP 1] Matthew, 희동"

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,52 +4,9 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 // 과일 저장소 타입
-final class FruitStore {
-    static let shared: FruitStore = FruitStore()
+class FruitStore {
     
-    private init() {}
-    
-    var fruitList: [FruitInfo] = [
-        FruitInfo(name: .strawberry, quantity: 0),
-        FruitInfo(name: .banana, quantity: 0),
-        FruitInfo(name: .pineapple, quantity: 0),
-        FruitInfo(name: .kiwi, quantity: 0),
-        FruitInfo(name: .mango, quantity: 0)
-    ]
-    
-    public func updateFruitQuantity(fruit: Fruit, result: Int) {
-        if let index = fruitList.firstIndex(where: { $0.name == fruit }) {
-            fruitList[index].quantity = result
-        }
-    }
-    
-    public func showFruitQuantity(fruit: Fruit) -> String {
-        if let index = fruitList.firstIndex(where: { $0.name == fruit }) {
-            return String(fruitList[index].quantity)
-        }
-        return String(0)
-    }
-    
-    public func checkFruitsQuantity(fruit: Fruit, willUseQuantity: Int) -> Bool {
-        let result = fruitList
-            .firstIndex { $0.name == fruit }
-            .map { fruitList[$0].quantity >= willUseQuantity } ?? false
-        return result
-    }
-    
-    public func usedFruitsQuantity(fruit: Fruit, usedQuantity: Int) {
-        if let index = fruitList.firstIndex(where: { $0.name == fruit }) {
-            fruitList[index].quantity -= usedQuantity
-        }
-    }
-    
-    func checkFruit(fruit: Fruit) -> Int {
-        return fruitList.map { $0.name == fruit }.count
-    }
-    
-
 }
-
