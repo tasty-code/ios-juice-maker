f0923f87578b5bf16ab7c29b23320aec9a821a97
ujhong7 <ujhong7>
2023-12-14 15:32:51 +0900

    [feat] 재고 수정 화면 레이블, 스테퍼 연결 및 레이블 세팅

--- JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -8,9 +8,48 @@
 import UIKit
 
 class StockManagementViewController: UIViewController {
+    
+    @IBOutlet var numberOfStrawberry: UILabel!
+    @IBOutlet var numberOfBanana: UILabel!
+    @IBOutlet var numberOfPineApple: UILabel!
+    @IBOutlet var numberOfKiwi: UILabel!
+    @IBOutlet var numberOfMango: UILabel!
+    
+    @IBOutlet var changeAmountOfStrawberry: UIStepper!
+    @IBOutlet var changeAmountOfBanana: UIStepper!
+    @IBOutlet var changeAmountOfPineApple: UIStepper!
+    @IBOutlet var changeAmountOfKiwi: UIStepper!
+    @IBOutlet var changeAmountOfMango: UIStepper!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    
+    
+}
+
+extension StockManagementViewController {
+    func setUp(number: Int, on label:UILabel) {
+        label.text = String(number)
+    }
+    
+    func showNumberOnLabel(fruits: [Fruit: Int]) {
+        for fruit in fruits {
+            switch fruit.key {
+            case .strawberry:
+                setUp(number: fruit.value, on: numberOfStrawberry)
+            case .banana:
+                setUp(number: fruit.value, on: numberOfBanana)
+            case .pineapple:
+                setUp(number: fruit.value, on: numberOfPineApple)
+            case .kiwi:
+                setUp(number: fruit.value, on: numberOfKiwi)
+            case .mango:
+                setUp(number: fruit.value, on: numberOfMango)
+            }
+        }
+    }
 }
