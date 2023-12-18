8a7f8b22ce185ddd10a43652f1d6e6d6e9ae5d13
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-18 19:08:14 +0900

    refactor: where절과 completion handler를 활용하여 리팩토링

--- JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
@@ -8,27 +8,24 @@
 import UIKit
 
 protocol JuiceMakerAlert {
-    func showJuiceMakerAlert(viewController: UIViewController, isCompletedMakeJucie: Bool, message: String)
+    func showJuiceMakerAlert(isCompletedMakeJuice: Bool, message: String, completion: (() -> Void)?)
 }
 
-extension JuiceMakerAlert {
-    func showJuiceMakerAlert(viewController: UIViewController, isCompletedMakeJucie: Bool, message: String) {
+extension JuiceMakerAlert where Self: UIViewController {
+    func showJuiceMakerAlert(isCompletedMakeJuice: Bool, message: String, completion: (() -> Void)?) {
         let alert = UIAlertController(title: "알림" , message: message, preferredStyle: .alert)
         guard
-            isCompletedMakeJucie
+            isCompletedMakeJuice
         else {
-            let juiceMakerViewController = JuiceMakerViewController()
             alert.addAction(UIAlertAction(title: "예", style: .default, handler: { _ in
-                print("누름")
-                juiceMakerViewController.showFruitStoreViewController()
-                print("누름")
+                completion?()
             }))
             alert.addAction(UIAlertAction(title: "아니요", style: .cancel))
-            viewController.present(alert, animated: true)
+            self.present(alert, animated: true)
             return
         }
         
         alert.addAction(UIAlertAction(title: "확인", style: .cancel))
-        viewController.present(alert, animated: true)
+        self.present(alert, animated: true)
     }
 }
