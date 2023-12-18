11ae166e8533e728782c91ef8ae97ab2c2671514
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-18 10:04:32 +0900

    feat: JuiceMakerAlert 프로토콜 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
@@ -5,4 +5,30 @@
 //  Created by 강창현 on 12/17/23.
 //
 
-import Foundation
+import UIKit
+
+protocol JuiceMakerAlert {
+    func showJuiceMakerAlert(viewController: UIViewController, isCompletedMakeJucie: Bool, message: String)
+}
+
+extension JuiceMakerAlert {
+    func showJuiceMakerAlert(viewController: UIViewController, isCompletedMakeJucie: Bool, message: String) {
+        let alert = UIAlertController(title: "알림" , message: message, preferredStyle: .alert)
+        guard
+            isCompletedMakeJucie
+        else {
+            let juiceMakerViewController = JuiceMakerViewController()
+            alert.addAction(UIAlertAction(title: "예", style: .default, handler: { _ in
+                print("누름")
+                juiceMakerViewController.showFruitStoreViewController()
+                print("누름")
+            }))
+            alert.addAction(UIAlertAction(title: "아니요", style: .cancel))
+            viewController.present(alert, animated: true)
+            return
+        }
+        
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        viewController.present(alert, animated: true)
+    }
+}
