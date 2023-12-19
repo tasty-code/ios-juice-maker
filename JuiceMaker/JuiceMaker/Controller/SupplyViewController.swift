75e9cb1c425dd79a8dbe32ee1fdf94f39eda5e90
llamang <yohanlee1230@daum.net>
2023-12-19 13:24:00 +0900

    feat: Steppers 연결

--- JuiceMaker/JuiceMaker/Controller/SupplyViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/SupplyViewController.swift
@@ -14,11 +14,47 @@ class SupplyViewController: UIViewController {
     @IBOutlet private weak var kiwiCountLabel: UILabel!
     @IBOutlet private weak var mangoCountLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     private let fruitStore = FruitStore.shared
     
     override func viewIsAppearing(_ animated: Bool) {
         super.viewIsAppearing(animated)
+        
+        setInitStepperValue()
         reloadFruitsCount()
+    }
+    
+    @IBAction func strawberryStepperTapped(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func bananaStepperTapped(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func pineappleStepperTapped(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func kiwiStepperTapped(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func mangoStepperTapped(_ sender: UIStepper) {
+        
+    }
+    
+    private func setInitStepperValue() {
+        strawberryStepper.value = Double(fruitStore.storage[.strawberry] ?? 0)
+        bananaStepper.value = Double(fruitStore.storage[.banana] ?? 0)
+        pineappleStepper.value = Double(fruitStore.storage[.pineapple] ?? 0)
+        kiwiStepper.value = Double(fruitStore.storage[.kiwi] ?? 0)
+        mangoStepper.value = Double(fruitStore.storage[.mango] ?? 0)
     }
     
     private func reloadFruitsCount() {
