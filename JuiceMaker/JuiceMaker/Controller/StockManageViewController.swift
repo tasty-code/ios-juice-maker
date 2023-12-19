21d564b9520c09959421653d0a5432f0e6be2b55
NERO <mxzxro@gmail.com>
2023-12-19 11:47:02 +0900

    feat: StockManageViewController 초기값 반영

--- JuiceMaker/JuiceMaker/Controller/StockManageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManageViewController.swift
@@ -3,8 +3,8 @@ import UIKit
 
 // MARK: - StockManageViewController init & deinit
 class StockManageViewController: UIViewController {
-    
-    var reception: Reception? = nil
+    @IBOutlet var stockManageView: StockManageView!
+    var reception: Reception?
 }
 
 // MARK: - StockManageViewController LifeCycle
@@ -12,6 +12,33 @@ extension StockManageViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNotificationCenter()
+        reception?.applyCurrentStocks()
+    }
+}
+
+// MARK: - SetUp Notification Center
+private extension StockManageViewController {
+    
+    func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateFruitStock), name: .fruitStockDidChange, object: nil)
+    }
+    
+    @objc func updateFruitStock(notification: Notification) {
+        guard let fruitStock =
+            notification.userInfo?["fruitsStock"] as? [FruitStore.Fruits: Int] else {
+            ErrorType.notificationCenterError("\(#function)에서 문제가 생겼습니다!").printMessage()
+            return
+        }
+        updateStockLabel(from: fruitStock)
+    }
+    
+    func updateStockLabel(from fruitStock: [FruitStore.Fruits: Int]) {
+        stockManageView.bananaStockLabel.text = String(fruitStock[.banana] ?? 0)
+        stockManageView.strawberryStockLabel.text = String(fruitStock[.strawberry] ?? 0)
+        stockManageView.mangoStockLabel.text = String(fruitStock[.mango] ?? 0)
+        stockManageView.pineappleStockLabel.text = String(fruitStock[.pineapple] ?? 0)
+        stockManageView.kiwiStockLabel.text = String(fruitStock[.kiwi] ?? 0)
     }
 }
 
