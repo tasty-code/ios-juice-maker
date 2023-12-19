f75fbf935744d68e91e431e7ef6cf06add559b5d
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-19 20:07:47 +0900

    feat: FruitStockDelegate 선언

---
+++ JuiceMaker/JuiceMaker/Model/FruitStockDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  FruitStockDelegate.swift
+//  JuiceMaker
+//
+//  Created by 강창현 on 12/19/23.
+//
+
+import UIKit
+
+protocol FruitStockDelegate: AnyObject {
+    func updateFruitStock()
+}
