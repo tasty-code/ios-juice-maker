dc508e9947ff9d3e7cc04c5631929d490e2ffe0d
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-19 20:10:42 +0900

    feat: FruitStoreViewController -> navigationItem 및 Stepper 기능 구현

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -15,10 +15,18 @@ final class FruitStoreViewController: UIViewController {
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pinappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     private let fruitStore: FruitStore
+    weak var delegate: FruitStockDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         setStockLabelUI()
     }
     
@@ -29,6 +37,15 @@ final class FruitStoreViewController: UIViewController {
     
     required init?(coder: NSCoder) {
          fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        self.navigationItem.hidesBackButton = true
+        self.navigationItem.title = "재고추가"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "닫기",
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(tappedCloseButton))
     }
     
     private func setStockLabelUI() {
@@ -46,5 +63,36 @@ final class FruitStoreViewController: UIViewController {
         pineappleStockLabel.text = String(pineappleStock)
         kiwiStockLabel.text = String(kiwiStock)
         mangoStockLabel.text = String(mangoStock)
+    }
+    
+    @objc
+    func tappedCloseButton() {
+        delegate?.updateFruitStock()
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tappedStrawberryStepper(_ sender: UIStepper) {
+        strawberryStockLabel.text = String(Int(sender.value))
+        fruitStore.fruitStocks[.strawberry] = Int(sender.value)
+    }
+    
+    @IBAction func tappedBananaStepper(_ sender: UIStepper) {
+        bananaStockLabel.text = String(Int(sender.value))
+        fruitStore.fruitStocks[.banana] = Int(sender.value)
+    }
+    
+    @IBAction func tappedPineappleStepper(_ sender: UIStepper) {
+        pineappleStockLabel.text = String(Int(sender.value))
+        fruitStore.fruitStocks[.pineapple] = Int(sender.value)
+    }
+    
+    @IBAction func tappedKiwiStepper(_ sender: UIStepper) {
+        kiwiStockLabel.text = String(Int(sender.value))
+        fruitStore.fruitStocks[.kiwi] = Int(sender.value)
+    }
+    
+    @IBAction func tappedMangoStepper(_ sender: UIStepper) {
+        mangoStockLabel.text = String(Int(sender.value))
+        fruitStore.fruitStocks[.mango] = Int(sender.value)
     }
 }
