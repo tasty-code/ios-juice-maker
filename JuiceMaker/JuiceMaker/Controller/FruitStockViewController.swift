eda9134a8230b4eef2d0db69d60ba18aacabff33
yoruck2 <yoruck2@gmail.com>
2023-12-21 15:51:46 +0900

    feat: 재고 관리화면 저장하기 버튼 추가(재고와 다른 경우만 저장 버튼 활성화)

--- JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-
 class FruitStockViewController: UIViewController {
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
@@ -21,18 +20,21 @@ class FruitStockViewController: UIViewController {
     @IBOutlet weak var kiwiStepper: UIStepper!
     @IBOutlet weak var mangoStepper: UIStepper!
     
+    @IBOutlet weak var fruitStockSaveButton: UIButton!
+    
     private var labelDict: Dictionary<UIStepper, UILabel> = [:]
     private var stepperDict: Dictionary<UIStepper, Fruit> = [:]
     var fruitCount: ((Fruit) -> Int)?
     var updateFruitStore: ((Dictionary<Fruit, Int>) -> Void)?
+    var originStockValue: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fruitStockSaveButton.isEnabled = false
         setStepperDict()
         setLabelDict()
         labelDict.forEach(setLabelText)
     }
-    
 }
 
 extension FruitStockViewController {
@@ -43,17 +45,22 @@ extension FruitStockViewController {
     @IBAction func stepperTapped(_ sender: UIStepper) {
         guard let label = labelDict[sender] else {
             return
-        }
+        }   
         setLabelText(stepper: sender, label: label)
+        fruitStockSaveButton.isEnabled = stepperDict.map { Int($0.key.value) } == originStockValue ? false : true
     }
     
-    func dismissFruitStockViewController() {
+    @IBAction func fruitStockSaveButtonTapped() {
         let store: Dictionary<Fruit, Int> = stepperDict.reduce(into: [:]) {
             (fruitStore, keyValuePair) in
             let (stepper, fruit) = keyValuePair
             fruitStore[fruit] = Int(stepper.value)
         }
         updateFruitStore?(store)
+        makeAlert(title: "알림", message: "재고가 수정되었습니다!")
+    }
+    
+    func dismissFruitStockViewController() {
         self.dismiss(animated: false)
     }
     
@@ -70,6 +77,7 @@ extension FruitStockViewController {
         stepper.minimumValue = fruit.minimum
         stepper.maximumValue = fruit.maximum
         stepper.value = Double(fruitCount?(fruit) ?? 0)
+        originStockValue.append(Int(stepper.value))
     }
     
     func setLabelText(stepper: UIStepper, label: UILabel) {
