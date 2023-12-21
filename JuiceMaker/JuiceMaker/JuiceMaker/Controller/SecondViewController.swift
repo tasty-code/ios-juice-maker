dda76ee34ad42cc20749d91cc8e9b8f75d0e94de
Chan Ho Park <park_ch@kakao.com>
2023-12-21 21:54:31 +0900

    feat: SecondViewController의 재고 표시 및 업데이트 기능 구현
    - 각 과일별 stepper에 대한 액션 메서드(예: strawberryStepperChanged) 구현.
    - 각 과일별 레이블 업데이트를 위한 updateLabel 메서드 구현.
    - 완료 버튼 클릭 시 delegate 메서드 호출 및 화면 닫기 로직 구현.

--- JuiceMaker/JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
@@ -34,9 +34,74 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(true, animated: true)
+        setupStockDisplay()
     }
-
-
+    
+    func setupStockDisplay() {
+        fruitStockDisplay(fruit: .strawberry, label: strawberryStockLabel, stepper: strawberryStockStepper)
+        fruitStockDisplay(fruit: .banana, label: bananaStockLabel, stepper: bananaStockStepper)
+        fruitStockDisplay(fruit: .kiwi, label: kiwiStockLabel, stepper: kiwiStockStepper)
+        fruitStockDisplay(fruit: .mango, label: mangoStockLabel, stepper: mangoStockStepper)
+        fruitStockDisplay(fruit: .pineapple, label: pineappleStockLabel, stepper: pineappleStockStepper)
+    }
+    
+    func fruitStockDisplay(fruit: Fruit, label: UILabel, stepper: UIStepper) {
+        guard let quantity = juiceMaker?.fruitStore.getQuantity(of: fruit) else {
+            label.text = "N/A"
+            stepper.isEnabled = false
+            return
+        }
+        label.text = "\(quantity)"
+        stepper.value = Double(quantity)
+        stepper.minimumValue = 0
+        stepper.maximumValue = 100
+    }
+    
+    @IBAction func strawberryStepperChanged(_ sender: UIStepper) {
+        updateFruitStock(fruit: .strawberry, newValue: Int(sender.value))
+    }
+    @IBAction func bananaStepperChanged(_ sender: UIStepper) {
+        updateFruitStock(fruit: .banana, newValue: Int(sender.value))
+    }
+    @IBAction func pineappleStepperChanged(_ sender: UIStepper) {
+        updateFruitStock(fruit: .pineapple, newValue: Int(sender.value))
+    }
+    @IBAction func kiwiStepperChanged(_ sender: UIStepper) {
+        updateFruitStock(fruit: .kiwi, newValue: Int(sender.value))
+    }
+    @IBAction func mangoStepperChanged(_ sender: UIStepper) {
+        updateFruitStock(fruit: .mango, newValue: Int(sender.value))
+    }
+    
+    
+    
+    
+    func updateFruitStock(fruit: Fruit, newValue: Int) {
+        juiceMaker?.fruitStore.setQuantity(of: fruit, to: newValue)
+        updateLabel(for: fruit, with: newValue)
+    }
+    
+    func updateLabel(for fruit: Fruit, with quantity: Int ) {
+        switch fruit {
+        case .strawberry:
+            strawberryStockLabel.text = "\(quantity)"
+        case .banana:
+            bananaStockLabel.text = "\(quantity)"
+        case .kiwi:
+            kiwiStockLabel.text = "\(quantity)"
+        case .mango:
+            mangoStockLabel.text = "\(quantity)"
+        case .pineapple:
+            pineappleStockLabel.text = "\(quantity)"
+        default:
+            break
+        }
+    }
+    
+    
+    @IBAction func completeButton(_ sender: UIBarButtonItem) {
+        delegate?.didUpdateFruitStock()
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
-
