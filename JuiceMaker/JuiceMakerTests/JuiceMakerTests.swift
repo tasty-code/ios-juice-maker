c36e8b216524a0c2c72be1848bc42c7739a4b467
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-22 13:11:31 +0900

    chore: 불필요 test 코드 제거

--- JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
+++ JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
@@ -7,20 +7,16 @@
 import XCTest
 @testable import JuiceMaker
 final class JuiceMakerTests: XCTestCase {
-    private var sut: JuiceMaker!
     override func setUpWithError() throws {
-        sut = JuiceMaker()
         try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        sut = nil
         try super.tearDownWithError()
     }
     
     func test_딸기재고의_초기값이_10개라_딸기쥬스를_만들수_있는_재고가_없다() {
         
-        XCTAssertNoThrow(try sut.makeJuice(juice: .strawberryJuice), "딸기 재고 있음! 테스트 실패")
     }
     
     func test_망고키위쥬스를_만드는데_망고_키위_둘다_재고가_없다() {
@@ -37,13 +33,5 @@ final class JuiceMakerTests: XCTestCase {
     
     func test_파인애플의_재고를_3개로_바꾼다() {
         
-    }
-    
-    func test_망고쥬스를_만들고_남은_망고는_7개다() {
-      
-    }
-  
-    func test_파인애플의_재고를_3개로_바꾼다() {
-
     }
 }
