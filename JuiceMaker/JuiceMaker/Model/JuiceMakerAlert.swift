4a796fcd84525686b0c0aba67959af21ef3ec8f3
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-22 13:11:07 +0900

    refactor: JuiceMakerAlert Protocol을 더 범용적으로 확장할 수 있도록 리팩토링

--- JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
@@ -8,23 +8,39 @@
 import UIKit
 
 protocol JuiceMakerAlert {
-    func showJuiceMakerAlert(isCompletedMakeJuice: Bool, message: String, completion: (() -> Void)?)
+    /// JuiceMaker 프로젝트에서 필요한 Alert을 띄웁니다. 의도한 Alert Action을 순서에 맞게 입력해주세요.
+    /// - Parameters:
+    ///   - buttonTitles: Alert이 present 되었을 때, 필요한 버튼들의 제목을 순서대로 나열해주세요.
+    ///   - message: Alert이 present 되었을 때, 표시할 메세지를 입력해주세요.
+    ///   - styles: Alert 버튼의 스타일을 순서대로 지정해주세요.
+    ///   - completions: Alert 버튼을 눌렀을 때 진행할 Action을 순서대로 입력해주세요.
+    func showJuiceMakerAlert(buttonTitles: [String],
+                             message: String,
+                             styles: [UIAlertAction.Style],
+                             completions: [(() -> Void)?])
 }
 
 extension JuiceMakerAlert where Self: UIViewController {
-    func showJuiceMakerAlert(isCompletedMakeJuice: Bool, message: String, completion: (() -> Void)?) {
-        let alert = UIAlertController(title: "알림" , message: message, preferredStyle: .alert)
-        guard
-            isCompletedMakeJuice
+    func showJuiceMakerAlert(buttonTitles: [String],
+                             message: String,
+                             styles: [UIAlertAction.Style] = [.default],
+                             completions: [(() -> Void)?] = [nil]) {
+        guard buttonTitles.count == styles.count,
+              buttonTitles.count > completions.count
         else {
-            alert.addAction(UIAlertAction(title: "예", style: .default, handler: { _ in
-                completion?()
-            }))
-            alert.addAction(UIAlertAction(title: "아니요", style: .cancel))
-            self.present(alert, animated: true)
-            return
+            fatalError("InputError: showJuiceMakerAlert() - Please Check Parameters")
         }
-        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        let alert = UIAlertController(title: "알림",
+                                      message: message,
+                                      preferredStyle: .alert)
+        for (index, buttonTitle) in buttonTitles.enumerated() {
+            let action = UIAlertAction(title: buttonTitle, 
+                                       style: styles[index],
+                                       handler: { _ in
+                completions[index]?()
+            })
+            alert.addAction(action)
+        }
         self.present(alert, animated: true)
     }
 }
