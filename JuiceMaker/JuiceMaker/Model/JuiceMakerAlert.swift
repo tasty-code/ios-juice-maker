e8083907660c19ed1dfc5be7d72e618f3716fddc
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-26 12:27:09 +0900

    refactor: JuiceMakerAlert Protocol 확장성 및 안정성 고려 리팩토링

--- JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerAlert.swift
@@ -8,37 +8,15 @@
 import UIKit
 
 protocol JuiceMakerAlert {
-    /// JuiceMaker 프로젝트에서 필요한 Alert을 띄웁니다. 의도한 Alert Action을 순서에 맞게 입력해주세요.
-    /// - Parameters:
-    ///   - buttonTitles: Alert이 present 되었을 때, 필요한 버튼들의 제목을 순서대로 나열해주세요.
-    ///   - message: Alert이 present 되었을 때, 표시할 메세지를 입력해주세요.
-    ///   - styles: Alert 버튼의 스타일을 순서대로 지정해주세요.
-    ///   - completions: Alert 버튼을 눌렀을 때 진행할 Action을 순서대로 입력해주세요.
-    func showJuiceMakerAlert(buttonTitles: [String],
-                             message: String,
-                             styles: [UIAlertAction.Style],
-                             completions: [(() -> Void)?])
+    func showJuiceMakerAlert(message: String, actions: [UIAlertAction])
 }
 
 extension JuiceMakerAlert where Self: UIViewController {
-    func showJuiceMakerAlert(buttonTitles: [String],
-                             message: String,
-                             styles: [UIAlertAction.Style] = [.default],
-                             completions: [(() -> Void)?] = [nil]) {
-        guard buttonTitles.count == styles.count,
-              buttonTitles.count > completions.count
-        else {
-            fatalError("InputError: showJuiceMakerAlert() - Please Check Parameters")
-        }
+    func showJuiceMakerAlert(message: String, actions: [UIAlertAction]) {
         let alert = UIAlertController(title: "알림",
                                       message: message,
                                       preferredStyle: .alert)
-        for (index, buttonTitle) in buttonTitles.enumerated() {
-            let action = UIAlertAction(title: buttonTitle, 
-                                       style: styles[index],
-                                       handler: { _ in
-                completions[index]?()
-            })
+        for action in actions {
             alert.addAction(action)
         }
         self.present(alert, animated: true)
